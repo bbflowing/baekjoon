620ac4cf066542d78460727355ca1be2f6adfab5
mjda611 <mjda611@naver.com>
2021-01-09 10:29:04 +0900

    version 2.0

--- src/graph/Q2206.java
+++ src/graph/Q2206.java
@@ -1,7 +1,10 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Q2206 {
     public static void main (String args[]) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
