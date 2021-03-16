1db4550ce9b2f156ae3df4348ec6ebea44b1156c
mjda611 <mjda611@naver.com>
2021-03-16 20:17:22 +0900

    version 1.0

--- src/graph/Q13549.java
+++ src/graph/Q13549.java
@@ -8,9 +8,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Q12851 {
+public class Q13549 {
     /*
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -18,49 +18,43 @@ public class Q12851 {
         bfs(N, K);
     }
 
-    public static void bfs (int N, int K) {
+    public static void bfs(int N, int K) {
         Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
         int times[] = new int[100000 + 1];
         Arrays.fill(times, 987_654_321);
         times[N] = 0;
-        queue.add(N);
-        int answer = 0;
-        boolean flag = false;
-        int min = 0;
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
-            if (!flag && now == K) {
-                min = times[now];
-                ++answer;
-                flag = true;
-                continue;
-            }
-            if (min == times[now] && now == K) {
-                ++answer;
-                continue;
+            if (now == K) {
+                System.out.println(times[K]);
+                return;
             }
 
-            for (int i = 0; i < 3; ++i) {
+            int index = 2 * now;
+            while (index <= 100000 && times[index] == 987_654_321) {
+                times[index] = times[now];
+                queue.add(index);
+                index *= 2;
+            }
+
+            for (int i = 0; i < 2; ++i) {
                 int next = now;
                 if (i == 0) {
                     next -= 1;
                 } else if (i == 1) {
                     next += 1;
-                } else if (i == 2) {
-                    next *= 2;
                 }
-                if (next < 0 || 100000 < next) {
+                if (next < 0 || next > 100000) {
                     continue;
                 }
-                if (times[next] >= times[now] + 1) {
-                    times[next] = times[now] + 1;
+                if (times[next] == 987_654_321) {
+                    times[next] = times[now]+1;
                     queue.add(next);
                 }
             }
         }
-        System.out.println(min);
-        System.out.println(answer);
     }
 
      */
