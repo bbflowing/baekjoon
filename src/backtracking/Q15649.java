959a60ebfe584454ddb9bece08108cdbbae5bf83
mjda611 <mjda611@naver.com>
2021-03-18 23:36:24 +0900

    version 1.0

--- src/backtracking/Q15649.java
+++ src/backtracking/Q15649.java
@@ -1,44 +1,50 @@
 package backtracking;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Q15652 {
+public class Q15649 {
     /*
     public static BufferedWriter bw;
+    public static int input [];
+    public static boolean visited [];
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        input = new int [N];
+        for (int i=0; i<N; ++i) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
         int result [] = new int [M];
+        visited = new boolean [N];
         solve(N, M, 0, result);
         bw.flush();
     }
 
-    public static void solve (int N, int M, int index, int [] result) throws IOException {
+    public static void solve (int N, int M, int index, int result []) throws IOException {
         if (index == M) {
             String answer = "";
             for (int i=0; i<M; ++i) {
-                answer += result[i]+" ";
+                answer += result[i] + " ";
             }
             bw.append(answer);
             bw.newLine();
             return;
         }
 
-        for (int i=1; i<=N; ++i) {
-            if (1 <= index) {
-                if (result[index - 1] <= i) {
-                    result[index] = i;
-                    solve(N, M, index+1, result);
-                }
-            } else if (index == 0) {
-                result[index] = i;
-                solve(N, M, index+1, result);
+        for (int i=0; i<N; ++i) {
+            if (!visited[i]) {
+                result[index] = input[i];
+                visited[i] = true;
+                solve (N, M, index+1, result);
+                visited[i] = false;
             }
-
         }
     }
      */
