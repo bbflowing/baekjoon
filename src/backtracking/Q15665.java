92f45b02d447d3ac5bc495f98c85ebc5baad88aa
mjda611 <mjda611@naver.com>
2021-03-19 12:33:27 +0900

    version 1.0

---
+++ src/backtracking/Q15665.java
@@ -0,0 +1,53 @@
+package backtracking;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Q15665 {
+    /*
+    public static BufferedWriter bw;
+    public static int input [];
+    public static HashMap<String, Boolean> map;
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        input = new int [N];
+        map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; ++i) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+        int result [] = new int [M];
+        solve(N, M, 0, result);
+        bw.flush();
+    }
+
+    public static void solve (int N, int M, int index, int [] result) throws IOException {
+        if (index == M) {
+            String answer = "";
+            for (int i=0; i<M; ++i) {
+                answer += result[i] + " ";
+            }
+            if (map.containsKey(answer)) {
+                return;
+            } else {
+                map.put(answer, true);
+                bw.append(answer);
+                bw.newLine();
+            }
+            return;
+        }
+
+        for (int i=0; i<N; ++i) {
+            result[index] = input[i];
+            solve(N, M, index+1, result);
+        }
+    }
+     */
+}
