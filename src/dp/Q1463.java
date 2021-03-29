8a532600b8a8a9dbb9975655c36bced519245a39
mjda611 <mjda611@naver.com>
2021-03-29 18:58:14 +0900

    version 1.0

--- src/dp/Q1463.java
+++ src/dp/Q1463.java
@@ -6,27 +6,35 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class Q1463 {
+    /*
+    public static int min;
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(1, 0);
-        map.put(2, 1);
-        map.put(3, 1);
-
-        for (int i=4; i<=X; ++i) {
-            if (i%3 == 0 && i%2 != 0) {
-                map.put(i, Math.min(map.get(i/3), map.get(i-1))+1);
-            } else if (i%3 != 0 && i%2 == 0) {
-                map.put(i, Math.min(map.get(i/2), map.get(i-1))+1);
-            } else if (i%3 == 0 && i%2 == 0) {
-                int temp = Math.min(map.get(i/2), map.get(i/3));
-                map.put(i, Math.min(temp, map.get(i-1))+1);
-            } else {
-                map.put(i, map.get(i-1)+1);
-            }
-        }
-
-        System.out.println(map.get(X));
+        min = 987_654_321;
+        solve(X, 0);
+        System.out.println(min);
     }
+
+    public static void solve (int X, int count) {
+        if (X == 1) {
+            if (min > count) {
+                min = count;
+            }
+            return;
+        }
+        if (count >= min) {
+            return;
+        }
+        if (X % 3 == 0) {
+            int next = X/3;
+            solve(next, count+1);
+        }
+        if (X % 2 == 0) {
+            int next = X/2;
+            solve(next, count+1);
+        }
+        solve(X-1, count+1);
+    }
+     */
 }
