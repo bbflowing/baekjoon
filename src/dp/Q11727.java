c0d46e8e728cf2b503a85a67668dd9af352b3141
mjda611 <mjda611@naver.com>
2021-03-29 15:46:54 +0900

    version 1.0

---
+++ src/dp/Q11727.java
@@ -0,0 +1,25 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11727 {
+    /*
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int dp [] = new int [N+1];
+        for (int i=1; i<=N; ++i) {
+            if (i==1) {
+                dp[i] = 1;
+            } else if (i==2) {
+                dp[i] = 3;
+            } else {
+                dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+     */
+}
