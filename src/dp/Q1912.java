0ff311ec1734ff86d98099c184c9281fca965b83
mjda611 <mjda611@naver.com>
2021-03-29 16:05:38 +0900

    version 1.0

--- src/dp/Q1912.java
+++ src/dp/Q1912.java
@@ -5,19 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Q1912 {
+    /*
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int max = -1_000_000_000;
-
-        ArrayList<Integer> input = new ArrayList<>(N);
-        HashMap<String, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int input [] = new int [N];
+        int dp [] = new int [N];
         for (int i=0; i<N; ++i) {
-            input.add(Integer.parseInt(br.readLine()));
+            input[i] = Integer.parseInt(st.nextToken());
         }
-
-
+        dp[0] = input[0];
+        int max = dp[0];
+        for (int i=1; i<N; ++i) {
+            if (dp[i-1]+input[i] > input[i]) {
+                dp[i] = dp[i-1]+input[i];
+            } else {
+                dp[i] = input[i];
+            }
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        System.out.println(max);
     }
+     */
 }
