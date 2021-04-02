153b6bc7f237fa5cd1a8a787ff485ef0eb94c39f
mjda611 <mjda611@naver.com>
2021-04-02 17:27:16 +0900

    version 1.0

---
+++ src/dp/Q2748.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2748 {
+    /*
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long answers [] = new long [N+1];
+        answers[0] = 0;
+        answers[1] = 1;
+        if (N>=2) {
+            for (int i = 2; i <= N; ++i) {
+                answers[i] = answers[i-2] + answers[i-1];
+            }
+        }
+        System.out.println(answers[N]);
+    }
+     */
+}
