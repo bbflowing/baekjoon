4b105308f1e28c9ff542cfee15f313e7b7825078
mjda611 <mjda611@naver.com>
2021-04-02 17:53:52 +0900

    version 1.0

---
+++ src/dp/Q10870.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10870 {
+    /*
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answers [] = new int [N+2];
+        answers[0] = 0;
+        answers[1] = 1;
+        if (2<=N) {
+            for (int i = 2; i <= N; ++i) {
+                answers[i] = answers[i - 2] + answers[i - 1];
+            }
+        }
+        System.out.println(answers[N]);
+    }
+     */
+}
