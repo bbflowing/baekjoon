18e515240a5800c98eb7124680fa1435ed46c8b7
mjda611 <mjda611@naver.com>
2021-04-05 15:20:11 +0900

    version 1.0

--- src/graph/Q16390.java
+++ src/graph/Q16390.java
@@ -7,55 +7,52 @@ import java.util.StringTokenizer;
 
 public class Q16390 {
     /*
-    public static char dish [][];
+    public static int R, C;
+    public static char pixels [][];
     public static boolean visited [][];
-    public static int dx [] = {-1, -1, -1, 0, 0, 1, 1, 1};
-    public static int dy [] = {-1, 0, +1, -1, +1, -1, 0, 1};
-    public static int N, M;
+    public static int dx [] = {-1, -1, -1, +0, +0, +1, +1, +1};
+    public static int dy [] = {-1, +0, +1, -1, +1, -1, +0, +1};
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        dish = new char [N][M];
-        visited = new boolean [N][M];
-        int answer = 0;
-        for (int i=0; i<N; ++i) {
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        pixels = new char [R][C];
+        for (int i=0; i<R; ++i) {
             String line = br.readLine();
-            for (int j=0; j<M; ++j) {
-                dish[i][j] = line.charAt(j);
+            for (int j=0; j<C; ++j) {
+                pixels[i][j] = line.charAt(j);
             }
         }
-
-        for (int i=0; i<N; ++i) {
-            for (int j=0; j<M; ++j) {
-                if (!visited[i][j] && dish[i][j] == '#') {
-                    visited[i][j] = true;
-                    dfs(i, j);
-                    //System.out.println(i+","+j);
-                    ++answer;
+        visited = new boolean [R][C];
+        int counter = 0;
+        for (int i=0; i<R; ++i) {
+            for (int j=0; j<C; ++j) {
+                if (pixels[i][j] == '#' && !visited[i][j]) {
+                    ++counter;
+                    solve(i, j);
                 }
             }
         }
-        System.out.println(answer);
+        System.out.println(counter);
     }
 
-    public static void dfs (int x, int y) {
+    public static void solve (int x, int y) {
         for (int i=0; i<8; ++i) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx<0 || N<=nx || ny<0 || M<=ny) {
+            if (nx<0 || R<=nx || ny<0 || C<=ny) {
                 continue;
             }
             if (visited[nx][ny]) {
                 continue;
             }
-            if (dish[nx][ny] == '.') {
+            if (pixels[nx][ny] == '.') {
                 continue;
             }
             visited[nx][ny] = true;
-            dfs(nx, ny);
+            solve(nx,ny);
         }
     }
      */
