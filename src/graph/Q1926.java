5a78ac61edf01248fbdc7b746caaf4a3a0bf6208
mjda611 <mjda611@naver.com>
2021-04-05 18:43:24 +0900

    version 1.0

--- src/graph/Q1926.java
+++ src/graph/Q1926.java
@@ -1,68 +1,67 @@
 package graph;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Q1926 {
+    /*
+    public static int N, M, size;
+    public static int paper [][];
+    public static boolean visited [][];
     public static int dx [] = {-1, 1, 0, 0};
     public static int dy [] = {0, 0, -1, 1};
-    public static int input [][];
-    public static boolean check [][];
-    public static int N;
-    public static int M;
-    public static int answer;
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        input = new int [N][M];
-        check = new boolean [N][M];
+        paper = new int [N][M];
+        visited = new boolean [N][M];
         for (int i=0; i<N; ++i) {
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<M; ++j) {
-                input[i][j] = Integer.parseInt(st.nextToken());
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         int counter = 0;
-        int max = -1;
+        int answer = 0;
         for (int i=0; i<N; ++i) {
             for (int j=0; j<M; ++j) {
-                if (input[i][j] == 1 && !check[i][j]) {
-                    answer = 1;
-                    check[i][j] = true;
-                    dfs(i, j);
+                if (paper[i][j] == 1 && !visited[i][j]) {
+                    size = 1;
                     ++counter;
-                    if (answer > max) {
-                        max = answer;
+                    visited[i][j] = true;
+                    dfs(i, j);
+                    if (size > answer) {
+                        answer = size;
                     }
                 }
             }
         }
-        if (counter == 0) {
-            max = 0;
-        }
         System.out.println(counter);
-        System.out.println(max);
+        System.out.println(answer);
     }
+
     public static void dfs (int x, int y) {
         for (int i=0; i<4; ++i) {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if (newX < 0 || N <= newX || newY < 0 || M <= newY) {
+            if (nx<0 || N<=nx || ny<0 || M<=ny) {
                 continue;
             }
-            if (check[newX][newY]) {
+            if (visited[nx][ny]) {
                 continue;
             }
-            if (input[newX][newY] == 1) {
-                check[newX][newY] = true;
-                ++answer;
-                dfs (newX, newY);
+            if (paper[nx][ny] == 1) {
+                ++size;
+                visited[nx][ny] = true;
+                dfs(nx, ny);
             }
         }
     }
+     */
 }
