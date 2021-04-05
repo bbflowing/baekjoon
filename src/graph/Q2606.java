74a5cda50e154405d9228b7eb15b4448ca0d81cc
mjda611 <mjda611@naver.com>
2021-04-05 18:31:49 +0900

    version 1.0

--- src/graph/Q2606.java
+++ src/graph/Q2606.java
@@ -9,43 +9,43 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q2606 {
-    public static ArrayList<ArrayList<Integer>> connection;
+    public static ArrayList<ArrayList<Integer>> network;
+    public static boolean visited [];
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int computers = Integer.parseInt(br.readLine());
-        int edges = Integer.parseInt(br.readLine());
-        connection = new ArrayList<>(computers);
-        for (int i=0; i<=computers; ++i) {
-            connection.add(new ArrayList<>());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        network = new ArrayList<>();
+        for (int i=0; i<=N; ++i) {
+            network.add(new ArrayList<>());
         }
-        for (int i=0; i<edges; ++i) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<M; ++i) {
+            st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            connection.get(start).add(end);
-            connection.get(end).add(start);
+            network.get(start).add(end);
+            network.get(end).add(start);
         }
-        bfs(1, computers);
-    }
-
-    public static void bfs(int start, int computers) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean check [] = new boolean[computers];
-        queue.add(start);
-        check[start-1] = true;
-        int counter = 0;
-
-        while (!queue.isEmpty()) {
-            int visited = queue.poll();
-            for (int i=0; i<connection.get(visited).size(); ++i) {
-                int next = connection.get(visited).get(i);
-                if (!check[next-1]) {
-                    check[next-1] = true;
-                    queue.add(next);
-                    counter++;
-                }
+        visited = new boolean [N+1];
+        visited[1] = true;
+        solve(1);
+        int virus = 0;
+        for (int i=2; i<=N; ++i) {
+            if (visited[i]) {
+                ++virus;
             }
         }
-        System.out.println(counter);
+        System.out.println(virus);
+    }
+
+    public static void solve (int start) {
+        ArrayList<Integer> target = network.get(start);
+        for (int i=0; i<target.size(); ++i) {
+            if (!visited[target.get(i)]) {
+                visited[target.get(i)] = true;
+                solve(target.get(i));
+            }
+        }
     }
 }
---
+++ src/graph/Q3709.java
@@ -0,0 +1,10 @@
+package graph;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Q3709 {
+
+}
