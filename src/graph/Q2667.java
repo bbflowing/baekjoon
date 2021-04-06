c711795fafc0b4041bf3c7765f87ba1c8c54862e
mjda611 <mjda611@naver.com>
2021-04-06 11:33:51 +0900

    version 1.0

--- src/graph/Q2667.java
+++ src/graph/Q2667.java
@@ -7,39 +7,40 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Q2667 {
-    public static char input [][];
+    /*
+    public static int map [][];
     public static boolean visited [][];
-    public static int number = 0;
+    public static int size;
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        input = new char [N][N];
-        visited = new boolean [N][N];
+        map = new int [N][N];
         for (int i=0; i<N; ++i) {
             String line = br.readLine();
             for (int j=0; j<N; ++j) {
-                input[i][j] = line.charAt(j);
+                map[i][j] = line.charAt(j)-'0';
             }
         }
+        visited = new boolean [N][N];
         int counter = 0;
-        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Pair> result = new ArrayList<>();
         for (int i=0; i<N; ++i) {
             for (int j=0; j<N; ++j) {
-                if (input[i][j] == '1' && !visited[i][j]) {
-                    visited[i][j] = true;
-                    number = 1;
+                if (!visited[i][j] && map[i][j] == 1) {
                     ++counter;
+                    size = 1;
+                    visited[i][j] = true;
+                    map[i][j] = counter;
                     dfs(i, j, N, counter);
-                    numbers.add(number);
+                    result.add(new Pair(counter, size));
                 }
             }
         }
-        String answer = counter+"\n";
-        Collections.sort(numbers);
-        for (int i=0; i<counter; ++i) {
-            answer += numbers.get(i)+"\n";
+        Collections.sort(result);
+        System.out.println(result.size());
+        for (int i=0; i<result.size(); ++i) {
+            System.out.println(result.get(i).size);
         }
-        System.out.println(answer);
     }
 
     public static void dfs (int x, int y, int N, int counter) {
@@ -56,14 +57,36 @@ public class Q2667 {
             if (visited[nx][ny]) {
                 continue;
             }
-            if (input[nx][ny] == '0') {
+            if (map[nx][ny] == 0) {
                 continue;
             }
-
+            map[nx][ny] = counter;
             visited[nx][ny] = true;
-            ++number;
+            ++size;
             dfs(nx, ny, N, counter);
+        }
+    }
+     */
+}
 
+/*
+class Pair implements Comparable<Pair>{
+    int counter;
+    int size;
+
+    Pair (int counter, int size) {
+        this.counter = counter;
+        this.size = size;
+    }
+
+    public int compareTo (Pair p) {
+        if (this.size > p.size) {
+            return 1;
+        } else if (this.size == p.size) {
+            return 0;
+        } else {
+            return -1;
         }
     }
 }
+ */
