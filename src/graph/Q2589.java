b04208da7f796e24b5a53b0b2b44f825d3e50378
mjda611 <mjda611@naver.com>
2021-04-08 12:59:00 +0900

    version 1.0

--- src/graph/Q2589.java
+++ src/graph/Q2589.java
@@ -10,118 +10,68 @@ import java.util.StringTokenizer;
 
 public class Q2589 {
     /*
-    public static char input [][];
-    public static int check [][];
-    public static int size;
+    public static int R, C;
+    public static char map [][];
+    public static int dx [] = {-1, 1, 0, 0};
+    public static int dy [] = {0, 0, -1, 1};
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        input = new char [N][M];
-        check = new int [N][M];
-        for (int i=0; i<N; ++i) {
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char [R][C];
+        for (int i=0; i<R; ++i) {
             String line = br.readLine();
-            for (int j=0; j<M; ++j) {
-                input[i][j] = line.charAt(j);
+            for (int j=0; j<C; ++j) {
+                map[i][j] = line.charAt(j);
             }
         }
-        int counter = 0;
-        int max = -1;
-
-        for (int i=0; i<N; ++i) {
-            Arrays.fill(check[i], -1);
-        }
-
-        for (int i=0; i<N; ++i) {
-            for (int j=0; j<M; ++j) {
-                if (input[i][j] == 'L' && check[i][j] == -1) {
-                    size = 1;
-                    check[i][j] = counter;
-                    dfs(i, j, counter, N, M);
-                    if (max < size) {
-                        max = size;
-                    }
-                    ++counter;
-                }
-            }
-        }
-        if (max != 1) {
-            max = -1;
-            int x = 0; int y = 0;
-            for (int i=0; i<N; ++i) {
-                for (int j=0; j<M; ++j) {
-                    if (check[i][j] != -1) {
-                        x = i; y = j;
-                        int answer = bfs(check[i][j], x, y, N, M);
-                        if (answer > max) {
-                            max = answer;
-                        }
+        int answer = 0;
+        for (int i=0; i<R; ++i) {
+            for (int j=0; j<C; ++j) {
+                if (map[i][j] == 'L') {
+                    int result = bfs(i, j);
+                    if (answer < result) {
+                        answer = result;
                     }
                 }
             }
         }
-        System.out.println(max);
-
+        System.out.println(answer);
     }
 
-    public static int bfs (int target, int x, int y, int N, int M) {
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
+    public static int bfs (int x, int y) {
         Queue<Coordinate> queue = new LinkedList<>();
         queue.add(new Coordinate(x, y, 0));
-        boolean visited [][] = new boolean [N][M];
+        boolean visited [][] = new boolean [R][C];
         visited[x][y] = true;
-        int max = -1;
+        int result = -1;
 
         while (!queue.isEmpty()) {
-            Coordinate current = queue.poll();
-            if (current.distance > max) {
-                max = current.distance;
+            Coordinate c = queue.poll();
+            if (c.distance > result) {
+                result = c.distance;
             }
-            for (int i=0; i<4; ++i) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
 
-                if (nx<0 || N<=nx || ny<0 || M<=ny) {
-                    continue;
-                }
-                if (check[nx][ny] != target) {
+            for (int i=0; i<4; ++i) {
+                int nx = c.x + dx[i];
+                int ny = c.y + dy[i];
+
+                if (nx<0 || R<=nx || ny<0 || C<=ny) {
                     continue;
                 }
                 if (visited[nx][ny]) {
                     continue;
                 }
+                if (map[nx][ny] == 'W') {
+                    continue;
+                }
+                queue.add(new Coordinate(nx, ny, c.distance+1));
                 visited[nx][ny] = true;
-                queue.add(new Coordinate(nx, ny, current.distance+1));
             }
         }
-        return max;
+        return result;
     }
-    public static void dfs (int x, int y, int counter, int N, int M) {
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
-
-        for (int i=0; i<4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx<0 || N<=nx || ny<0 || M<=ny) {
-                continue;
-            }
-            if (check[nx][ny] != -1) {
-                continue;
-            }
-            if (input[nx][ny] == 'W') {
-                continue;
-            }
-            check[nx][ny] = counter;
-            ++size;
-            dfs(nx, ny, counter, N, M);
-        }
-
-    }
-
      */
 }
 
@@ -137,5 +87,4 @@ class Coordinate {
         this.distance = distance;
     }
 }
-
  */
