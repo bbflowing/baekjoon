9c4f3754b7cf2ec72df0ec08b740d3d6572062bb
mjda611 <mjda611@naver.com>
2021-04-09 17:50:16 +0900

    version 1.0

--- src/graph/Q15559.java
+++ src/graph/Q15559.java
@@ -3,61 +3,67 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q15559 {
     /*
-    public static int R, C, answer;
+    public static int N, M, answer;
     public static char map [][];
-    public static int group [][];
+    public static int groups [][];
+    public static boolean visited [][];
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        map = new char [R][C];
-        group = new int [R][C];
-        for (int i=0; i<R; ++i) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char [N][M];
+        for (int i=0; i<N; ++i) {
             String line = br.readLine();
-            for (int j=0; j<C; ++j) {
+            for (int j=0; j<M; ++j) {
                 map[i][j] = line.charAt(j);
             }
         }
-        int counter = 0;
         answer = 0;
-        for (int i=0; i<R; ++i) {
-            for (int j=0; j<C; ++j) {
-                if (group[i][j] == 0) {
+        groups = new int [N][M];
+        visited = new boolean [N][M];
+        int counter = 0;
+        for (int i=0; i<N; ++i) {
+            for (int j=0; j<M; ++j) {
+                if (!visited[i][j]) {
                     ++counter;
-                    group[i][j] = counter;
-                    solve(i, j, counter);
+                    groups[i][j] = counter;
+                    visited[i][j] = true;
+                    dfs(i, j, counter);
                 }
             }
         }
         System.out.println(answer);
-
     }
 
-    public static void solve (int x, int y, int counter) {
-        int nx = x; int ny = y;
+    public static void dfs (int x, int y, int counter) {
+        int nx = x;
+        int ny = y;
         if (map[x][y] == 'S') {
             ++nx;
         } else if (map[x][y] == 'N') {
             --nx;
         } else if (map[x][y] == 'E') {
             ++ny;
-        } else if (map[x][y] == 'W') {
+        } else {
             --ny;
         }
-        if (group[nx][ny] == counter) {
-            ++answer;
+
+        if (!visited[nx][ny]) {
+            visited[nx][ny] = true;
+            groups[nx][ny] = counter;
+            dfs(nx, ny, counter);
+        } else {
+            if (groups[nx][ny] == counter) {
+                ++answer;
+            }
             return;
         }
-        if (group[nx][ny] != counter && group[nx][ny] != 0) {
-            return;
-        }
-        group[nx][ny] = counter;
-        solve(nx, ny, counter);
     }
      */
 }
