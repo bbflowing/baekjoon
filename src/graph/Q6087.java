269e0d4eec3780c98ffb41b85056563239d9a229
mjda611 <mjda611@naver.com>
2021-04-12 17:01:59 +0900

    version 1.0

--- src/graph/Q6087.java
+++ src/graph/Q6087.java
@@ -10,34 +10,37 @@ import java.util.StringTokenizer;
 
 public class Q6087 {
     /*
+    public static int R, C;
     public static char map [][];
-    public static int W, H;
-    public static void main (String args[]) throws IOException {
+    public static void main (String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        map = new char [H][W];
+        C = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        map = new char [R][C];
         boolean flag = false;
         int startX = 0; int startY = 0;
-        for (int i=0; i<H; ++i) {
+        for (int i=0; i<R; ++i) {
             String line = br.readLine();
-            for (int j=0; j<W; ++j) {
+            for (int j=0; j<C; ++j) {
                 map[i][j] = line.charAt(j);
-                if (map[i][j] == 'C' && !flag) {
-                    startX = i; startY = j;
-                    map[i][j] = '.';
-                    flag = true;
+                if (map[i][j] == 'C') {
+                    if (!flag) {
+                        map[i][j] = '.';
+                        startX = i; startY = j;
+                        flag = true;
+                    }
                 }
             }
         }
         bfs(startX, startY);
     }
+
     public static void bfs (int x, int y) {
         int dx [] = {-1, 1, 0, 0};
         int dy [] = {0, 0, -1, 1};
-        int mirrors [][] = new int [H][W];
-        for (int i=0; i<H; ++i) {
+        int mirrors [][] = new int [R][C];
+        for (int i=0; i<R; ++i) {
             Arrays.fill(mirrors[i], 987_654_321);
         }
         mirrors[x][y] = 0;
@@ -48,32 +51,33 @@ public class Q6087 {
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
             if (map[c.x][c.y] == 'C') {
-                if (c.count < answer) {
-                    answer = c.count;
+                if (answer > c.distance) {
+                    answer = c.distance;
                 }
             }
-            for (int i=0; i<4; ++i) {
-                int nx = c.x + dx[i];
-                int ny = c.y + dy[i];
 
-                if (nx<0 || H<=nx || ny<0 || W<=ny) {
+            for (int dir=0; dir<4; ++dir) {
+                int nx = c.x + dx[dir];
+                int ny = c.y + dy[dir];
+
+                if (nx<0 || R<=nx || ny<0 || C<=ny) {
                     continue;
                 }
                 if (map[nx][ny] == '*') {
                     continue;
                 }
-                if (c.direction == -1) {
+                if (c.before == -1) {
                     mirrors[nx][ny] = 0;
-                    queue.add(new Coordinate(nx, ny, i, 0));
-                } else if (c.direction != i) {
-                    if (c.count+1 <= mirrors[nx][ny]) {
-                        mirrors[nx][ny] = c.count+1;
-                        queue.add(new Coordinate(nx, ny, i, c.count+1));
+                    queue.add(new Coordinate(nx, ny, dir, c.distance));
+                } else if (c.before == dir) {
+                    if (mirrors[nx][ny] >= c.distance) {
+                        mirrors[nx][ny] = c.distance;
+                        queue.add(new Coordinate(nx, ny, dir, c.distance));
                     }
-                } else if (c.direction == i) {
-                    if (c.count <= mirrors[nx][ny]) {
-                        mirrors[nx][ny] = c.count+1;
-                        queue.add(new Coordinate(nx, ny, i, c.count));
+                } else {
+                    if (mirrors[nx][ny] >= c.distance+1) {
+                        mirrors[nx][ny] = c.distance+1;
+                        queue.add(new Coordinate(nx, ny, dir, c.distance+1));
                     }
                 }
             }
@@ -87,14 +91,15 @@ public class Q6087 {
 class Coordinate {
     int x;
     int y;
-    int direction;
-    int count;
+    int before;
+    int distance;
 
-    Coordinate (int x, int y, int direction, int count) {
+    Coordinate(int x, int y, int before, int distance) {
         this.x = x;
         this.y = y;
-        this.direction = direction;
-        this.count = count;
+        this.before = before;
+        this.distance = distance;
     }
 }
  */
+
