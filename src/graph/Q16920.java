574186798cf72a2f58218550401536e53134060b
mjda611 <mjda611@naver.com>
2021-04-13 16:00:45 +0900

    version 1.0

--- src/graph/Q16920.java
+++ src/graph/Q16920.java
@@ -5,125 +5,117 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q16920 {
     /*
-    static int r,c,p;
-    static int dx[] = { 1, -1, 0, 0 };
-    static int dy[] = { 0, 0, 1, -1 };
-    static int player[];
-    static int[][] map;
-    static Queue<Node> q[];
-
-
-    public static void main(String[] args) throws IOException {
+    public static int N, M;
+    public static char board[][];
+    public static int answers[];
+    public static ArrayList<Queue<Coordinate>> queues;
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-//		int test_case = Integer.parseInt(br.readLine().trim());
-
-//		for (int T = 1; T <= test_case; T++) {
-        st =new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        p = Integer.parseInt(st.nextToken());
-
-        player = new int[p+1];
-        map = new int[r][c];
-        q = new LinkedList[p+1];
-        for(int i=1; i<=p; i++)q[i] = new LinkedList<>();
-
-        st =new StringTokenizer(br.readLine());
-        for(int i=1; i<=p; i++)player[i] = Integer.parseInt(st.nextToken());
-
-        for(int i=0; i<r; i++) {
-            String a =br.readLine();
-            for(int j=0; j<a.length(); j++) {
-                char tmp = a.charAt(j);
-                if(tmp>='1' && tmp<='9') {
-                    map[i][j] = tmp-'0';
-                    q[map[i][j]].add(new Node(i,j));
-                }
-                else if(tmp =='#')map[i][j]=-1;
-                if(map[i][j]=='.') {
-                    map[i][j]=0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        board = new char[N][M];
+        int moves [] = new int[P + 1];
+        queues = new ArrayList<>();
+        answers = new int [P + 1];
+        for (int i=0; i<=P; ++i) {
+            queues.add(new LinkedList<>());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= P; ++i) {
+            moves[i] = Integer.parseInt(st.nextToken());
+        }
+        //System.out.println(Arrays.toString(moves));
+        int blanks = 0;
+        for (int i = 0; i < N; ++i) {
+            String line = br.readLine();
+            for (int j = 0; j < M; ++j) {
+                board[i][j] = line.charAt(j);
+                int index = board[i][j] - '0';
+                if (1 <= index && index <= 9) {
+                    queues.get(index).add(new Coordinate(i, j));
+                    ++answers[index];
+                } else if (board[i][j] == '.'){
+                    ++blanks;
                 }
             }
-        }//insert
+        }
+        bfs(P, blanks, moves);
+        String answer = "";
+        for (int i=1; i<=P; ++i) {
+            answer += answers[i]+" ";
+        }
+        bw.append(answer);
+        bw.flush();
+    }
 
-        //sol
+    public static void bfs (int P, int blanks, int [] moves) {
+        int dx [] = {-1, 1, 0, 0};
+        int dy [] = {0, 0, -1, 1};
+        boolean [] end = new boolean [P+1];
+        int counter = 0;
 
-        boolean flag=false;
-        while(!flag){
-            //플레이어 큐가 비면 종료시켜줌
-            int cnt=0;
-            for(int i=1; i<=p; i++) {
-                if(q[i].size() ==0)cnt++;
-            }
-            if(cnt==p) {
-                flag=true;
-                break;
-            }
+        while (blanks > 0 || counter != P) {
+            for (int player=1; player<=P; ++player) {
+                if (end[player]) {
+                    continue;
+                }
+                for (int move=0; move<moves[player]; ++move) {
+                    Queue<Coordinate> queue = queues.get(player);
+                    int size = queue.size();
+                    for (int i=0; i<size; ++i) {
+                        Coordinate c = queue.poll();
+                        for (int dir=0; dir<4; ++dir) {
+                            int nx = c.x + dx[dir];
+                            int ny = c.y + dy[dir];
 
-            for(int i=1; i<=p; i++) {
-                for(int play=0; play<player[i]; play++ ) {
-                    int qsize = q[i].size();
-                    if(qsize==0)break;
-
-                    while(qsize-->0) {
-                        Node cur = q[i].poll();
-
-                        for(int dir=0; dir<4; dir++) {
-                            int nx = cur.x + dx[dir];
-                            int ny = cur.y + dy[dir];
-                            if(nx>=0 && ny>=0 && nx<r && ny<c && map[nx][ny]==0) {
-                                map[nx][ny]=i;
-                                q[i].add(new Node(nx,ny));
+                            if (nx<0 || N<=nx || ny<0 || M<=ny) {
+                                continue;
                             }
-
+                            if (board[nx][ny] == '#') {
+                                continue;
+                            }
+                            if (board[nx][ny] == '.' || board[nx][ny] == (char) (player+'0')) {
+                                queue.add(new Coordinate(nx, ny));
+                                if (board[nx][ny] == '.') {
+                                    board[nx][ny] = (char)(player+'0');
+                                    --blanks;
+                                    ++answers[player];
+                                    if (blanks == 0) {
+                                        return;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
-
-            }
-
-        }//while loop
-
-        //print
-        int rst[] = new int[p+1];
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                if(map[i][j]>=1 && map[i][j]<=9)
-                    rst[map[i][j]]++;
+                if (queues.get(player).size() == 0 && !end[player]) {
+                    end[player] = true;
+                    ++counter;
+                    if (counter == P) {
+                        return;
+                    }
+                }
             }
         }
-
-        for(int i=1; i<=p; i++) {
-            System.out.print(rst[i]+" ");
-        }
-
-//		} // test_case
-
-    }// main
-*/
-}// class
+    }
+     */
+}
 
 /*
-class Node{
-    int x,y;
+class Coordinate {
+    int x;
+    int y;
 
-    public Node(int x, int y) {
-        super();
+    Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
 }
-*/
-
-
+ */
