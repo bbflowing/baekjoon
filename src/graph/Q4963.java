57c02d79fbf0392a5e8b976af44357596d310cdf
mjda611 <mjda611@naver.com>
2021-04-14 12:35:08 +0900

    version 1.0

--- src/graph/Q4963.java
+++ src/graph/Q4963.java
@@ -7,9 +7,11 @@ import java.util.StringTokenizer;
 
 public class Q4963 {
     /*
-    public static char map [][];
+    public static int map [][];
     public static boolean visited [][];
-    public static int C, R;
+    public static int R, C;
+    public static int dx [] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    public static int dy [] = {-1, 0, 1, -1, 1, -1, 0, 1};
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -22,68 +24,48 @@ public class Q4963 {
             st = new StringTokenizer(line);
             C = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
-            map = new char [R][C];
-            for (int i=0; i<R; ++i) {
+            map = new int[R][C];
+            visited = new boolean[R][C];
+            for (int i = 0; i < R; ++i) {
                 st = new StringTokenizer(br.readLine());
-                for (int j=0; j<C; ++j) {
-                    map[i][j] = st.nextToken().charAt(0);
+                for (int j = 0; j < C; ++j) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            int answer = 0;
-            visited = new boolean [R][C];
-            for (int i=0; i<R; ++i) {
-                for (int j=0; j<C; ++j) {
-                    if (!visited[i][j] && map[i][j] == '1') {
+
+            int count = 0;
+            for (int i = 0; i < R; ++i) {
+                for (int j = 0; j < C; ++j) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        ++count;
                         visited[i][j] = true;
-                        ++answer;
-                        bfs(i, j);
+                        solve(i, j);
                     }
                 }
             }
-            bw.append(String.valueOf(answer));
+            bw.append(String.valueOf(count));
             bw.newLine();
         }
         bw.flush();
     }
 
-    public static void bfs (int x, int y) {
-        int dx [] = {-1, -1, -1, 0, 0, +1, +1, +1};
-        int dy [] = {-1, 0, +1, -1, +1, -1, 0, +1};
+    public static void solve (int x, int y) {
+        for (int dir=0; dir<8; ++dir) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
 
-        Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(new Coordinate(x, y));
-
-        while (!queue.isEmpty()) {
-            Coordinate c = queue.poll();
-            for (int dir=0; dir<8; ++dir) {
-                int nx = c.x + dx[dir];
-                int ny = c.y + dy[dir];
-
-                if (nx<0 || R<=nx || ny<0 || C<=ny) {
-                    continue;
-                }
-                if (visited[nx][ny]) {
-                    continue;
-                }
-                if (map[nx][ny] == '0') {
-                    continue;
-                }
-                visited[nx][ny] = true;
-                queue.add(new Coordinate(nx, ny));
+            if (nx<0 || R<=nx || ny<0 || C<=ny) {
+                continue;
             }
+            if (visited[nx][ny]) {
+                continue;
+            }
+            if (map[nx][ny] == 0) {
+                continue;
+            }
+            visited[nx][ny] = true;
+            solve(nx, ny);
         }
     }
      */
 }
-
-/*
-class Coordinate {
-    int x;
-    int y;
-
-    Coordinate (int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
- */
