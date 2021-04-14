c7147951b3a190dd71f2a93db64d3a06bc5344ab
mjda611 <mjda611@naver.com>
2021-04-14 10:50:37 +0900

    version 1.0

--- src/graph/Q17836.java
+++ src/graph/Q17836.java
@@ -10,73 +10,69 @@ import java.util.StringTokenizer;
 
 public class Q17836 {
     /*
-    public static int map [][];
+    public static int R, C;
+    public static int castle [][];
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
-        map = new int [N+1][M+1];
-        for (int i=1; i<=N; ++i) {
+        castle = new int [R+1][C+1];
+        for (int i=1; i<=R; ++i) {
             st = new StringTokenizer(br.readLine());
-            for (int j=1; j<=M; ++j) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j=1; j<=C; ++j) {
+                castle[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        bfs(N, M, T);
+        bfs(T);
     }
-
-    public static void bfs (int N, int M, int T) {
-        Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(new Coordinate(1, 1, 0, 0));
-        boolean visited [][][] = new boolean [N+1][M+1][2];
-        visited[1][1][0] = true;
+    public static void bfs (int T) {
         int dx [] = {-1, 1, 0, 0};
         int dy [] = {0, 0, -1, 1};
+        boolean visited [][][] = new boolean [2][R+1][C+1];
+        visited[0][1][1] = true;
+        Queue<Coordinate> queue = new LinkedList<>();
+        queue.add(new Coordinate(1, 1, 0, 0));
 
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
-            if (c.x == N && c.y == M && c.distance <= T) {
+            if (c.x == R && c.y == C && c.distance<=T) {
                 System.out.println(c.distance);
                 return;
             }
             if (c.distance > T) {
-                break;
+                System.out.println("Fail");
+                return;
             }
-            for (int i=0; i<4; ++i) {
-                int nx = c.x + dx[i];
-                int ny = c.y + dy[i];
-                int nsword = c.sword;
-                int ndistance = c.distance+1;
+            for (int dir=0; dir<4; ++dir) {
+                int nx = c.x + dx[dir];
+                int ny = c.y + dy[dir];
 
-                if (nx<1 || N+1<=nx || ny<1 || M+1<=ny) {
+                if (nx<1 || R<nx || ny<1 || C<ny) {
                     continue;
                 }
-                if (map[nx][ny] == 0) {
-                    if (!visited[nx][ny][nsword]) {
-                        visited[nx][ny][nsword] = true;
-                        queue.add(new Coordinate(nx, ny, nsword, ndistance));
+                if (castle[nx][ny] == 0) {
+                    if (!visited[c.sword][nx][ny]) {
+                        visited[c.sword][nx][ny] = true;
+                        queue.add(new Coordinate(nx, ny, c.distance+1, c.sword));
                     }
-                } else if (map[nx][ny] == 1) {
-                    if (nsword == 1) {
-                        if (!visited[nx][ny][nsword]) {
-                            visited[nx][ny][nsword] = true;
-                            queue.add(new Coordinate(nx, ny, nsword, ndistance));
-                        }
+                } else if (castle[nx][ny] == 1) {
+                    if (c.sword == 1 && !visited[1][nx][ny]) {
+                        visited[1][nx][ny] = true;
+                        queue.add(new Coordinate(nx, ny, c.distance+1, 1));
                     }
-                } else if (map[nx][ny] == 2) {
-                    nsword = 1;
-                    if (!visited[nx][ny][nsword]) {
-                        visited[nx][ny][nsword] = true;
-                        queue.add(new Coordinate(nx, ny, nsword, ndistance));
+                } else if (castle[nx][ny] == 2) {
+                    int nsword = 1;
+                    if (!visited[nsword][nx][ny]) {
+                        visited[1][nx][ny] = true;
+                        queue.add(new Coordinate(nx, ny, c.distance+1, nsword));
                     }
                 }
             }
         }
         System.out.println("Fail");
     }
-
      */
 }
 
@@ -84,15 +80,14 @@ public class Q17836 {
 class Coordinate {
     int x;
     int y;
-    int sword;
     int distance;
+    int sword;
 
-    Coordinate (int x, int y, int sword, int distance) {
+    Coordinate (int x, int y, int distance, int sword) {
         this.x = x;
         this.y = y;
-        this.sword = sword;
         this.distance = distance;
+        this.sword = sword;
     }
 }
-
  */
