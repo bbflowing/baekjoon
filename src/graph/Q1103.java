99b6aa76f32cd914ae1d3741aecc4b0b9ad4b6f4
mjda611 <mjda611@naver.com>
2021-04-17 13:29:30 +0900

    version 1.0

---
+++ src/graph/Q1103.java
@@ -0,0 +1,61 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q1103 {
+    /*
+    public static int R, C;
+    public static char board[][];
+    public static int dx[] = {-1, 1, 0, 0};
+    public static int dy[] = {0, 0, -1, 1};
+    public static int distance[][];
+    public static boolean visited[][];
+
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new char[R][C];
+        visited = new boolean[R][C];
+        distance = new int[R][C];
+        for (int i=0; i<R; ++i) {
+            String line = br.readLine();
+            for (int j=0; j<C; ++j) {
+                board[i][j] = line.charAt(j);
+                distance[i][j] = -1;
+            }
+        }
+        solve(0, 0);
+        System.out.println(distance[0][0]);
+    }
+
+    public static int solve(int x, int y)  {
+        if (x<0 || R<=x || y<0 || C<=y || (board[x][y] == 'H')) {
+            return 0;
+        }
+        if (visited[x][y]) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+        if (distance[x][y] != -1) {
+            return distance[x][y];
+        }
+        visited[x][y] = true;
+        distance[x][y] = 0;
+        int multiplier = board[x][y] - '0';
+        for (int dir = 0; dir < 4; ++dir) {
+            int nx = x + dx[dir] * multiplier;
+            int ny = y + dy[dir] * multiplier;
+            distance[x][y] = Math.max(distance[x][y], solve(nx, ny)+1);
+        }
+        visited[x][y] = false;
+        return distance[x][y];
+    }
+     */
+}
+
--- src/graph/Q14948.java
+++ src/graph/Q14948.java
@@ -3,9 +3,7 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -13,16 +11,15 @@ public class Q14948 {
     /*
     public static int R, C;
     public static int army[][];
-
-    public static void main(String args[]) throws IOException {
+    public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         army = new int[R][C];
-        for (int i = 0; i < R; ++i) {
+        for (int i=0; i<R; ++i) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < C; ++j) {
+            for (int j=0; j<C; ++j) {
                 army[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -33,55 +30,54 @@ public class Q14948 {
         int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, -1, 1};
         int visited[][][][] = new int[2][2][R][C];
-        for (int i = 0; i < 2; ++i) {
-            for (int a = 0; a < 2; ++a) {
-                for (int j = 0; j < R; ++j) {
-                    for (int k = 0; k < C; ++k) {
-                        visited[i][a][j][k] = -1;
-                    }
+        for (int i=0; i<2; ++i) {
+            for (int j=0; j<2; ++j) {
+                for (int k=0; k<R; ++k) {
+                    Arrays.fill(visited[i][j][k], -1);
                 }
             }
         }
-        visited[0][0][0][0] = army[0][0];
         PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        queue.add(new Coordinate(0, 0, 0, army[0][0]));
-        int answer = 987_654_321;
+        queue.add(new Coordinate(0, army[0][0], 0, 0));
+        visited[0][0][0][0] = army[0][0];
 
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
-
-            if (c.x == R - 1 && c.y == C - 1) {
+            if (c.x == R-1 && c.y == C-1) {
                 System.out.println(c.max);
-                break;
+                return;
             }
-            for (int dir = 0; dir < 4; ++dir) {
+            for (int dir=0; dir<4; ++dir) {
                 int nx = c.x + dx[dir];
                 int ny = c.y + dy[dir];
 
-                if (nx < 0 || R <= nx || ny < 0 || C <= ny) {
+                if (nx<0 || R<=nx || ny<0 || C<=ny) {
                     continue;
                 }
                 int nmax = Math.max(c.max, army[nx][ny]);
-                if (visited[0][c.skipped][nx][ny] <= nmax && visited[0][c.skipped][nx][ny] != -1) {
+                if (visited[c.skipped][0][nx][ny] <= nmax && visited[c.skipped][0][nx][ny] != -1) {
                     continue;
-                } else {
-                    visited[0][c.skipped][nx][ny] = nmax;
-                    queue.add(new Coordinate(nx, ny, c.skipped, nmax));
-                    //System.out.println(nx + "," + ny + "," + c.skipped + "," + nmax);
                 }
+                visited[c.skipped][0][nx][ny] = nmax;
+                queue.add(new Coordinate(c.skipped, nmax, nx, ny));
+
                 if (c.skipped == 0) {
                     int nnx = nx + dx[dir];
                     int nny = ny + dy[dir];
-                    if (nnx < 0 || R <= nnx || nny < 0 || C <= nny) {
+
+                    if (nnx<0 || R<=nnx || nny<0 || C<=nny) {
                         continue;
                     }
                     nmax = Math.max(c.max, army[nnx][nny]);
-                    if (visited[1][0][nx][ny] <= nmax && visited[1][0][nx][ny] != -1) {
+                    if (visited[1][0][nnx][nny] <= nmax && visited[1][0][nnx][nny] != -1) {
                         continue;
                     }
-                    visited[0][1][nnx][nny] = nmax;
-                    visited[1][0][nx][ny] = nmax;
-                    queue.add(new Coordinate(nnx, nny, 1, nmax));
+                    if (visited[0][1][nx][ny] <= nmax && visited[0][1][nx][ny] != -1) {
+                        continue;
+                    }
+                    visited[1][0][nnx][nny] = nmax;
+                    visited[0][1][nx][ny] = nmax;
+                    queue.add(new Coordinate(1, nmax, nnx, nny));
                 }
             }
         }
@@ -91,25 +87,21 @@ public class Q14948 {
 
 /*
 class Coordinate implements Comparable<Coordinate> {
-    int x;
-    int y;
     int skipped;
     int max;
+    int x;
+    int y;
 
-    Coordinate(int x, int y, int skipped, int max) {
-        this.x = x;
-        this.y = y;
+    Coordinate (int skipped, int max, int x, int y) {
         this.skipped = skipped;
         this.max = max;
+        this.x = x;
+        this.y = y;
     }
 
     @Override
-    public int compareTo(Coordinate c) {
-        if (this.max >= c.max) {
-            return 1;
-        } else {
-            return -1;
-        }
+    public int compareTo (Coordinate c) {
+        return this.max - c.max;
     }
 }
  */
