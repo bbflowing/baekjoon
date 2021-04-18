9b89691475da5121778ba5680407428a6b7e4fca
mjda611 <mjda611@naver.com>
2021-04-18 16:33:42 +0900

    version 1.0

--- src/graph/Q5558.java
+++ src/graph/Q5558.java
@@ -10,65 +10,68 @@ import java.util.StringTokenizer;
 public class Q5558 {
     /*
     public static int R, C;
-    public static char map [][];
+    public static char map[][];
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new char [R][C];
-        int startX = 0; int startY = 0;
+        int K = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+        Coordinate start = null;
         for (int i=0; i<R; ++i) {
             String line = br.readLine();
             for (int j=0; j<C; ++j) {
                 map[i][j] = line.charAt(j);
                 if (map[i][j] == 'S') {
                     map[i][j] = '.';
-                    startX = i; startY= j;
+                    start = new Coordinate (i, j, 0, 1);
                 }
             }
         }
-        int K = Integer.parseInt(st.nextToken());
-        bfs(startX, startY, K);
+        bfs(start, K);
     }
 
-    public static void bfs (int x, int y, int K) {
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
-        boolean visited [][][] = new boolean [R][C][K+1];
-        visited[x][y][1] = true;
+    public static void bfs (Coordinate start, int max) {
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        boolean visited[][][] = new boolean[max+1][R][C];
         Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(new Coordinate(x, y, 1, 0));
+        queue.add(start);
+        visited[1][start.x][start.y] = true;
 
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
-            if (map[c.x][c.y]-'0' == c.target) {
-                if (c.target == K) {
+            int dst = map[c.x][c.y]-'0';
+            if (c.target == dst) {
+                if (dst == max) {
                     System.out.println(c.distance);
                     return;
-                } else {
-                    visited[c.x][c.y][c.target+1] = true;
-                    queue.add(new Coordinate(c.x, c.y, c.target+1, c.distance));
-                    continue;
                 }
+                visited[dst+1][c.x][c.y] = true;
+                queue.add(new Coordinate(c.x, c.y, c.distance, dst+1));
+                continue;
             }
+
             for (int dir=0; dir<4; ++dir) {
                 int nx = c.x + dx[dir];
                 int ny = c.y + dy[dir];
 
-                if (nx<0 || R<=nx || ny<0 || C<=ny) {
-                    continue;
-                }
-                if (map[nx][ny] == 'X') {
-                    continue;
-                }
-                if (visited[nx][ny][c.target]) {
-                    continue;
-                }
-                visited[nx][ny][c.target] = true;
-                queue.add(new Coordinate(nx, ny, c.target, c.distance+1));
+                if (!check(nx, ny)) continue;
+                if (visited[c.target][nx][ny]) continue;
+                if (map[nx][ny] == 'X') continue;
+
+                visited[c.target][nx][ny] = true;
+                queue.add(new Coordinate(nx, ny, c.distance+1, c.target));
             }
         }
+    }
+
+    public static boolean check (int x, int y) {
+        if (x<0 || R<=x || y<0 || C<=y) {
+            return false;
+        }
+        return true;
     }
      */
 }
@@ -77,14 +80,14 @@ public class Q5558 {
 class Coordinate {
     int x;
     int y;
-    int target;
     int distance;
+    int target;
 
-    Coordinate (int x, int y, int target, int distance) {
+    Coordinate (int x, int y, int distance, int target) {
         this.x = x;
         this.y = y;
-        this.target = target;
         this.distance = distance;
+        this.target = target;
     }
 }
  */
