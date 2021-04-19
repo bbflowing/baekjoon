583406e93215fab6314f24ffa34a1f6de6ad448b
mjda611 <mjda611@naver.com>
2021-04-19 22:17:36 +0900

    version 1.0

--- src/graph/Q1981.java
+++ src/graph/Q1981.java
@@ -8,19 +8,20 @@ import java.util.*;
 public class Q1981 {
     /*
     public static int N;
-    public static int input [][];
+    public static int array[][];
+
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        input = new int [N+1][N+1];
+        array = new int[N][N];
         StringTokenizer st;
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i=1; i<=N; ++i) {
+        for (int i=0; i<N; ++i) {
             st = new StringTokenizer(br.readLine());
-            for (int j=1; j<=N; ++j) {
-                input[i][j] = Integer.parseInt(st.nextToken());
-                if (!numbers.contains(input[i][j])) {
-                    numbers.add(input[i][j]);
+            for (int j=0; j<N; ++j) {
+                array[i][j] = Integer.parseInt(st.nextToken());
+                if (!numbers.contains(array[i][j])) {
+                    numbers.add(array[i][j]);
                 }
             }
         }
@@ -29,15 +30,18 @@ public class Q1981 {
     }
 
     public static void solve (ArrayList<Integer> numbers) {
-        int maxIndex = 0;
-        int minIndex = 0;
+        int minIndex = 0; int maxIndex = 0;
         int answer = 987_654_321;
-        while (maxIndex < numbers.size() && minIndex < numbers.size()) {
-            int max = numbers.get(maxIndex);
+        while (minIndex < numbers.size() && maxIndex < numbers.size()) {
             int min = numbers.get(minIndex);
-            if (bfs (max, min)) {
-                answer = max-min<answer ? max-min : answer;
+            int max = numbers.get(maxIndex);
+            if (array[0][0] < min || max < array[0][0]) {
+                ++maxIndex;
+                continue;
+            }
+            if (bfs(min, max)) {
                 ++minIndex;
+                answer = answer > (max-min) ? max-min : answer;
             } else {
                 ++maxIndex;
             }
@@ -45,37 +49,34 @@ public class Q1981 {
         System.out.println(answer);
     }
 
-    public static boolean bfs (int max, int min) {
-        if (max < input[1][1] || input[1][1] < min) {
-            return false;
-        }
-
-        boolean visited [][] = new boolean [N+1][N+1];
+    public static boolean bfs (int min, int max) {
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
         Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(new Coordinate(1, 1));
-        visited[1][1] = true;
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
+        boolean visited[][] = new boolean[N][N];
+        visited[0][0] = true;
+        queue.add(new Coordinate(0, 0));
 
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
-            if (c.x == N && c.y == N) {
+            if (c.x == N-1 && c.y == N-1) {
                 return true;
             }
             for (int dir=0; dir<4; ++dir) {
                 int nx = c.x + dx[dir];
                 int ny = c.y + dy[dir];
 
-                if (nx<1 || N<nx || ny<1 || N<ny) {
+                if (nx<0 || N<=nx || ny<0 || N<=ny) {
                     continue;
                 }
                 if (visited[nx][ny]) {
                     continue;
                 }
-                if (min <= input[nx][ny] && input[nx][ny] <= max) {
-                    queue.add(new Coordinate(nx, ny));
-                    visited[nx][ny] = true;
+                if (array[nx][ny] < min || max < array[nx][ny]) {
+                    continue;
                 }
+                queue.add(new Coordinate(nx, ny));
+                visited[nx][ny] = true;
             }
         }
         return false;
@@ -88,7 +89,7 @@ class Coordinate {
     int x;
     int y;
 
-    Coordinate(int x, int y) {
+    Coordinate (int x, int y) {
         this.x = x;
         this.y = y;
     }
