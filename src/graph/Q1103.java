7a4977ba64d2a5eaf9fab51025900bc0adfa6241
mjda611 <mjda611@naver.com>
2021-04-20 00:27:44 +0900

    version 1.0

--- src/graph/Q1103.java
+++ src/graph/Q1103.java
@@ -10,10 +10,10 @@ public class Q1103 {
     /*
     public static int R, C;
     public static char board[][];
-    public static int dx[] = {-1, 1, 0, 0};
-    public static int dy[] = {0, 0, -1, 1};
     public static int distance[][];
     public static boolean visited[][];
+    public static int dx[] = {-1, 1, 0, 0};
+    public static int dy[] = {0, 0, -1, 1};
 
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,8 +21,8 @@ public class Q1103 {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         board = new char[R][C];
-        visited = new boolean[R][C];
         distance = new int[R][C];
+        visited = new boolean[R][C];
         for (int i=0; i<R; ++i) {
             String line = br.readLine();
             for (int j=0; j<C; ++j) {
@@ -34,8 +34,8 @@ public class Q1103 {
         System.out.println(distance[0][0]);
     }
 
-    public static int solve(int x, int y)  {
-        if (x<0 || R<=x || y<0 || C<=y || (board[x][y] == 'H')) {
+    public static int solve (int x, int y) {
+        if (x<0 || R<=x || y<0 || C<=y || board[x][y] == 'H') {
             return 0;
         }
         if (visited[x][y]) {
@@ -45,12 +45,12 @@ public class Q1103 {
         if (distance[x][y] != -1) {
             return distance[x][y];
         }
-        visited[x][y] = true;
         distance[x][y] = 0;
+        visited[x][y] = true;
         int multiplier = board[x][y] - '0';
-        for (int dir = 0; dir < 4; ++dir) {
-            int nx = x + dx[dir] * multiplier;
-            int ny = y + dy[dir] * multiplier;
+        for (int dir=0; dir<4; ++dir) {
+            int nx = x + multiplier*dx[dir];
+            int ny = y + multiplier*dy[dir];
             distance[x][y] = Math.max(distance[x][y], solve(nx, ny)+1);
         }
         visited[x][y] = false;
