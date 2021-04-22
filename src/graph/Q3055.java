13c3f9371037b68e06e64f1c5b6d770af1f6e167
mjda611 <mjda611@naver.com>
2021-04-22 17:39:36 +0900

    version 1.0

--- src/graph/Q3055.java
+++ src/graph/Q3055.java
@@ -9,95 +9,90 @@ import java.util.*;
 public class Q3055 {
     /*
     public static int R, C;
-    public static char map [][];
-    public static void main (String args[]) throws IOException {
+    public static char[][] map;
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new char [R][C];
-        Queue<Coordinate> wQ = new LinkedList<>();
-        Queue<Coordinate> hQ = new LinkedList<>();
-        boolean wVisited [][] = new boolean [R][C];
-        boolean hVisited [][] = new boolean [R][C];
-        int endX = 0; int endY = 0;
-        for (int i=0; i<R; ++i) {
-            String line = br.readLine();
-            for (int j=0; j<C; ++j) {
-                map[i][j] = line.charAt(j);
-                if (map[i][j] == '*') {
-                    wQ.add(new Coordinate(i, j));
-                    wVisited [i][j] = true;
-                } else if (map[i][j] == 'S') {
-                    hQ.add(new Coordinate(i, j));
-                    hVisited[i][j] = true;
-                } else if (map[i][j] == 'D') {
-                    endX = i; endY = j;
+        map = new char[R][C];
+        String line;
+        Queue<Coordinate> wq = new ArrayDeque<>();
+        boolean[][] water = new boolean[R][C];
+        Queue<Coordinate> hq = new ArrayDeque<>();
+        boolean[][] hedgehog = new boolean[R][C];
+        for (int r = 0; r < R; ++r) {
+            line = br.readLine();
+            for (int c = 0; c < C; ++c) {
+                map[r][c] = line.charAt(c);
+                if (map[r][c] == 'S') {
+                    hq.add(new Coordinate(r, c, 0));
+                    hedgehog[r][c] = true;
+                } else if (map[r][c] == '*') {
+                    wq.add(new Coordinate(r, c, 0));
+                    water[r][c] = true;
                 }
             }
         }
-        bfs(wQ, hQ, wVisited, hVisited, endX, endY);
+        bfs(wq, water, hq, hedgehog);
     }
 
-    public static void bfs (Queue<Coordinate> wQ, Queue<Coordinate> hQ,
-                            boolean [][] wVisited, boolean [][] hVisited, int endX, int endY) {
+    public static void bfs(Queue<Coordinate> wq, boolean[][] water,
+                           Queue<Coordinate> hq, boolean[][] hedgehog) {
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
 
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
-        int time = 0;
-        while (!hQ.isEmpty()) {
-            ++time;
-            int wSize = wQ.size();
-            for (int i=0; i<wSize; ++i) {
-                Coordinate w = wQ.poll();
-                for (int dir=0; dir<4; ++dir) {
+        int dir = 0;
+        int i = 0;
+        while (!hq.isEmpty()) {
+            int wSize = wq.size();
+            for (i = 0; i < wSize; ++i) {
+                Coordinate w = wq.poll();
+                for (dir = 0; dir < 4; ++dir) {
                     int nwx = w.x + dx[dir];
                     int nwy = w.y + dy[dir];
 
                     if (nwx < 0 || R <= nwx || nwy < 0 || C <= nwy) {
                         continue;
                     }
-                    if (map[nwx][nwy] == 'X' || map[nwx][nwy] == 'D') {
+                    if (water[nwx][nwy]) {
                         continue;
                     }
-                    if (wVisited[nwx][nwy]) {
+                    if (map[nwx][nwy] == 'D' || map[nwx][nwy] == 'X') {
                         continue;
                     }
-                    wQ.add(new Coordinate(nwx, nwy));
-                    wVisited[nwx][nwy] = true;
+                    wq.add(new Coordinate(nwx, nwy, w.time + 1));
+                    water[nwx][nwy] = true;
                 }
             }
 
-            int hSize = hQ.size();
-            for (int i=0; i<hSize; ++i) {
-                Coordinate h = hQ.poll();
-
-                for (int dir=0; dir<4; ++dir) {
+            int hSize = hq.size();
+            for (i = 0; i < hSize; ++i) {
+                Coordinate h = hq.poll();
+                if (map[h.x][h.y] == 'D') {
+                    System.out.println(h.time);
+                    return;
+                }
+                for (dir = 0; dir < 4; ++dir) {
                     int nhx = h.x + dx[dir];
                     int nhy = h.y + dy[dir];
 
-                    if (nhx<0 || R<=nhx || nhy<0 || C<=nhy) {
+                    if (nhx < 0 || R <= nhx || nhy < 0 || C <= nhy) {
                         continue;
                     }
-                    if (map[nhx][nhy] == 'X' || wVisited[nhx][nhy]) {
+                    if (hedgehog[nhx][nhy] || water[nhx][nhy]) {
                         continue;
                     }
-                    if (hVisited[nhx][nhy]) {
+                    if (map[nhx][nhy] == 'X') {
                         continue;
                     }
-                    if (nhx == endX && nhy == endY) {
-                        System.out.println(time);
-                        return;
-                    }
-                    hQ.add(new Coordinate(nhx, nhy));
-                    hVisited[nhx][nhy] = true;
+                    hq.add(new Coordinate(nhx, nhy, h.time + 1));
+                    hedgehog[nhx][nhy] = true;
                 }
             }
-            if (hQ.size() == 0) {
-                System.out.println("KAKTUS");
-                return;
-            }
         }
+        System.out.println("KAKTUS");
     }
      */
 }
@@ -106,10 +101,12 @@ public class Q3055 {
 class Coordinate {
     int x;
     int y;
+    int time;
 
-    Coordinate (int x, int y) {
+    Coordinate(int x, int y, int time) {
         this.x = x;
         this.y = y;
+        this.time = time;
     }
 }
  */
