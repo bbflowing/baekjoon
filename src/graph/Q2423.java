7f972e375a521578f240e7fa594d8cd999f51645
mjda611 <mjda611@naver.com>
2021-04-26 13:10:53 +0900

    version 1.0

--- src/graph/Q2423.java
+++ src/graph/Q2423.java
@@ -17,114 +17,111 @@ public class Q2423 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        circuit = new int[R+2][C+2];
-        for (int i=0; i<R+2; ++i) {
-            Arrays.fill(circuit[i], -1);
-        }
-        for (int r=1; r<=R; ++r) {
-            String line = br.readLine();
-            for (int c=1; c<=C; ++c) {
-                if (line.charAt(c-1) == '/') {
+        circuit = new int[R][C];
+        String line;
+        for (int r=0; r<R; ++r) {
+            line = br.readLine();
+            for (int c=0; c<C; ++c) {
+                if (line.charAt(c) == '\\') {
                     circuit[r][c] = 1;
-                } else {
-                    circuit[r][c] = 0;
                 }
             }
         }
         bfs();
     }
 
-    public static void bfs() {
-        int[][][] visited = new int[2][R+2][C+2];
+    public static void bfs () {
+        int[][][] visited = new int[2][R][C];
+        int[] zeroR = {-1, +1, 0, 0, -1, +1};
+        int[] zeroC = {0, 0, -1, +1, +1, -1};
+        int[] oneR = {-1, +1, 0, 0, -1, +1};
+        int[] oneC = {0, 0, -1, +1, -1, +1};
+
         for (int i=0; i<2; ++i) {
-            for (int r = 0; r < R + 2; ++r) {
-                Arrays.fill(visited[i][r], Integer.MAX_VALUE);
+            for (int j=0; j<R; ++j) {
+                Arrays.fill(visited[i][j], Integer.MAX_VALUE);
             }
         }
         PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        if (circuit[1][1] == 0) {
-            queue.add(new Coordinate(1, 1, 0,0));
-            visited[0][1][1] = 0;
+        if (circuit[0][0] == 1) { // no changes
+            queue.add(new Coordinate(0, 0, 1, 0));
+            visited[0][0][0] = 0;
         } else {
-            queue.add(new Coordinate(1, 1, 0,1));
-            visited[1][1][1] = 1;
+            queue.add(new Coordinate(0,0, 1, 1));
+            visited[1][0][0] = 1;
         }
-        int[] zeroR = {-1, 1, 0, 0, -1, 1};
-        int[] zeroC = {0, 0, -1, 1, -1, 1};
-        int[] oneR = {-1, 1, 0, 0, -1, 1};
-        int[] oneC = {0, 0, -1, 1, 1, -1};
 
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
-            if (c.r == R+1 && c.c == C+1) {
+            if (c.r == R && c.c == C) {
                 System.out.println(c.changes);
                 return;
             }
-            if (c.type == 0) { // \
+            if (c.type == 0) { // /
                 for (int dir=0; dir<6; ++dir) {
                     int nr = c.r + zeroR[dir];
                     int nc = c.c + zeroC[dir];
-                    if (nr == R+1 && nc == C+1) {
-                        queue.add(new Coordinate(nr, nc, 1, c.changes));
-                        continue;
-                    }
-                    if ((1<=nr && nr<=R && 1<=nc && nc<=C)) {
-                        if (dir < 4) {
-                            if (circuit[nr][nc] == 1) { // no need to change
-                                if (visited[0][nr][nc] > c.changes) {
-                                    visited[0][nr][nc] = c.changes;
-                                    queue.add(new Coordinate(nr, nc, 1, c.changes));
-                                }
-                            } else if (circuit[nr][nc] == 0) { // must change
-                                if (visited[1][nr][nc] > c.changes + 1) {
-                                    visited[1][nr][nc] = c.changes + 1;
-                                    queue.add(new Coordinate(nr, nc, 1, c.changes + 1));
-                                }
+                    if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+
+                    if (dir < 4) { // must be 1
+                        if (circuit[nr][nc] == 1) { // no need to change
+                            if (visited[0][nr][nc] > c.changes) {
+                                visited[0][nr][nc] = c.changes;
+                                queue.add(new Coordinate(nr, nc, 1, c.changes));
                             }
-                        } else {
-                            if (circuit[nr][nc] == 0) { // no need to change
-                                if (visited[0][nr][nc] > c.changes) {
-                                    visited[0][nr][nc] = c.changes;
-                                    queue.add(new Coordinate(nr, nc, 0, c.changes));
-                                }
-                            } else if (circuit[nr][nc] == 1) {
-                                if (visited[1][nr][nc] > c.changes + 1) {
-                                    visited[1][nr][nc] = c.changes + 1;
-                                    queue.add(new Coordinate(nr, nc, 0, c.changes + 1));
-                                }
+                        } else { // must change
+                            if (visited[1][nr][nc] > c.changes+1) {
+                                visited[1][nr][nc] = c.changes+1;
+                                queue.add(new Coordinate(nr, nc, 1, c.changes+1));
+                            }
+                        }
+                    } else { // must be 0
+                        if (circuit[nr][nc] == 0) { // no need to change
+                            if (visited[0][nr][nc] > c.changes) {
+                                visited[0][nr][nc] = c.changes;
+                                queue.add(new Coordinate(nr, nc, 0, c.changes));
+                            }
+                        } else { // must change
+                            if (visited[1][nr][nc] > c.changes+1) {
+                                visited[1][nr][nc] = c.changes+1;
+                                queue.add(new Coordinate(nr, nc, 0, c.changes+1));
                             }
                         }
                     }
                 }
-            } else if (c.type == 1) { // /
+            } else if (c.type == 1) { // \
                 for (int dir=0; dir<6; ++dir) {
                     int nr = c.r + oneR[dir];
                     int nc = c.c + oneC[dir];
 
-                    if ((1 <= nr && nr <= R && 1 <= nc && nc <= C) || (nr == R + 1 && nc == C + 1)) {
-                        if (dir < 4) {
-                            if (circuit[nr][nc] == 0) { // no need to change
-                                if (visited[0][nr][nc] > c.changes) {
-                                    visited[0][nr][nc] = c.changes;
-                                    queue.add(new Coordinate(nr, nc, 0, c.changes));
-                                }
-                            } else if (circuit[nr][nc] == 1) {
-                                if (visited[1][nr][nc] > c.changes + 1) {
-                                    visited[1][nr][nc] = c.changes + 1;
-                                    queue.add(new Coordinate(nr, nc, 0, c.changes + 1));
-                                }
+                    if (nr == R && nc == C) {
+                        queue.add(new Coordinate(nr, nc, 1, c.changes));
+                        continue;
+                    }
+                    if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+
+                    if (dir < 4) { // must be 0
+                        if (circuit[nr][nc] == 0) { // no need to change
+                            if (visited[0][nr][nc] > c.changes) {
+                                visited[0][nr][nc] = c.changes;
+                                queue.add(new Coordinate(nr, nc, 0, c.changes));
                             }
-                        } else {
-                            if (circuit[nr][nc] == 1) {
-                                if (visited[0][nr][nc] > c.changes) {
-                                    visited[0][nr][nc] = c.changes;
-                                    queue.add(new Coordinate(nr, nc, 1, c.changes));
-                                }
-                            } else if (circuit[nr][nc] == 0){
-                                if (visited[1][nr][nc] > c.changes + 1) {
-                                    visited[1][nr][nc] = c.changes + 1;
-                                    queue.add(new Coordinate(nr, nc, 1, c.changes + 1));
-                                }
+                        } else { // must change
+                            if (visited[1][nr][nc] > c.changes+1) {
+                                visited[1][nr][nc] = c.changes+1;
+                                queue.add(new Coordinate(nr, nc, 0, c.changes+1));
+                            }
+                        }
+                    } else { // must be 1
+                        if (circuit[nr][nc] == 1) { // no need to change
+                            if (visited[0][nr][nc] > c.changes) {
+                                visited[0][nr][nc] = c.changes;
+                                queue.add(new Coordinate(nr, nc, 1, c.changes));
+                            }
+                        } else { // must change
+                            if (visited[1][nr][nc] > c.changes+1) {
+                                visited[1][nr][nc] = c.changes+1;
+                                queue.add(new Coordinate(nr, nc, 1, c.changes+1));
                             }
                         }
                     }
@@ -137,7 +134,7 @@ public class Q2423 {
 }
 
 /*
-class Coordinate implements Comparable<Coordinate> {
+class Coordinate implements Comparable<Coordinate>{
     int r;
     int c;
     int type;
--- src/graph/Q6087.java
+++ src/graph/Q6087.java
@@ -10,43 +10,54 @@ public class Q6087 {
     public static int R, C;
     public static char[][] map;
 
-    public static void main (String args[]) throws IOException {
-
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         C = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         map = new char[R][C];
-        String line;
+        int[][] mirrors = new int[R][C];
         boolean flag = false;
-        Coordinate start = null; Coordinate end = null;
-        for (int r=0; r<R; ++r) {
+        Coordinate start = null;
+        Coordinate end = null;
+        String line;
+
+        for (int r = 0; r < R; ++r) {
             line = br.readLine();
-            for (int c=0; c<C; ++c) {
+            for (int c = 0; c < C; ++c) {
                 map[r][c] = line.charAt(c);
+                mirrors[r][c] = -1;
                 if (map[r][c] == 'C') {
                     if (!flag) {
+                        start = new Coordinate(r, c, -1, 0);
                         flag = true;
-                        start = new Coordinate(r, c);
                     } else {
-                        end = new Coordinate(r, c);
+                        end = new Coordinate(r, c, -1, 0);
                     }
                 }
             }
         }
-        solve(start, end);
+        bfs(start, end, mirrors);
     }
 
-    public static void solve (Coordinate start, Coordinate end) {
+    public static void bfs(Coordinate start, Coordinate end, int[][] mirrors) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        int[][] visited = new int[R][C];
-        for (int r=0; r<R; ++r) {
-            Arrays.fill(visited[r], Integer.MAX_VALUE);
-        }
         PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        queue.add(new Coordinate(start.r, start.c, -1, 0));
-        visited[start.r][start.c] = 0;
+        mirrors[start.r][start.c] = 0;
+        for (int dir = 0; dir < 4; ++dir) {
+            int nr = start.r + dr[dir];
+            int nc = start.c + dc[dir];
+
+            if (nr < 0 || R <= nr || nc < 0 || C <= nc) {
+                continue;
+            }
+            if (map[nr][nc] == '*') {
+                continue;
+            }
+            mirrors[nr][nc] = 0;
+            queue.add(new Coordinate(nr, nc, dir, 0));
+        }
 
         while (!queue.isEmpty()) {
             Coordinate c = queue.poll();
@@ -54,49 +65,41 @@ public class Q6087 {
                 System.out.println(c.mirrors);
                 return;
             }
-            for (int dir=0; dir<4; ++dir) {
+            for (int dir = 0; dir < 4; ++dir) {
                 int nr = c.r + dr[dir];
                 int nc = c.c + dc[dir];
 
-                if (nr<0 || R<=nr || nc<0 || C<=nc) {
+                if (nr < 0 || R <= nr || nc < 0 || C <= nc) {
                     continue;
                 }
                 if (map[nr][nc] == '*') {
                     continue;
                 }
-                if (c.direction == -1) {
-                    visited[nr][nc] = 0;
-                    queue.add(new Coordinate(nr, nc, dir, 0));
-                } else if (c.direction != dir) {
-                    if (visited[nr][nc] >= c.mirrors+1) {
-                        visited[nr][nc] = c.mirrors+1;
-                        queue.add(new Coordinate(nr, nc, dir, c.mirrors+1));
+                if (c.direction == dir) {
+                    if (mirrors[nr][nc] >= c.mirrors || mirrors[nr][nc] == -1) {
+                        mirrors[nr][nc] = c.mirrors;
+                        queue.add(new Coordinate(nr, nc, dir, c.mirrors));
                     }
                 } else {
-                    if (visited[nr][nc] >= c.mirrors) {
-                        visited[nr][nc] = c.mirrors;
-                        queue.add(new Coordinate(nr, nc, dir, c.mirrors));
+                    if (mirrors[nr][nc] >= c.mirrors + 1 || mirrors[nr][nc] == -1) {
+                        mirrors[nr][nc] = c.mirrors + 1;
+                        queue.add(new Coordinate(nr, nc, dir, c.mirrors + 1));
                     }
                 }
             }
         }
     }
-    */
+     */
 }
 
 /*
-class Coordinate implements Comparable<Coordinate>{
+class Coordinate implements Comparable<Coordinate> {
     int r;
     int c;
     int direction;
     int mirrors;
 
-    Coordinate (int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-
-    Coordinate (int r, int c, int direction, int mirrors) {
+    Coordinate(int r, int c, int direction, int mirrors) {
         this.r = r;
         this.c = c;
         this.direction = direction;
@@ -104,7 +107,7 @@ class Coordinate implements Comparable<Coordinate>{
     }
 
     @Override
-    public int compareTo (Coordinate c) {
+    public int compareTo(Coordinate c) {
         return this.mirrors - c.mirrors;
     }
 }
