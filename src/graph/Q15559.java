aba30f5bcab4d728eba9b685cee820ce1fe69f79
mjda611 <mjda611@naver.com>
2021-04-26 13:36:07 +0900

    version 1.0

--- src/graph/Q15559.java
+++ src/graph/Q15559.java
@@ -8,55 +8,56 @@ import java.util.StringTokenizer;
 public class Q15559 {
     /*
     public static int R, C, answer;
-    public static char directions[][];
-    public static int groups[][];
+    public static char[][] map;
+    public static int[][] groups;
 
-    public static void main(String args[]) throws IOException {
+    public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        directions = new char[R][C];
+        map = new char[R][C];
         groups = new int[R][C];
-        for (int i = 0; i < R; ++i) {
-            String line = br.readLine();
-            for (int j = 0; j < C; ++j) {
-                directions[i][j] = line.charAt(j);
+        String line;
+        for (int r=0; r<R; ++r) {
+            line = br.readLine();
+            for (int c=0; c<C; ++c) {
+                map[r][c] = line.charAt(c);
             }
         }
+
         int counter = 0;
-        for (int i = 0; i < R; ++i) {
-            for (int j = 0; j < C; ++j) {
-                if (groups[i][j] == 0) {
+        answer = 0;
+        for (int r=0; r<R; ++r) {
+            for (int c=0; c<C; ++c) {
+                if (groups[r][c] == 0) {
                     ++counter;
-                    solve(i, j, counter);
+                    groups[r][c] = counter;
+                    solve(r, c, counter);
                 }
             }
         }
         System.out.println(answer);
-
     }
 
-    public static void solve(int x, int y, int counter) {
-        int nx = x; int ny = y;
-        if (directions[x][y] == 'N') {
-            --nx;
-        } else if (directions[x][y] == 'S') {
-            ++nx;
-        } else if (directions[x][y] == 'E') {
-            ++ny;
-        } else {
-            --ny;
-        }
+    public static void solve (int r, int c, int counter) {
+        int nr = r;
+        int nc = c;
 
-        if (nx < 0 || R <= nx || ny < 0 || C <= ny) {
-            return;
+        if (map[r][c] == 'N') {
+            --nr;
+        } else if (map[r][c] == 'S') {
+            ++nr;
+        } else if (map[r][c] == 'E') {
+            ++nc;
+        } else {
+            --nc;
         }
-        if (groups[nx][ny] == counter) {
+        if (groups[nr][nc] == 0) {
+            groups[nr][nc] = counter;
+            solve(nr, nc, counter);
+        } else if (groups[nr][nc] == counter) {
             ++answer;
-        } else if (groups[nx][ny] == 0) {
-            groups[nx][ny] = counter;
-            solve(nx, ny, counter);
         }
     }
      */
