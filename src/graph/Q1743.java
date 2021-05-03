ee824922f56f844723ae89766f9007c2ba4b640b
mjda611 <mjda611@naver.com>
2021-05-03 14:15:28 +0900

    version 1.0

--- src/graph/Q1743.java
+++ src/graph/Q1743.java
@@ -3,65 +3,74 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q1743 {
     /*
-    public static int R, C, size;
-    public static int map [][];
-    public static boolean visited [][];
-    public static int dx [] = {-1, 1, 0, 0};
-    public static int dy [] = {0, 0, -1, 1};
+    public static int R, C;
+    public static int[][] hallway;
+    public static boolean[][] visited;
+
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        map = new int [R+1][C+1];
-        visited = new boolean [R+1][C+1];
-        for (int i=0; i<K; ++i) {
+        hallway = new int[R][C];
+        visited = new boolean[R][C];
+        for (int k=0; k<K; ++k) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            map[x][y] = 1;
+            int r = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken())-1;
+            hallway[r][c] = 1;
         }
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i=1; i<=R; ++i) {
-            for (int j=1; j<=C; ++j) {
-                if (!visited[i][j] && map[i][j] == 1) {
-                    visited[i][j] = true;
-                    size = 1;
-                    solve(i, j);
-                    result.add(size);
+        int answer = 0;
+        for (int r=0; r<R; ++r) {
+            for (int c=0; c<C; ++c) {
+                if (hallway[r][c] == 1 && !visited[r][c]) {
+                    visited[r][c] = true;
+                    answer = Math.max(answer, bfs(r, c));
                 }
             }
         }
-        result.sort(Collections.reverseOrder());
-        System.out.println(result.get(0));
+        System.out.println(answer);
     }
 
-    public static void solve (int x, int y) {
-        for (int dir=0; dir<4; ++dir) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+    public static int bfs (int r, int c) {
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        Queue<Coordinate> queue = new ArrayDeque<>();
+        queue.add(new Coordinate(r, c));
+        int size = 0;
 
-            if (nx<1 || R<nx || ny<1 || C<ny) {
-                continue;
-            }
-            if (visited[nx][ny]) {
-                continue;
-            }
-            if (map[nx][ny] == 0) {
-                continue;
-            }
-            visited[nx][ny] = true;
+        while (!queue.isEmpty()) {
+            Coordinate cur = queue.poll();
             ++size;
-            solve(nx, ny);
+            for (int dir=0; dir<4; ++dir) {
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
+
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (hallway[nr][nc] == 0) continue;
+                if (visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+                queue.add(new Coordinate(nr, nc));
+            }
         }
+        return size;
     }
      */
 }
+
+/*
+class Coordinate {
+    int r;
+    int c;
+
+    Coordinate (int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+}
+ */
