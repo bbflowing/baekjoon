89bdc61b781e8a7240136f348c16e2a97e30492e
mjda611 <mjda611@naver.com>
2021-05-06 16:43:56 +0900

    version 1.0

--- src/graph/Q5558.java
+++ src/graph/Q5558.java
@@ -3,36 +3,32 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q5558 {
     /*
     public static int R, C;
-    public static int[][] input;
+    public static int[][] board;
 
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        input = new int[R][C];
         int K = Integer.parseInt(st.nextToken());
+        board = new int[R][C];
         String line = "";
         Coordinate start = null;
-
         for (int r=0; r<R; ++r) {
             line = br.readLine();
             for (int c=0; c<C; ++c) {
                 char target = line.charAt(c);
-                if (target == 'X') {
-                    input[r][c] = -1;
+                if (target == 'S') {
+                    start = new Coordinate(1, r, c, 0);
+                } else if (target == 'X') {
+                    board[r][c] = -1;
                 } else if ('1' <= target && target <= '9') {
-                    input[r][c] = target-'0';
-                } else if (target == 'S') {
-                    start = new Coordinate(r, c, 1,0);
+                    board[r][c] = target - '0';
                 }
             }
         }
@@ -42,58 +38,51 @@ public class Q5558 {
     public static void bfs (Coordinate start, int K) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-
         Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(start);
-        boolean[][][] visited = new boolean[R][C][K+1];
-        visited[start.r][start.c][1] = true;
+        boolean[][][] visited = new boolean[K+1][R][C];
+        queue.add(new Coordinate(1, start.r, start.c, 0));
+        visited[1][start.r][start.c] = true;
 
         while (!queue.isEmpty()) {
-            Coordinate c = queue.poll();
-            if (input[c.r][c.c] == c.target) {
-                if (c.target == K) {
-                    System.out.println(c.distance);
+            Coordinate cur = queue.poll();
+            if (board[cur.r][cur.c] == cur.target) {
+                if (cur.target == K) {
+                    System.out.println(cur.times);
                     return;
                 } else {
-                    queue.add(new Coordinate(c.r, c.c, c.target+1, c.distance));
-                    visited[c.r][c.c][c.target+1] = true;
-                    continue;
+                    visited[cur.target+1][cur.r][cur.c] = true;
+                    queue.add(new Coordinate(cur.target+1, cur.r, cur.c, cur.times));
                 }
+                continue;
             }
-
             for (int dir=0; dir<4; ++dir) {
-                int nr = c.r + dr[dir];
-                int nc = c.c + dc[dir];
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
 
-                if (nr<0 || R<=nr || nc<0 || C<=nc) {
-                    continue;
-                }
-                if (input[nr][nc] == -1) {
-                    continue;
-                }
-                if (visited[nr][nc][c.target]) {
-                    continue;
-                }
-                visited[nr][nc][c.target] = true;
-                queue.add(new Coordinate(nr, nc, c.target, c.distance+1));
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (visited[cur.target][nr][nc]) continue;
+                if (board[nr][nc] == -1) continue;
+                visited[cur.target][nr][nc] = true;
+                queue.add(new Coordinate(cur.target, nr, nc, cur.times+1));
             }
         }
     }
+
      */
 }
 
 /*
 class Coordinate {
+    int target;
     int r;
     int c;
-    int target;
-    int distance;
+    int times;
 
-    Coordinate (int r, int c, int target, int distance) {
+    Coordinate (int target, int r, int c, int times) {
+        this.target = target;
         this.r = r;
         this.c = c;
-        this.target = target;
-        this.distance = distance;
+        this.times = times;
     }
 }
  */
