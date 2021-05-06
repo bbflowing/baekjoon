ca91c5abf43f2d63347b19a121dc5fc0b1995bbd
mjda611 <mjda611@naver.com>
2021-05-06 13:34:01 +0900

    version 1.0

--- src/graph/Q18224.java
+++ src/graph/Q18224.java
@@ -15,8 +15,8 @@ public class Q18224 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
         maze = new int[N][N];
+        int M = Integer.parseInt(st.nextToken());
         for (int r=0; r<N; ++r) {
             st = new StringTokenizer(br.readLine());
             for (int c=0; c<N; ++c) {
@@ -30,57 +30,62 @@ public class Q18224 {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         boolean[][][][] visited = new boolean[2][M+1][N][N];
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(new Coordinate(0, 0, 1, 0, 1));
         visited[0][1][0][0] = true;
+        Queue<Coordinate> queue = new ArrayDeque<>();
+        queue.add(new Coordinate(0, 1, 0, 0, 1));
 
         while (!queue.isEmpty()) {
-            Coordinate c = queue.poll();
-            if (c.r == N-1 && c.c == N-1) {
-                if (c.time == 0) {
-                    System.out.println(c.days+" sun");
+            Coordinate cur = queue.poll();
+            if (cur.r == N-1 && cur.c == N-1) {
+                if (cur.night == 0) {
+                    System.out.println(cur.days+" sun");
                 } else {
-                    System.out.println(c.days+" moon");
+                    System.out.println(cur.days+" moon");
                 }
                 return;
             }
+
             for (int dir=0; dir<4; ++dir) {
-                int nr = c.r + dr[dir];
-                int nc = c.c + dc[dir];
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
 
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
-                int ndistance = c.distance+1;
-                int ntime = c.time;
-                int ndays = c.days;
-                if (ndistance == M+1) {
-                    ndistance = 1;
-                    if (ntime == 0) {
-                        ntime = 1;
-                    } else {
-                        ntime = 0;
+                int nnight = cur.night;
+                int ntimes = cur.times+1;
+                int ndays = cur.days;
+
+                if (ntimes == M+1) {
+                    ntimes = 1;
+                    if (nnight == 1) {
+                        nnight = 0;
                         ++ndays;
+                    } else {
+                        nnight = 1;
                     }
                 }
                 if (maze[nr][nc] == 1) {
-                    if (c.time == 1) {
+                    if (cur.night == 1) {
+                        boolean success = false;
                         while (true) {
                             nr += dr[dir];
                             nc += dc[dir];
 
-                            if (nr < 0 || N <= nr || nc < 0 || N <= nc) break;
+                            if (nr<0 || N<=nr || nc<0 || N<=nc) break;
                             if (maze[nr][nc] == 0) {
-                                if (!visited[ntime][ndistance][nr][nc]) {
-                                    visited[ntime][ndistance][nr][nc] = true;
-                                    queue.add(new Coordinate(nr, nc, ndays, ntime, ndistance));
-                                }
+                                success = true;
                                 break;
                             }
                         }
+                        if (success) {
+                            if (visited[nnight][ntimes][nr][nc]) continue;
+                            visited[nnight][ntimes][nr][nc] = true;
+                            queue.add(new Coordinate(nnight, ntimes, nr, nc, ndays));
+                        }
                     }
                 } else {
-                    if (visited[ntime][ndistance][nr][nc]) continue;
-                    queue.add(new Coordinate(nr, nc, ndays, ntime, ndistance));
-                    visited[ntime][ndistance][nr][nc] = true;
+                    if (visited[nnight][ntimes][nr][nc]) continue;
+                    visited[nnight][ntimes][nr][nc] = true;
+                    queue.add(new Coordinate(nnight, ntimes, nr, nc, ndays));
                 }
             }
         }
@@ -91,18 +96,18 @@ public class Q18224 {
 
 /*
 class Coordinate {
+    int night;
+    int times;
     int r;
     int c;
     int days;
-    int time;
-    int distance;
 
-    Coordinate (int r, int c, int days, int time, int distance) {
+    Coordinate (int night, int times, int r, int c, int days) {
+        this.night = night;
+        this.times = times;
         this.r = r;
         this.c = c;
         this.days = days;
-        this.time = time;
-        this.distance = distance;
     }
 }
  */
