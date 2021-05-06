d3a6d82c827b12dde1b97b7ec2940264fe1f4cb4
mjda611 <mjda611@naver.com>
2021-05-06 12:06:20 +0900

    version 1.0

--- src/graph/Q9376.java
+++ src/graph/Q9376.java
@@ -9,38 +9,34 @@ import java.util.StringTokenizer;
 public class Q9376 {
     /*
     public static int R, C;
-    public static char[][] plan;
+    public static char[][] prison;
 
-    public static void main(String args[]) throws IOException {
+    public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for (int t = 0; t < T; ++t) {
+        for (int t=0; t<T; ++t) {
             st = new StringTokenizer(br.readLine());
             R = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
-            plan = new char[R + 2][C + 2];
+            prison = new char[R + 2][C + 2];
             String line = "";
-            boolean flag = false;
             Coordinate first = null;
             Coordinate second = null;
             for (int r = 0; r < R + 2; ++r) {
-                if (r == 0 || r == R + 1) {
-                    Arrays.fill(plan[r], '.');
-                    continue;
-                } else {
+                if (r == 0 || r == R + 1) Arrays.fill(prison[r], '.');
+                else {
                     line = br.readLine();
                     for (int c = 0; c < C + 2; ++c) {
                         if (c == 0 || c == C + 1) {
-                            plan[r][c] = '.';
+                            prison[r][c] = '.';
+                            continue;
                         } else {
-                            plan[r][c] = line.charAt(c - 1);
-                            if (plan[r][c] == '$') {
-                                if (!flag) {
+                            prison[r][c] = line.charAt(c - 1);
+                            if (prison[r][c] == '$') {
+                                if (first == null) {
                                     first = new Coordinate(r, c, 0);
-                                    flag = true;
                                 } else {
                                     second = new Coordinate(r, c, 0);
                                 }
@@ -50,64 +46,60 @@ public class Q9376 {
                 }
             }
             Coordinate outside = new Coordinate(0, 0, 0);
-            int[][] distance1 = bfs(first);
-            int[][] distance2 = bfs(second);
-            int[][] distance3 = bfs(outside);
+            int[][] count1 = bfs(first);
+            int[][] count2 = bfs(second);
+            int[][] count3 = bfs(outside);
+            int[][] result = new int[R+2][C+2];
+            int min = Integer.MAX_VALUE;
 
-
-            int answer = Integer.MAX_VALUE;
-            for (int r = 0; r < R+2; ++r) {
-                for (int c = 0; c < C+2; ++c) {
-                    if (distance1[r][c] == -1) {
-                        continue;
-                    } else if (plan[r][c] == '#') {
-                        answer = Math.min(answer, distance1[r][c]+distance2[r][c]+distance3[r][c]-2);
+            for (int r=0; r<R+2; ++r) {
+                for (int c=0; c<C+2; ++c) {
+                    if (count1[r][c] == -1 || count2[r][c] == -1 || count3[r][c] == -1) continue;
+                    if (prison[r][c] == '#') {
+                        result[r][c] = count1[r][c] + count2[r][c] + count3[r][c] - 2;
                     } else {
-                        answer = Math.min(answer, distance1[r][c]+distance2[r][c]+distance3[r][c]);
+                        result[r][c] = count1[r][c] + count2[r][c] + count3[r][c];
                     }
-
+                    min = Math.min(min, result[r][c]);
                 }
             }
-            bw.append(String.valueOf(answer));
+            bw.append(String.valueOf(min));
             bw.newLine();
         }
         bw.flush();
     }
 
-
-    public static int[][] bfs(Coordinate start) {
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(start);
+    public static int[][] bfs (Coordinate start) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        int[][] visited = new int[R + 2][C + 2];
-        for (int i = 0; i < R + 2; ++i) {
-            Arrays.fill(visited[i], -1);
-        }
-        visited[start.r][start.c] = 0;
+        int[][] count = new int[R+2][C+2];
+        for (int r=0; r<R+2; ++r) Arrays.fill(count[r], -1);
+        Queue<Coordinate> queue = new ArrayDeque<>();
+        queue.add(start);
+        count[start.r][start.c] = 0;
         while (!queue.isEmpty()) {
-            Coordinate c = queue.poll();
-            for (int dir = 0; dir < 4; ++dir) {
-                int nr = c.r + dr[dir];
-                int nc = c.c + dc[dir];
+            Coordinate cur = queue.poll();
+            for (int dir=0; dir<4; ++dir) {
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
 
-                if (nr < 0 || R+2 <= nr || nc < 0 || C+2 <= nc) continue;
-                if (plan[nr][nc] == '*') {
+                if (nr<0 || R+2<=nr || nc<0 || C+2<=nc) continue;
+                if (prison[nr][nc] == '*') {
                     continue;
-                } else if (plan[nr][nc] == '.' || plan[nr][nc] == '$') {
-                    if (visited[nr][nc] == -1 || visited[nr][nc] > c.count) {
-                        visited[nr][nc] = c.count;
-                        queue.add(new Coordinate(nr, nc, c.count));
+                } else if (prison[nr][nc] == '.' || prison[nr][nc] == '$') {
+                    if (count[nr][nc] == -1 || cur.doors < count[nr][nc]) {
+                        count[nr][nc] = cur.doors;
+                        queue.add(new Coordinate(nr, nc, cur.doors));
                     }
-                } else if (plan[nr][nc] == '#') {
-                    if (visited[nr][nc] == -1 || visited[nr][nc] > c.count + 1) {
-                        visited[nr][nc] = c.count + 1;
-                        queue.add(new Coordinate(nr, nc, c.count+1));
+                } else if (prison[nr][nc] == '#') {
+                    if (count[nr][nc] == -1 || cur.doors+1 < count[nr][nc]) {
+                        count[nr][nc] = cur.doors+1;
+                        queue.add(new Coordinate(nr, nc, cur.doors+1));
                     }
                 }
             }
         }
-        return visited;
+        return count;
     }
      */
 }
@@ -116,12 +108,12 @@ public class Q9376 {
 class Coordinate {
     int r;
     int c;
-    int count;
+    int doors;
 
-    Coordinate(int r, int c, int count) {
+    Coordinate (int r, int c, int doors) {
         this.r = r;
         this.c = c;
-        this.count = count;
+        this.doors = doors;
     }
 }
  */
