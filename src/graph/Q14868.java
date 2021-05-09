837a1cc41b3f7b849e4e09f2c07c6bb29411d949
mjda611 <mjda611@naver.com>
2021-05-09 12:01:16 +0900

    version2

--- src/graph/Q14868.java
+++ src/graph/Q14868.java
@@ -13,19 +13,19 @@ public class Q14868 {
     public static int N, count;
     public static int[][] map;
     public static int[] parents;
-    public static boolean[][] visited;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
-    public static Queue<Coordinate> queue, next;
+    public static Queue<Coordinate> queue;
+    public static Queue<Coordinate> next;
 
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         map = new int[N][N];
         queue = new ArrayDeque<>();
         next = new ArrayDeque<>();
-        int M = Integer.parseInt(st.nextToken());
         for (int i=1; i<=M; ++i) {
             st = new StringTokenizer(br.readLine());
             int c = Integer.parseInt(st.nextToken())-1;
@@ -36,16 +36,15 @@ public class Q14868 {
         count = M;
         parents = new int[M+1];
         Arrays.fill(parents, -1);
-        int years = 0;
-
+        int time = 0;
         while (true) {
             merge();
             if (count == 1) {
-                System.out.println(years);
+                System.out.println(time);
                 break;
             }
             spread();
-            ++years;
+            ++time;
         }
     }
 
@@ -67,6 +66,25 @@ public class Q14868 {
         }
     }
 
+    public static boolean union (int island1, int island2) {
+        int parent1 = find(island1);
+        int parent2 = find(island2);
+
+        if (parent1 == parent2) {
+            return false;
+        } else if (parent1 < parent2) {
+            parents[parent2] = parent1;
+        } else {
+            parents[parent1] = parent2;
+        }
+        return true;
+    }
+
+    public static int find (int island) {
+        if (parents[island] == -1) return island;
+        return parents[island] = find(parents[island]);
+    }
+
     public static void spread() {
         while (!next.isEmpty()) {
             Coordinate cur = next.poll();
@@ -76,32 +94,11 @@ public class Q14868 {
 
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
                 if (map[nr][nc] == 0) {
-                    queue.add(new Coordinate(nr, nc));
                     map[nr][nc] = map[cur.r][cur.c];
+                    queue.add(new Coordinate(nr, nc));
                 }
             }
         }
-    }
-
-    public static boolean union (int island1, int island2) {
-        int parent1 = find(island1);
-        int parent2 = find(island2);
-
-        if (parent1 == parent2) {
-            return false;
-        } else {
-            if (parent1 < parent2) {
-                parents[parent2] = parent1;
-            } else {
-                parents[parent1] = parent2;
-            }
-            return true;
-        }
-    }
-
-    public static int find (int island) {
-        if (parents[island] == -1) return island;
-        else return parents[island] = find(parents[island]);
     }
      */
 }
