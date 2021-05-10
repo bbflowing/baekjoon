03bdedd9b930569011f3f89e8625bfb0522ec58d
mjda611 <mjda611@naver.com>
2021-05-10 19:10:59 +0900

    version2

--- src/graph/Q5427.java
+++ src/graph/Q5427.java
@@ -1,118 +1,103 @@
 package graph;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Q5427 {
     /*
-    public static int R, C;
-    public static char map [][];
+    public static int C, R;
+    public static char[][] map;
+
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         for (int t=0; t<T; ++t) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             C = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
-            map = new char [R][C];
-            int startX = 0; int startY = 0;
-            Queue<Coordinate> fire = new LinkedList<>();
-            for (int i=0; i<R; ++i) {
-                String line = br.readLine();
-                for (int j=0; j<C; ++j) {
-                    map[i][j] = line.charAt(j);
-                    if (map[i][j] == '@') {
-                        startX = i; startY = j;
-                        map[i][j] = '.';
-                    } else if (map[i][j] == '*') {
-                        fire.add(new Coordinate(i, j));
+            map = new char[R][C];
+            String line = "";
+            boolean[][] fireV = new boolean[R][C];
+            boolean[][] personV = new boolean[R][C];
+            Queue<Coordinate> fire = new ArrayDeque<>();
+            Queue<Coordinate> person = new ArrayDeque<>();
+            for (int r=0; r<R; ++r) {
+                line = br.readLine();
+                for (int c=0; c<C; ++c) {
+                    map[r][c] = line.charAt(c);
+                    if (map[r][c] == '@') {
+                        person.add(new Coordinate(r, c, 0));
+                        personV[r][c] = true;
+                    } else if (map[r][c] == '*') {
+                        fire.add(new Coordinate(r, c, 0));
+                        fireV[r][c] = true;
                     }
                 }
             }
-            if (!bfs(startX, startY, fire)) {
-                System.out.println("IMPOSSIBLE");
+            int answer = bfs(fire, person, fireV, personV);
+            if (answer == -1) {
+                bw.append("IMPOSSIBLE");
+            } else {
+                bw.append(String.valueOf(answer));
             }
+            bw.newLine();
         }
+        bw.flush();
     }
 
-    public static boolean bfs (int x, int y, Queue<Coordinate> fire) {
-        boolean svisited [][] = new boolean [R][C];
-        boolean fvisited [][] = new boolean [R][C];
-        Queue<Coordinate> queue = new LinkedList<>();
-        svisited[x][y] = true;
-        queue.add(new Coordinate(x, y, 0));
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
+    public static int bfs (Queue<Coordinate> fire, Queue<Coordinate> person,
+                            boolean[][] fireV, boolean[][] personV) {
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
 
-        while (!queue.isEmpty()) {
-            int fsize = fire.size();
-            for (int i=0; i<fsize; ++i) {
+        while (!person.isEmpty()) {
+            int fireS = fire.size();
+            for (int i = 0; i < fireS; ++i) {
                 Coordinate f = fire.poll();
-                fvisited[f.x][f.y] = true;
-                for (int j=0; j<4; ++j) {
-                    int nfx = f.x + dx[j];
-                    int nfy = f.y + dy[j];
+                for (int dir = 0; dir < 4; ++dir) {
+                    int nfr = f.r + dr[dir];
+                    int nfc = f.c + dc[dir];
 
-                    if (nfx<0 || R<=nfx || nfy<0 || C<=nfy) {
-                        continue;
-                    }
-                    if (map[nfx][nfy] == '#' || map[nfx][nfy] == '*') {
-                        continue;
-                    }
-                    if (fvisited[nfx][nfy]) {
-                        continue;
-                    }
-                    fvisited[nfx][nfy] = true;
-                    fire.add(new Coordinate(nfx, nfy));
+                    if (nfr < 0 || R <= nfr || nfc < 0 || C <= nfc) continue;
+                    if (fireV[nfr][nfc]) continue;
+                    if (map[nfr][nfc] == '#') continue;
+                    fireV[nfr][nfc] = true;
+                    fire.add(new Coordinate(nfr, nfc, f.time + 1));
                 }
             }
-            int ssize = queue.size();
-            for (int i=0; i<ssize; ++i) {
-                Coordinate s = queue.poll();
-                for (int j=0; j<4; ++j) {
-                    int nsx = s.x + dx[j];
-                    int nsy = s.y + dy[j];
 
-                    if (nsx<0 || R<=nsx || nsy<0 || C<=nsy) {
-                        System.out.println(s.times+1);
-                        return true;
+            int personS = person.size();
+            for (int i = 0; i < personS; ++i) {
+                Coordinate p = person.poll();
+                for (int dir = 0; dir < 4; ++dir) {
+                    int npr = p.r + dr[dir];
+                    int npc = p.c + dc[dir];
+
+                    if (npr < 0 || R <= npr || npc < 0 || C <= npc) {
+                        return (p.time + 1);
                     }
-                    if (map[nsx][nsy] == '#' || map[nsx][nsy] == '*' || fvisited[nsx][nsy]) {
-                        continue;
-                    }
-                    if (svisited[nsx][nsy]) {
-                        continue;
-                    }
-                    svisited[nsx][nsy] = true;
-                    queue.add(new Coordinate(nsx, nsy, s.times+1));
+                    if (fireV[npr][npc] || personV[npr][npc]) continue;
+                    if (map[npr][npc] == '#') continue;
+                    personV[npr][npc] = true;
+                    person.add(new Coordinate(npr, npc, p.time + 1));
                 }
             }
         }
-        return false;
+        return -1;
     }
      */
 }
 
 /*
 class Coordinate {
-    int x;
-    int y;
-    int times;
+    int r, c, time;
 
-    Coordinate (int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    Coordinate (int x, int y, int times) {
-        this.x = x;
-        this.y = y;
-        this.times = times;
+    Coordinate (int r, int c, int time) {
+        this.r = r;
+        this.c = c;
+        this.time = time;
     }
 }
  */
