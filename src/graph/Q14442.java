f33df7387848d6c8c02cc756b3d91b9ad36576d2
mjda611 <mjda611@naver.com>
2021-05-10 19:24:08 +0900

    version2

--- src/graph/Q14442.java
+++ src/graph/Q14442.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Q14442 {
     /*
     public static int R, C;
-    public static int map[][];
+    public static int[][] map;
 
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,48 +16,47 @@ public class Q14442 {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        map = new int[R+1][C+1];
-        for (int i=1; i<=R; ++i) {
-            String line = br.readLine();
-            for (int j=1; j<=C; ++j) {
-                map[i][j] = line.charAt(j-1) - '0';
+        map = new int[R][C];
+        String line = "";
+        for (int r=0; r<R; ++r) {
+            line = br.readLine();
+            for (int c=0; c<C; ++c) {
+                map[r][c] = line.charAt(c)-'0';
             }
         }
-        bfs (K);
+        bfs(K);
     }
 
     public static void bfs (int K) {
-        boolean visited[][][] = new boolean[K+1][R+1][C+1];
+        boolean[][][] visited = new boolean[R][C][K+1];
+        visited[0][0][0] = true;
         Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(new Coordinate(1, 1, 0, 1));
-        visited[0][1][1] = true;
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
+        queue.add(new Coordinate(0, 0, 0, 1));
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
 
         while (!queue.isEmpty()) {
-            Coordinate c = queue.poll();
-            if (c.x == R && c.y == C) {
-                System.out.println(c.distance);
+            Coordinate cur = queue.poll();
+            if (cur.r == R-1 && cur.c == C-1) {
+                System.out.println(cur.distance);
                 return;
             }
             for (int dir=0; dir<4; ++dir) {
-                int nx = c.x + dx[dir];
-                int ny = c.y + dy[dir];
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
 
-                if (nx<1 || R<nx || ny<1 || C<ny) {
-                    continue;
-                }
-                if (map[nx][ny] == 1) {
-                    if (c.broken <= K-1) {
-                        if (!visited[c.broken+1][nx][ny]) {
-                            visited[c.broken+1][nx][ny] = true;
-                            queue.add(new Coordinate(nx, ny, c.broken+1, c.distance+1));
-                        }
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (map[nr][nc] == 0) {
+                    if (!visited[nr][nc][cur.broken]) {
+                        visited[nr][nc][cur.broken] = true;
+                        queue.add(new Coordinate(nr, nc, cur.broken, cur.distance+1));
                     }
                 } else {
-                    if (!visited[c.broken][nx][ny]) {
-                        visited[c.broken][nx][ny] = true;
-                        queue.add(new Coordinate(nx, ny, c.broken, c.distance+1));
+                    if (cur.broken+1 <= K) {
+                        if (!visited[nr][nc][cur.broken+1]) {
+                            visited[nr][nc][cur.broken+1] = true;
+                            queue.add(new Coordinate(nr, nc, cur.broken+1, cur.distance+1));
+                        }
                     }
                 }
             }
@@ -69,14 +68,11 @@ public class Q14442 {
 
 /*
 class Coordinate {
-    int x;
-    int y;
-    int broken;
-    int distance;
+    int r, c, broken, distance;
 
-    Coordinate (int x, int y, int broken, int distance) {
-        this.x = x;
-        this.y = y;
+    Coordinate (int r, int c, int broken, int distance) {
+        this.r = r;
+        this.c = c;
         this.broken = broken;
         this.distance = distance;
     }
