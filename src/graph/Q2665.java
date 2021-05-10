44d47fb755c1f96ab739d53d7044376570712ab6
mjda611 <mjda611@naver.com>
2021-05-10 17:29:11 +0900

    version2

--- src/graph/Q2665.java
+++ src/graph/Q2665.java
@@ -15,22 +15,23 @@ public class Q2665 {
         N = Integer.parseInt(br.readLine());
         rooms = new int[N][N];
         String line = "";
-        int[][] visited = new int[N][N];
+        int[][] dp = new int[N][N];
         for (int r=0; r<N; ++r) {
-            line =br.readLine();
+            line = br.readLine();
             for (int c=0; c<N; ++c) {
                 rooms[r][c] = line.charAt(c)-'0';
-                visited[r][c] = -1;
+                dp[r][c] = -1;
             }
         }
-        bfs(visited);
+        bfs(dp);
     }
-    public static void bfs(int[][] visited) {
+
+    public static void bfs(int[][] dp) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         PriorityQueue<Coordinate> queue = new PriorityQueue<>();
         queue.add(new Coordinate(0, 0, 0));
-        visited[0][0] = 0;
+        dp[0][0] = 0;
 
         while (!queue.isEmpty()) {
             Coordinate cur = queue.poll();
@@ -41,15 +42,18 @@ public class Q2665 {
             for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-                int nchanges = cur.changes;
 
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
                 if (rooms[nr][nc] == 0) {
-                    ++nchanges;
-                }
-                if (visited[nr][nc] == -1 || nchanges < visited[nr][nc]) {
-                    visited[nr][nc] = nchanges;
-                    queue.add(new Coordinate(nr, nc, nchanges));
+                    if (dp[nr][nc] == -1 || cur.changes+1 < dp[nr][nc]) {
+                        dp[nr][nc] = cur.changes+1;
+                        queue.add(new Coordinate(nr, nc, cur.changes+1));
+                    }
+                } else {
+                    if (dp[nr][nc] == -1 || cur.changes < dp[nr][nc]) {
+                        dp[nr][nc] = cur.changes;
+                        queue.add(new Coordinate(nr, nc, cur.changes));
+                    }
                 }
             }
         }
@@ -59,9 +63,7 @@ public class Q2665 {
 
 /*
 class Coordinate implements Comparable<Coordinate> {
-    int r;
-    int c;
-    int changes;
+    int r, c, changes;
 
     Coordinate (int r, int c, int changes) {
         this.r = r;
