92963e20c28c4417ccc047730b088d9040d5ad92
mjda611 <mjda611@naver.com>
2021-05-12 12:08:32 +0900

    version2

--- src/graph/Q3197.java
+++ src/graph/Q3197.java
@@ -4,19 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q3197 {
     /*
-    public static int R, C;
-    public static int[][] map;
-    public static Queue<Coordinate> queue;
-    public static Queue<Coordinate> water;
-    public static boolean[][] visited;
+    public static int R, C, waters;
+    public static int[][] lake;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
+    public static Queue<Coordinate> queue;
+    public static Queue<Coordinate> next;
     public static int[] parents;
 
     public static void main(String args[]) throws IOException {
@@ -24,117 +22,105 @@ public class Q3197 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
+        lake = new int[R][C];
         String line = "";
         Coordinate start = null;
         Coordinate dst = null;
-        for (int r=0; r<R; ++r) {
+
+        for (int r = 0; r < R; ++r) {
             line = br.readLine();
-            for (int c=0; c<C; ++c) {
+            for (int c = 0; c < C; ++c) {
                 char target = line.charAt(c);
-                if (target == '.') {
-                    map[r][c] = -1;
-                } else if (target == 'L') {
-                    map[r][c] = -1;
-                    if (start == null) {
-                        start = new Coordinate (r, c, 0);
-                    } else {
-                        dst = new Coordinate(r, c, 0);
-                    }
-                }
+                if (target == 'L') {
+                    if (start == null) start = new Coordinate(r, c, 0);
+                    else dst = new Coordinate(r, c, 0);
+                    lake[r][c] = -1;
+                } else if (target == '.') lake[r][c] = -1;
             }
         }
+
         int counter = 0;
         queue = new ArrayDeque<>();
-        water = new ArrayDeque<>();
-        for (int r=0; r<R; ++r) {
-            for (int c=0; c<C; ++c) {
-                if (map[r][c] == -1) {
+        for (int r = 0; r < R; ++r) {
+            for (int c = 0; c < C; ++c) {
+                if (lake[r][c] == -1) {
                     ++counter;
                     dfs(r, c, counter);
                 }
             }
         }
-        start.number = map[start.r][start.c];
-        dst.number = map[dst.r][dst.c];
-        parents = new int[counter+1];
-        for (int i=1; i<counter+1; ++i) {
-            parents[i] = i;
-        }
+
+        int begin = lake[start.r][start.c];
+        int end = lake[dst.r][dst.c];
+        waters = counter;
+        parents = new int[counter + 1];
+        for (int i = 1; i < counter + 1; ++i) parents[i] = i;
         int time = 0;
+        next = new ArrayDeque<>();
         while (true) {
             merge();
-            int parent1 = find(start.number);
-            int parent2 = find(dst.number);
-            if (parent1 == parent2 ||
-                parent1 == dst.number ||
-                parent2 == start.number) break;
-            melt();
+            if (find(begin) == find(end)) {
+                System.out.println(time);
+                break;
+            }
+            spread();
             ++time;
         }
-        System.out.println(time);
+    }
+
+    public static int find(int w) {
+        if (parents[w] == w) return w;
+        return parents[w] = find(parents[w]);
+    }
+
+    public static boolean union(int w1, int w2) {
+        int p1 = find(w1);
+        int p2 = find(w2);
+        if (p1 == p2) return false;
+        else if (p1 < p2) parents[p2] = p1;
+        else parents[p1] = p2;
+        return true;
     }
 
     public static void merge() {
         while (!queue.isEmpty()) {
             Coordinate cur = queue.poll();
-            water.add(cur);
-            for (int dir=0; dir<4; ++dir) {
+            next.add(cur);
+            for (int dir = 0; dir < 4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
-                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
-                if (map[nr][nc] == cur.number) continue;
-                if (map[nr][nc] != 0) {
-                    union(map[cur.r][cur.c], map[nr][nc]);
+                if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
+                if (lake[nr][nc] != 0) {
+                    if (union(cur.number, lake[nr][nc])) --waters;
                 }
             }
         }
     }
 
-    public static void melt() {
-        int size = water.size();
-        for (int i=0; i<size; ++i) {
-            Coordinate cur = water.poll();
-            for (int dir=0; dir<4; ++dir) {
+    public static void spread() {
+        while (!next.isEmpty()) {
+            Coordinate cur = next.poll();
+            for (int dir = 0; dir < 4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
-                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
-                if (map[nr][nc] == 0) {
-                    map[nr][nc] = map[cur.r][cur.c];
-                    queue.add(new Coordinate(nr, nc, map[cur.r][cur.c]));
+                if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
+                if (lake[nr][nc] == 0) {
+                    lake[nr][nc] = cur.number;
+                    queue.add(new Coordinate(nr, nc, cur.number));
                 }
             }
         }
     }
 
-    public static int find (int island) {
-        if (parents[island] == island) return island;
-        return parents[island] = find(parents[island]);
-    }
-
-    public static void union (int island1, int island2) {
-        int parent1 = find(island1);
-        int parent2 = find(island2);
-
-        if (parent1 == parent2) {
-            return;
-        } else if (parent1 > parent2) {
-            parents[parent1] = parent2;
-        } else {
-            parents[parent2] = parent1;
-        }
-    }
-
-    public static void dfs (int r, int c, int counter) {
-        map[r][c] = counter;
+    public static void dfs(int r, int c, int counter) {
+        lake[r][c] = counter;
         queue.add(new Coordinate(r, c, counter));
-        for (int dir=0; dir<4; ++dir) {
+        for (int dir = 0; dir < 4; ++dir) {
             int nr = r + dr[dir];
             int nc = c + dc[dir];
-            if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
-            if (map[nr][nc] == -1) dfs(nr, nc, counter);
+            if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
+            if (lake[nr][nc] != -1) continue;
+            dfs(nr, nc, counter);
         }
     }
      */
