72e798fc752f3d6b03034b77abc28da65908871d
mjda611 <mjda611@naver.com>
2021-05-12 15:39:08 +0900

    version2

--- src/graph/Q17090.java
+++ src/graph/Q17090.java
@@ -12,7 +12,7 @@ public class Q17090 {
     /*
     public static int R, C;
     public static char[][] maze;
-    public static int[][] escape;
+    public static int[][] dp;
 
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,49 +20,36 @@ public class Q17090 {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         maze = new char[R][C];
-        escape = new int[R][C];
+        dp = new int[R][C];
         String line = "";
         for (int r=0; r<R; ++r) {
             line = br.readLine();
             for (int c=0; c<C; ++c) {
                 maze[r][c] = line.charAt(c);
-                escape[r][c] = -1;
+                dp[r][c] = -1;
             }
         }
         int answer = 0;
         for (int r=0; r<R; ++r) {
             for (int c=0; c<C; ++c) {
-                if (escape[r][c] == -1) {
-                    answer += solve(r, c);
-                } else if (escape[r][c] == 1) {
-                    ++answer;
-                }
+                if (dp[r][c] == 1) ++answer;
+                else if (dp[r][c] == -1) answer += solve(r, c);
             }
         }
         System.out.println(answer);
     }
 
     public static int solve (int r, int c) {
-        if (r<0 || R<=r || c<0 || C<=c) {
-            return 1;
-        }
-        if (escape[r][c] != -1) {
-            return escape[r][c];
-        }
-        escape[r][c] = 0;
-        int nr = r;
-        int nc = c;
-        if (maze[r][c] == 'U') {
-            --nr;
-        } else if (maze[r][c] == 'R') {
-            ++nc;
-        } else if (maze[r][c] == 'D') {
-            ++nr;
-        } else {
-            --nc;
-        }
-        escape[r][c] = solve(nr, nc);
-        return escape[r][c];
+        if (r<0 || R<=r || c<0 || C<=c) return 1;
+        if (dp[r][c] != -1) return dp[r][c];
+        dp[r][c] = 0;
+        int nr = r; int nc = c;
+        if (maze[r][c] == 'D') ++nr;
+        else if (maze[r][c] == 'U') --nr;
+        else if (maze[r][c] == 'L') --nc;
+        else ++nc;
+        dp[r][c] = solve(nr, nc);
+        return dp[r][c];
     }
      */
 }
