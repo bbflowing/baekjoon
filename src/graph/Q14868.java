6a9cb43f4dac41fdd53b49a4e4c46de53fa0ae81
mjda611 <mjda611@naver.com>
2021-05-15 14:08:20 +0900

    version2

--- src/graph/Q14868.java
+++ src/graph/Q14868.java
@@ -4,28 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q14868 {
     /*
-    public static int N, civilizations;
+    public static int N, count;
     public static int[][] map;
     public static Queue<Coordinate> queue;
     public static Queue<Coordinate> next;
-    public static int[] parents;
     public static int[] dr = {-1, 1, 0, 0};
-    public static int[] dc = {0, 0, -1 ,1};
+    public static int[] dc = {0, 0, -1, 1};
+    public static int[] parents;
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         map = new int[N][N];
         queue = new ArrayDeque<>();
         next = new ArrayDeque<>();
-        int M = Integer.parseInt(st.nextToken());
         for (int i=1; i<=M; ++i) {
             st = new StringTokenizer(br.readLine());
             int c = Integer.parseInt(st.nextToken())-1;
@@ -33,50 +32,20 @@ public class Q14868 {
             map[r][c] = i;
             queue.add(new Coordinate(r, c, i));
         }
-        civilizations = M;
+        count = M;
         parents = new int[M+1];
         for (int i=1; i<=M; ++i) parents[i] = i;
         int time = 0;
+
         while (true) {
             merge();
-            if (civilizations == 1) {
+            if (count == 1) {
                 System.out.println(time);
                 break;
             }
             spread();
             ++time;
         }
-    }
-
-    public static void spread() {
-        while (!next.isEmpty()) {
-            Coordinate cur = next.poll();
-            for (int dir=0; dir<4; ++dir) {
-                int nr = cur.r + dr[dir];
-                int nc = cur.c + dc[dir];
-
-                if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
-                if (map[nr][nc] == 0) {
-                    map[nr][nc] = cur.number;
-                    queue.add(new Coordinate(nr, nc, cur.number));
-                }
-            }
-        }
-    }
-
-    public static int find (int c) {
-        if (parents[c] == c) return c;
-        else return parents[c] = find(parents[c]);
-    }
-
-    public static boolean union (int c1, int c2) {
-        int p1 = find(c1);
-        int p2 = find(c2);
-
-        if (p1 == p2) return false;
-        else if (p1 < p2) parents[p2] = p1;
-        else parents[p1] = p2;
-        return true;
     }
 
     public static void merge() {
@@ -89,22 +58,53 @@ public class Q14868 {
 
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
                 if (map[nr][nc] != 0) {
-                    if (union(map[cur.r][cur.c], map[nr][nc])) --civilizations;
+                    if (union(cur.civilization, map[nr][nc])) --count;
                 }
             }
         }
+    }
+
+    public static void spread() {
+        while (!next.isEmpty()) {
+            Coordinate cur = next.poll();
+            for (int dir = 0; dir < 4; ++dir) {
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
+
+                if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
+                if (map[nr][nc] == 0) {
+                    map[nr][nc] = cur.civilization;
+                    queue.add(new Coordinate(nr, nc, cur.civilization));
+                }
+            }
+        }
+    }
+
+    public static int find(int island) {
+        if (parents[island] == island) return island;
+        else return parents[island] = find(parents[island]);
+    }
+
+    public static boolean union(int island1, int island2) {
+        int parent1 = find(island1);
+        int parent2 = find(island2);
+
+        if (parent1 == parent2) return false;
+        else if (parent1 < parent2) parents[parent2] = parent1;
+        else parents[parent1] = parent2;
+        return true;
     }
      */
 }
 
 /*
 class Coordinate {
-    int r, c, number;
+    int r, c, civilization;
 
-    Coordinate (int r, int c, int number) {
+    Coordinate (int r, int c, int civilization) {
         this.r = r;
         this.c = c;
-        this.number = number;
+        this.civilization = civilization;
     }
 }
  */
