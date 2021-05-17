aa9fb5ceb012eda1e65d5074f880dbf1244cd6f3
mjda611 <mjda611@naver.com>
2021-05-17 13:33:34 +0900

    version2

--- src/dijkstra/Q1261.java
+++ src/dijkstra/Q1261.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+// 알고스팟
+
 public class Q1261 {
     /*
     public static int R, C;
@@ -18,21 +20,21 @@ public class Q1261 {
         C = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         maze = new int[R][C];
-        int[][] dp = new int[R][C];
+        int[][] visited = new int[R][C];
         String line = "";
         for (int r=0; r<R; ++r) {
             line = br.readLine();
-            Arrays.fill(dp[r], -1);
+            Arrays.fill(visited[r], -1);
             for (int c=0; c<C; ++c) maze[r][c] = line.charAt(c)-'0';
         }
-        solve(dp);
+        dijkstra(visited);
     }
 
-    public static void solve(int[][] dp) {
+    public static void dijkstra(int[][] visited) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        dp[0][0] = 0;
+        visited[0][0] = 0;
         queue.add(new Coordinate(0, 0, 0));
 
         while (!queue.isEmpty()) {
@@ -44,16 +46,17 @@ public class Q1261 {
             for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
+
                 if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
-                if (maze[nr][nc] == 0) {
-                    if (dp[nr][nc] == -1 || cur.broken < dp[nr][nc]) {
-                        dp[nr][nc] = cur.broken;
-                        queue.add(new Coordinate(nr, nc, cur.broken));
+                if (maze[nr][nc] == 1) {
+                    if (visited[nr][nc] == -1 || cur.broken+1 < visited[nr][nc]) {
+                        visited[nr][nc] = cur.broken+1;
+                        queue.add(new Coordinate(nr, nc, cur.broken+1));
                     }
                 } else {
-                    if (dp[nr][nc] == -1 || cur.broken+1 < dp[nr][nc]) {
-                        dp[nr][nc] = cur.broken+1;
-                        queue.add(new Coordinate(nr, nc, cur.broken+1));
+                    if (visited[nr][nc] == -1 || cur.broken < visited[nr][nc]) {
+                        visited[nr][nc] = cur.broken;
+                        queue.add(new Coordinate(nr, nc, cur.broken));
                     }
                 }
             }
@@ -66,14 +69,15 @@ public class Q1261 {
 class Coordinate implements Comparable<Coordinate> {
     int r, c, broken;
 
-    Coordinate(int r, int c, int broken) {
+    Coordinate (int r, int c, int broken) {
         this.r = r;
         this.c = c;
         this.broken = broken;
     }
 
+    @Override
     public int compareTo(Coordinate c) {
-        return this.broken - c.broken;
+        return this.broken-c.broken;
     }
 }
  */
