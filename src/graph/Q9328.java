a8451fe33a39e94fd2448d68ddfa7a2dfbca5bc0
mjda611 <mjda611@naver.com>
2021-05-19 18:51:23 +0900

    version2

--- src/graph/Q9328.java
+++ src/graph/Q9328.java
@@ -6,38 +6,34 @@ import java.util.*;
 public class Q9328 {
     /*
     public static int R, C;
-    public static char[][] plan;
+    public static char[][] map;
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for (int t=0; t<T; ++t) {
+        for (int t = 0; t < T; ++t) {
             st = new StringTokenizer(br.readLine());
             R = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
-            plan = new char[R + 2][C + 2];
+            map = new char[R + 2][C + 2];
             String line = "";
             for (int r = 0; r < R + 2; ++r) {
-                if (r==0 || r==R+1) {
-                    Arrays.fill(plan[r], '.');
+                if (r == 0 || r == R + 1) {
+                    Arrays.fill(map[r], '.');
                     continue;
-                } else {
-                    line = br.readLine();
-                    plan[r][0] = '.';
-                    plan[r][C+1] = '.';
-                    for (int c=1; c<=C; ++c) {
-                        plan[r][c] = line.charAt(c-1);
-                    }
+                }
+                line = br.readLine();
+                for (int c = 0; c < C + 2; ++c) {
+                    if (c == 0 || c == C + 1) map[r][c] = '.';
+                    else map[r][c] = line.charAt(c - 1);
                 }
             }
+            line = br.readLine();
             boolean[] keys = new boolean[26];
-            String key = br.readLine();
-            if (!key.equals("0")) {
-                for (int i = 0; i < key.length(); ++i) {
-                    keys[key.charAt(i) - 'a'] = true;
-                }
+            if (!line.equals("0")) {
+                for (int i = 0; i < line.length(); ++i) keys[line.charAt(i) - 'a'] = true;
             }
             bw.append(String.valueOf(bfs(keys)));
             bw.newLine();
@@ -45,66 +41,71 @@ public class Q9328 {
         bw.flush();
     }
 
-    public static int bfs (boolean[] keys) {
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        Queue<Coordinate> yet = new ArrayDeque<>();
-        queue.add(new Coordinate(0, 0));
-        boolean[][] visited = new boolean[R+2][C+2];
-        visited[0][0] = true;
+    public static int bfs(boolean[] keys) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        int count = 0;
+        Queue<Coordinate> queue = new ArrayDeque<>();
+        Queue<Coordinate> locked = new ArrayDeque<>();
+        boolean[][] visited = new boolean[R + 2][C + 2];
+        visited[0][0] = true;
+        queue.add(new Coordinate(0, 0));
+        int max = 0;
 
         while (!queue.isEmpty()) {
             Coordinate cur = queue.poll();
-            for (int dir=0; dir<4; ++dir) {
+            for (int dir = 0; dir < 4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
 
-                if (nr<0 || R+2<=nr || nc<0 || C+2<=nc) continue;
+                if (nr < 0 || R + 2 <= nr || nc < 0 || C + 2 <= nc) continue;
                 if (visited[nr][nc]) continue;
-                if (plan[nr][nc] == '*') {
+                if (map[nr][nc] == '*') {
                     continue;
-                } else if (plan[nr][nc] == '$') {
-                    ++count;
-                } else if ('a' <= plan[nr][nc] && plan[nr][nc] <= 'z') {
-                    if (!keys[plan[nr][nc]-'a']) {
-                        keys[plan[nr][nc]-'a'] = true;
-                        int size = yet.size();
-                        for (int i=0; i<size; ++i) {
-                            Coordinate test = yet.poll();
-                            if (plan[test.r][test.c]-'A' == plan[nr][nc]-'a') {
-                                visited[test.r][test.c] = true;
-                                queue.add(new Coordinate(test.r, test.c));
+                } else if (map[nr][nc] == '$') {
+                    ++max;
+                    visited[nr][nc] = true;
+                    queue.add(new Coordinate(nr, nc));
+                } else if ('A' <= map[nr][nc] && map[nr][nc] <= 'Z') {
+                    if (keys[map[nr][nc] - 'A']) {
+                        visited[nr][nc] = true;
+                        queue.add(new Coordinate(nr, nc));
+                    } else {
+                        locked.add(new Coordinate(nr, nc));
+                    }
+                } else if ('a' <= map[nr][nc] && map[nr][nc] <= 'z') {
+                    if (!keys[map[nr][nc]-'a']) {
+                        keys[map[nr][nc] - 'a'] = true;
+                        visited[nr][nc] = true;
+                        queue.add(new Coordinate(nr, nc));
+                        int size = locked.size();
+                        for (int i = 0; i < size; ++i) {
+                            Coordinate c = locked.poll();
+                            if (map[c.r][c.c]-'A' == map[nr][nc]-'a') {
+                                visited[c.r][c.c] = true;
+                                queue.add(new Coordinate(c.r, c.c));
                             } else {
-                                yet.add(new Coordinate(test.r, test.c));
+                                locked.add(new Coordinate(c.r, c.c));
                             }
                         }
                     }
-                } else if ('A' <= plan[nr][nc] && plan[nr][nc] <= 'Z') {
-                    if (!keys[plan[nr][nc]-'A']) {
-                        yet.add(new Coordinate(nr, nc));
-                        continue;
-                    }
+                } else if (map[nr][nc] == '.') {
+                    visited[nr][nc] = true;
+                    queue.add(new Coordinate(nr, nc));
                 }
-                visited[nr][nc] = true;
-                queue.add(new Coordinate(nr, nc));
             }
         }
-        return count;
+        return max;
     }
      */
 }
 
 /*
 class Coordinate {
-    int r;
-    int c;
+    int r, c;
 
-    Coordinate (int r, int c) {
+    Coordinate(int r, int c) {
         this.r = r;
-        this.c =c;
+        this.c = c;
     }
 }
-
  */
