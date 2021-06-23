b346efd412103a15167459a92ef832aded6a3403
mjda611 <mjda611@naver.com>
2021-06-23 16:16:11 +0900

    version2

--- src/graph/Q16952.java
+++ src/graph/Q16952.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -15,135 +16,114 @@ public class Q16952 {
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
         board = new int[N][N];
-        StringTokenizer st;
-        int startR = 0;
-        int startC = 0;
-        for (int r = 0; r < N; ++r) {
+        int startR = 0; int startC = 0;
+        for (int r=0; r<N; ++r) {
             st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < N; ++c) {
+            for (int c=0; c<N; ++c) {
                 board[r][c] = Integer.parseInt(st.nextToken());
                 if (board[r][c] == 1) {
-                    startR = r;
-                    startC = c;
+                    startR = r; startC = c;
                 }
             }
         }
-        bfs(startR, startC);
+        solve(startR, startC);
     }
 
-    public static void bfs(int r, int c) {
-        PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        int max = (int) Math.pow(N, 2);
-        boolean[][][][] visited = new boolean[3][N][N][max + 1];
-
+    public static void solve(int r, int c) {
         int[] knightR = {-1, -2, -2, -1, +1, +2, +2, +1};
         int[] knightC = {-2, -1, +1, +2, -2, -1, +1, +2};
         int[] bishopR = {-1, -1, +1, +1};
         int[] bishopC = {-1, +1, -1, +1};
         int[] rookR = {-1, 0, 0, +1};
         int[] rookC = {0, -1, +1, 0};
-
-        for (int type = 0; type < 3; ++type) {
-            queue.add(new Coordinate(type, r, c, 0, 2, 0));
-            visited[type][r][c][2] = true;
+        int max = (int) Math.pow(N, 2);
+        PriorityQueue<Square> queue = new PriorityQueue<>();
+        boolean[][][][] visited = new boolean[3][max+1][N][N];
+        for (int piece=0; piece<3; ++piece) {
+            visited[piece][2][r][c] = true;
+            queue.add(new Square(r, c, 2, piece, 0, 0));
         }
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            if (cur.target == board[cur.r][cur.c]) {
-                if (cur.target == max) {
-                    System.out.println(cur.times + " " + cur.changes);
+            Square s = queue.poll();
+            if (s.goal == board[s.r][s.c]) {
+                if (s.goal == max) {
+                    System.out.println(s.time+" "+s.changes);
                     return;
                 } else {
-                    queue.add(new Coordinate(cur.type, cur.r, cur.c, cur.changes, cur.target + 1, cur.times));
-                    visited[cur.type][cur.r][cur.c][cur.target + 1] = true;
+                    queue.add(new Square(s.r, s.c, s.goal+1, s.piece, s.time, s.changes));
                     continue;
                 }
             } else {
-                for (int type = 0; type < 3; ++type) {
-                    if (cur.type != type) {
-                        if (!visited[type][cur.r][cur.c][cur.target]) {
-                            visited[type][cur.r][cur.c][cur.target] = true;
-                            queue.add(new Coordinate(type, cur.r, cur.c, cur.changes + 1, cur.target, cur.times + 1));
+                for (int piece=0; piece<3; ++piece) {
+                    if (piece != s.piece) {
+                        if (visited[piece][s.goal][s.r][s.c]) continue;
+                        visited[piece][s.goal][s.r][s.c] = true;
+                        queue.add(new Square(s.r, s.c, s.goal, piece, s.time+1, s.changes+1));
+                    }
+                }
+                if (s.piece == 0) {
+                    for (int dir=0; dir<8; ++dir) {
+                        int nr = s.r + knightR[dir];
+                        int nc = s.c + knightC[dir];
+                        if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
+                        if (visited[s.piece][s.goal][nr][nc]) continue;
+                        visited[s.piece][s.goal][nr][nc] = true;
+                        queue.add(new Square(nr, nc, s.goal, s.piece, s.time+1, s.changes));
+                    }
+                } else if (s.piece == 1) {
+                    for (int dir=0; dir<4; ++dir) {
+                        int nr = s.r; int nc = s.c;
+                        while (true) {
+                            nr += bishopR[dir]; nc += bishopC[dir];
+                            if (nr<0 || N<=nr || nc<0 || N<=nc) break;
+                            if (visited[s.piece][s.goal][nr][nc]) continue;
+                            visited[s.piece][s.goal][nr][nc] = true;
+                            queue.add(new Square(nr, nc, s.goal, s.piece, s.time+1, s.changes));
+                        }
+                    }
+                } else {
+                    for (int dir=0; dir<4; ++dir) {
+                        int nr = s.r; int nc = s.c;
+                        while (true) {
+                            nr += rookR[dir]; nc += rookC[dir];
+                            if (nr<0 || N<=nr || nc<0 || N<=nc) break;
+                            if (visited[s.piece][s.goal][nr][nc]) continue;
+                            visited[s.piece][s.goal][nr][nc] = true;
+                            queue.add(new Square(nr, nc, s.goal, s.piece, s.time+1, s.changes));
                         }
                     }
                 }
             }
-
-            if (cur.type == 0) {
-                for (int dir = 0; dir < 8; ++dir) {
-                    int nr = cur.r + knightR[dir];
-                    int nc = cur.c + knightC[dir];
-                    if (!check(nr, nc)) continue;
-                    if (visited[0][nr][nc][cur.target]) continue;
-                    visited[0][nr][nc][cur.target] = true;
-                    queue.add(new Coordinate(0, nr, nc, cur.changes, cur.target, cur.times + 1));
-                }
-            } else if (cur.type == 1) {
-                for (int dir = 0; dir < 4; ++dir) {
-                    int nr = cur.r;
-                    int nc = cur.c;
-                    while (true) {
-                        nr += bishopR[dir];
-                        nc += bishopC[dir];
-                        if (!check(nr, nc)) break;
-                        if (visited[1][nr][nc][cur.target]) continue;
-                        visited[1][nr][nc][cur.target] = true;
-                        queue.add(new Coordinate(1, nr, nc, cur.changes, cur.target, cur.times + 1));
-                    }
-                }
-            } else {
-                for (int dir = 0; dir < 4; ++dir) {
-                    int nr = cur.r;
-                    int nc = cur.c;
-                    while (true) {
-                        nr += rookR[dir];
-                        nc += rookC[dir];
-                        if (!check(nr, nc)) break;
-                        if (visited[2][nr][nc][cur.target]) continue;
-                        visited[2][nr][nc][cur.target] = true;
-                        queue.add(new Coordinate(2, nr, nc, cur.changes, cur.target, cur.times + 1));
-                    }
-                }
-            }
         }
-    }
-
-    public static boolean check(int r, int c) {
-        if (r < 0 || N <= r || c < 0 || N <= c) return false;
-        return true;
     }
      */
 }
 
 /*
-class Coordinate implements Comparable<Coordinate> {
-    int type;
-    int r;
-    int c;
-    int changes;
-    int target;
-    int times;
+class Square implements Comparable<Square>{
+    int r, c, goal, piece, time, changes;
 
-    Coordinate(int type, int r, int c, int changes, int target, int times) {
-        this.type = type;
-        this.target = target;
+    Square (int r, int c, int goal, int piece, int time, int changes) {
         this.r = r;
         this.c = c;
+        this.goal = goal;
+        this.piece = piece;
+        this.time = time;
         this.changes = changes;
-        this.times = times;
     }
 
     @Override
-    public int compareTo(Coordinate c) {
-        if (this.times > c.times) {
+    public int compareTo (Square s) {
+        if (this.time > s.time) {
             return 1;
-        } else if (this.times == c.times) {
-            if (this.changes > c.changes) {
+        } else if (this.time == s.time) {
+            if (this.changes > s.changes) {
                 return 1;
-            } else if (this.changes == c.changes) {
+            } else if (this.changes == s.changes) {
                 return 0;
             } else {
                 return -1;
