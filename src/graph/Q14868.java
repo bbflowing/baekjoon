630144903bcf81fa987710f23283600b3b24f2d3
mjda611 <mjda611@naver.com>
2021-06-23 16:51:01 +0900

    version2

--- src/graph/Q14868.java
+++ src/graph/Q14868.java
@@ -4,17 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q14868 {
     /*
-    public static int N, count;
+    public static int N, K, count;
     public static int[][] world;
-    public static Queue<Coordinate> queue;
-    public static Queue<Coordinate> next;
     public static int[] parents;
+    public static Queue<Civilization> queue;
+    public static Queue<Civilization> next;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
 
@@ -22,21 +21,21 @@ public class Q14868 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         world = new int[N][N];
+        parents = new int[K+1];
         queue = new ArrayDeque<>();
         next = new ArrayDeque<>();
-        for (int i=1; i<=M; ++i) {
+        for (int i=1; i<=K; ++i) {
             st = new StringTokenizer(br.readLine());
             int c = Integer.parseInt(st.nextToken())-1;
             int r = N-Integer.parseInt(st.nextToken());
+            queue.add(new Civilization(r, c, i));
             world[r][c] = i;
-            queue.add(new Coordinate(r, c, i));
+            parents[i] = i;
         }
-        parents = new int[M+1];
-        for (int i=0; i<M+1; ++i) parents[i] = i;
-        count = M;
         int years = 0;
+        count = K;
         while (true) {
             merge();
             if (count == 1) {
@@ -50,19 +49,29 @@ public class Q14868 {
 
     public static void merge() {
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            next.add(cur);
-
+            Civilization c = queue.poll();
+            next.add(c);
             for (int dir=0; dir<4; ++dir) {
-                int nr = cur.r + dr[dir];
-                int nc = cur.c + dc[dir];
-
+                int nr = c.r + dr[dir];
+                int nc = c.c + dc[dir];
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
                 if (world[nr][nc] != 0) {
-                    if (union(cur.civilization, world[nr][nc])) --count;
+                    if (union(c.civilization, world[nr][nc])) --count;
                 }
             }
         }
+    }
+
+    public static boolean union (int c1, int c2) {
+        int parent1 = find(c1);
+        int parent2 = find(c2);
+
+        if (parent1 == parent2) return false;
+        else {
+            if (parent1 < parent2) parents[parent2] = parent1;
+            else parents[parent1] = parent2;
+        }
+        return true;
     }
 
     public static int find(int c) {
@@ -70,27 +79,16 @@ public class Q14868 {
         return parents[c] = find(parents[c]);
     }
 
-    public static boolean union(int c1, int c2) {
-        int parent1 = find(c1);
-        int parent2 = find(c2);
-
-        if (parent1 == parent2) return false;
-        else if (parent1 < parent2) parents[parent2] = parent1;
-        else parents[parent1] = parent2;
-        return true;
-    }
-
     public static void spread() {
         while (!next.isEmpty()) {
-            Coordinate cur = next.poll();
+            Civilization c = next.poll();
             for (int dir=0; dir<4; ++dir) {
-                int nr = cur.r + dr[dir];
-                int nc = cur.c + dc[dir];
-
+                int nr = c.r + dr[dir];
+                int nc = c.c + dc[dir];
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
                 if (world[nr][nc] == 0) {
-                    world[nr][nc] = cur.civilization;
-                    queue.add(new Coordinate(nr, nc, world[nr][nc]));
+                    world[nr][nc] = c.civilization;
+                    queue.add(new Civilization(nr, nc, c.civilization));
                 }
             }
         }
@@ -99,10 +97,10 @@ public class Q14868 {
 }
 
 /*
-class Coordinate {
+class Civilization {
     int r, c, civilization;
 
-    Coordinate (int r, int c, int civilization) {
+    Civilization (int r, int c, int civilization) {
         this.r = r;
         this.c = c;
         this.civilization = civilization;
