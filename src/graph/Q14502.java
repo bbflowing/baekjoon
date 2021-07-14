14fd9bdfd773cc33a5e8672d9cee9569219af7ad
mjda611 <mjda611@naver.com>
2021-07-14 15:52:25 +0900

    version2

--- src/graph/Q14502.java
+++ src/graph/Q14502.java
@@ -1,101 +1,93 @@
 package graph;
 
+//연구소
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q14502 {
     /*
-    public static int N, M, answer;
-    public static int laboratory [][];
-    public static Queue<Coordinate> virus;
-    public static void main (String args[]) throws IOException {
+    public static int R, C, blank, answer;
+    public static int map[][];
+    public static Queue<Location> queue;
+    public static ArrayList<Location> blanks;
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        laboratory = new int [N][M];
-        ArrayList<Coordinate> blanks = new ArrayList<>();
-        Queue<Coordinate> original = new LinkedList<>();
-        int zeros = 0;
-        for (int i=0; i<N; ++i) {
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new int[R][C];
+        queue = new ArrayDeque<>();
+        blank = 0;
+        answer = Integer.MIN_VALUE;
+        blanks = new ArrayList<>();
+        for (int r=0; r<R; ++r) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<M; ++j) {
-                laboratory[i][j] = Integer.parseInt(st.nextToken());
-                if (laboratory[i][j] == 0) {
-                    blanks.add(new Coordinate(i, j));
-                    ++zeros;
-                } else if (laboratory[i][j] == 2) {
-                    original.add(new Coordinate(i, j));
+            for (int c=0; c<C; ++c) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+                if (map[r][c] == 2) queue.add(new Location(r, c));
+                else if (map[r][c] == 0) {
+                    ++blank;
+                    blanks.add(new Location(r, c));
                 }
             }
         }
-        answer = -1;
-        for (int i=0; i<blanks.size()-2; ++i) {
-            Coordinate first = blanks.get(i);
-            for (int j=i+1; j<blanks.size()-1; ++j) {
-                Coordinate second = blanks.get(j);
-                for (int k=j+1; k<blanks.size(); ++k) {
-                    Coordinate third = blanks.get(k);
-                    virus = new LinkedList<>(original);
-                    laboratory[first.x][first.y] = 1;
-                    laboratory[second.x][second.y] = 1;
-                    laboratory[third.x][third.y] = 1;
-                    bfs(zeros-3);
-                    laboratory[first.x][first.y] = 0;
-                    laboratory[second.x][second.y] = 0;
-                    laboratory[third.x][third.y] = 0;
-                }
-            }
-        }
+        Location[] result = new Location[3];
+        combination(result, 0, 0);
         System.out.println(answer);
     }
 
-    public static void bfs (int zeros) {
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
-        boolean visited [][] = new boolean [N][M];
+    public static int bfs(Location[] result) {
+        Queue<Location> virus = new ArrayDeque<>(queue);
+        for (int i=0; i<3; ++i) map[result[i].r][result[i].c] = 1;
+        boolean[][] visited = new boolean[R][C];
+        int count = blank-3;
 
         while (!virus.isEmpty()) {
-            Coordinate c = virus.poll();
-            for (int i = 0; i < 4; ++i) {
-                int nx = c.x + dx[i];
-                int ny = c.y + dy[i];
-
-                if (nx<0 || N<=nx || ny<0 || M<=ny) {
-                    continue;
-                }
-                if (visited[nx][ny]) {
-                    continue;
-                }
-                if (laboratory[nx][ny] == 1 || laboratory [nx][ny] == 2) {
-                    continue;
-                }
-                visited[nx][ny] = true;
-                virus.add(new Coordinate(nx, ny));
-                --zeros;
+            Location cur = virus.poll();
+            for (int dir=0; dir<4; ++dir) {
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (visited[nr][nc]) continue;
+                if (map[nr][nc] != 0) continue;
+                visited[nr][nc] = true;
+                --count;
+                virus.add(new Location(nr, nc));
             }
         }
-        if (zeros > answer) {
-            answer = zeros;
+        for (int i=0; i<3; ++i) map[result[i].r][result[i].c] = 0;
+        return count;
+    }
+
+    public static void combination(Location[] result, int start, int index) {
+        if (index == 3) {
+            answer = Math.max(answer, bfs(result));
+            return;
         }
+
+        if (start == blanks.size()) return;
+
+        result[index] = blanks.get(start);
+        combination(result, start+1, index+1);
+        combination(result, start+1, index);
     }
      */
 }
 
 /*
-class Coordinate {
-    int x;
-    int y;
+class Location {
+    int r, c;
 
-    Coordinate (int x, int y) {
-        this.x = x;
-        this.y = y;
+    Location(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
  */
