619cf09d39815cda46aaf6b649a25b0c4f8c6c4d
mjda611 <mjda611@naver.com>
2021-07-14 15:14:14 +0900

    version2

--- src/graph/Q1012.java
+++ src/graph/Q1012.java
@@ -1,40 +1,44 @@
 package graph;
 
+//유기농 배추
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q1012 {
     /*
-    public static int land [][];
-    public static int M, N;
-    public static boolean visited [][];
-    public static int dx [] = {-1, 1, 0, 0};
-    public static int dy [] = {0, 0, -1, 1};
-    public static void main (String args[]) throws IOException {
+    public static int[][] map;
+    public static int[][] check;
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
+    public static int R, C, size;
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         for (int t=0; t<T; ++t) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            land = new int [M][N];
-            for (int i=0; i<K; ++i) {
+            st = new StringTokenizer(br.readLine());
+            C = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+            int locations = Integer.parseInt(st.nextToken());
+            map = new int[R][C];
+            for (int i=0; i<locations; ++i) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                land[x][y] = 1;
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                map[r][c] = 1;
             }
-            visited = new boolean [M][N];
+            check = new int[R][C];
             int counter = 0;
-            for (int i=0; i<M; ++i) {
-                for (int j=0; j<N; ++j) {
-                    if (!visited[i][j] && land[i][j] == 1) {
-                        visited[i][j] = true;
+            for (int r=0; r<R; ++r) {
+                for (int c=0; c<C; ++c) {
+                    if (map[r][c] == 1 && check[r][c] == 0) {
                         ++counter;
-                        dfs(i, j);
+                        check[r][c] = counter;
+                        dfs(r, c, counter);
                     }
                 }
             }
@@ -44,17 +48,16 @@ public class Q1012 {
         bw.flush();
     }
 
-    public static void dfs (int x, int y) {
-        for (int i=0; i<4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+    public static void dfs(int r, int c, int counter) {
+        for (int dir=0; dir<4; ++dir) {
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
 
-            if (0 <= nx && nx < M && 0 <= ny && ny < N) {
-                if (!visited[nx][ny] && land[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    dfs(nx, ny);
-                }
-            }
+            if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+            if (check[nr][nc] != 0) continue;
+            if (map[nr][nc] == 0) continue;
+            check[nr][nc] = counter;
+            dfs(nr, nc, counter);
         }
     }
      */
