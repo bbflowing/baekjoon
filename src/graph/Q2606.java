725eb965926ac8f29364f90b5d0e0f5a24db65e3
mjda611 <mjda611@naver.com>
2021-07-14 14:25:32 +0900

    version2

--- src/graph/Q2606.java
+++ src/graph/Q2606.java
@@ -1,56 +1,53 @@
 package graph;
 
+// 바이러스
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q2606 {
     /*
+    public static ArrayList<ArrayList<Integer>> connection;
     public static int computers;
-    public static ArrayList<ArrayList<Integer>> network;
-    public static boolean[] visited;
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        computers = Integer.parseInt(br.readLine());
-        network = new ArrayList<>(computers+1);
-        for (int i=0; i<=computers; ++i) {
-            network.add(new ArrayList<>());
-        }
-        int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for (int i=0; i<N; ++i) {
+        computers = Integer.parseInt(br.readLine());
+        int edges = Integer.parseInt(br.readLine());
+        connection = new ArrayList<>();
+        for (int i=0; i<=computers; ++i) connection.add(new ArrayList<>());
+        for (int i=0; i<edges; ++i) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            network.get(start).add(end);
-            network.get(end).add(start);
+            connection.get(start).add(end);
+            connection.get(end).add(start);
         }
-        visited = new boolean[computers+1];
-        visited[1] = true;
-        solve(1);
+        bfs();
+    }
+
+    public static void bfs() {
+        boolean[] check = new boolean[computers+1];
+        check[1] = true;
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(1);
         int answer = 0;
-        for (int i=2; i<computers+1; ++i) {
-            if (visited[i]) {
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            ArrayList<Integer> list = connection.get(cur);
+            for (int i=0; i<list.size(); ++i) {
+                if (check[list.get(i)]) continue;
+                check[list.get(i)] = true;
+                queue.add(list.get(i));
                 ++answer;
             }
         }
         System.out.println(answer);
-    }
-
-    public static void solve (int computer) {
-        ArrayList<Integer> target = network.get(computer);
-        for (int i=0; i<target.size(); ++i) {
-            int dst = target.get(i);
-            if (!visited[dst]) {
-                visited[dst] = true;
-                solve(dst);
-            }
-        }
     }
      */
 }
