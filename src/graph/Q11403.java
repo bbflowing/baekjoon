166ace30dff4023f8ee56b357b42794f87a8d020
mjda611 <mjda611@naver.com>
2021-07-15 12:14:34 +0900

    version2

--- src/graph/Q11403.java
+++ src/graph/Q11403.java
@@ -1,61 +1,57 @@
 package graph;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+// 경로 찾기
+
+import java.io.*;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q11403 {
-    public static ArrayList<ArrayList<Integer>> list;
-    public static int result [][];
-    public static void main (String args[]) throws IOException {
+    /*
+    public static int N;
+    public static ArrayList<ArrayList<Integer>> edges;
+    public static int[][] result;
+
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        list = new ArrayList<>();
-        result = new int [N][N];
-        for (int i=0; i<N; ++i) {
-            list.add(new ArrayList<>());
-        }
-        for (int i=0; i<N; ++i) {
+        edges = new ArrayList<>();
+        result = new int[N][N];
+        for (int i=0; i<N; ++i) edges.add(new ArrayList<>());
+        for (int r=0; r<N; ++r) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; ++j) {
-                if (Integer.parseInt(st.nextToken()) == 1) {
-                    list.get(i).add(j);
+            for (int c=0; c<N; ++c) {
+                if (Integer.parseInt(st.nextToken()) == 1) edges.get(r).add(c);
+            }
+        }
+        for (int i=0; i<N; ++i) solve(i);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int r=0; r<N; ++r) {
+            for (int c=0; c<N; ++c) bw.append(result[r][c]+" ");
+            bw.newLine();
+        }
+        bw.flush();
+    }
+
+    public static void solve(int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            ArrayList<Integer> temp = edges.get(cur);
+            for (int i=0; i<temp.size(); ++i) {
+                int next = temp.get(i);
+                if (result[start][next] == 0) {
+                    result[start][next] = 1;
+                    queue.add(next);
                 }
             }
         }
-
-        for (int i=0; i<N; ++i) {
-            for (int j=0; j<N; ++j) {
-                boolean check [] = new boolean [N];
-                result[i][j] = dfs(i, j, check);
-            }
-        }
-        for (int i=0; i<N; ++i) {
-            String temp = "";
-            for (int j=0; j<N; ++j) {
-                temp += result[i][j] + " ";
-            }
-            System.out.println(temp);
-        }
-
     }
-    public static int dfs(int x, int y, boolean [] check) {
-        check[x] = true;
-        for (int i=0; i<list.get(x).size(); ++i) {
-            if (list.get(x).get(i) == y) {
-                return 1;
-            } else {
-                if (!check[list.get(x).get(i)]) {
-                    if (dfs(list.get(x).get(i), y, check) == 1) {
-                        return 1;
-                    }
-                }
-            }
-        }
-        return 0;
-    }
+     */
 }
 
