7b9e17b4dc73fab171ff428377525ad017c0e890
mjda611 <mjda611@naver.com>
2021-07-15 12:01:04 +0900

    version2

--- src/graph/Q4963.java
+++ src/graph/Q4963.java
@@ -1,5 +1,7 @@
 package graph;
 
+// 섬의 개수
+
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -7,64 +9,55 @@ import java.util.StringTokenizer;
 
 public class Q4963 {
     /*
-    public static int map [][];
-    public static boolean visited [][];
-    public static int R, C;
-    public static int dx [] = {-1, -1, -1, 0, 0, 1, 1, 1};
-    public static int dy [] = {-1, 0, 1, -1, 1, -1, 0, 1};
-    public static void main (String args[]) throws IOException {
+    public static int R, C, size;
+    public static int[][] map;
+    public static boolean[][] check;
+    public static int[] dr = {-1, -1, -1, 0, 0, +1, +1, +1};
+    public static int[] dc = {-1, 0, +1, -1, +1, -1, 0, +1};
+
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         while (true) {
             String line = br.readLine();
-            if (line.equals("0 0")) {
-                break;
-            }
+            if (line.equals("0 0")) break;
             st = new StringTokenizer(line);
             C = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
             map = new int[R][C];
-            visited = new boolean[R][C];
-            for (int i = 0; i < R; ++i) {
+            check = new boolean[R][C];
+            for (int r=0; r<R; ++r) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < C; ++j) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
+                for (int c=0; c<C; ++c) map[r][c] = Integer.parseInt(st.nextToken());
             }
-
-            int count = 0;
-            for (int i = 0; i < R; ++i) {
-                for (int j = 0; j < C; ++j) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
-                        ++count;
-                        visited[i][j] = true;
-                        solve(i, j);
+            int counter = 0;
+            for (int r=0; r<R; ++r) {
+                for (int c=0; c<C; ++c) {
+                    if (map[r][c] == 1 && !check[r][c]) {
+                        ++counter;
+                        check[r][c] = true;
+                        dfs(r, c);
                     }
                 }
             }
-            bw.append(String.valueOf(count));
+            bw.append(String.valueOf(counter));
             bw.newLine();
         }
         bw.flush();
     }
 
-    public static void solve (int x, int y) {
+    public static void dfs(int r, int c) {
         for (int dir=0; dir<8; ++dir) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
 
-            if (nx<0 || R<=nx || ny<0 || C<=ny) {
-                continue;
-            }
-            if (visited[nx][ny]) {
-                continue;
-            }
-            if (map[nx][ny] == 0) {
-                continue;
-            }
-            visited[nx][ny] = true;
-            solve(nx, ny);
+            if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+            if (map[nr][nc] == 0) continue;
+            if (check[nr][nc]) continue;
+            check[nr][nc] = true;
+            dfs(nr, nc);
         }
     }
      */
