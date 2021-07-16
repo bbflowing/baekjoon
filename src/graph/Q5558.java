a4b03fd5cf552c5375e0fdb0ca27bc087673d851
mjda611 <mjda611@naver.com>
2021-07-16 14:37:00 +0900

    version2

--- src/graph/Q5558.java
+++ src/graph/Q5558.java
@@ -8,62 +8,56 @@ import java.util.*;
 
 public class Q5558 {
     /*
+    public static char[][] map;
     public static int R, C;
-    public static int[][] input;
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         int max = Integer.parseInt(st.nextToken());
-        input = new int[R][C];
+        map = new char[R][C];
+        Location S = null;
         String line = "";
-        Coordinate start = null;
-
         for (int r=0; r<R; ++r) {
             line = br.readLine();
             for (int c=0; c<C; ++c) {
-                char target = line.charAt(c);
-                if (target == 'S') start = new Coordinate(r, c, 1, 0);
-                else if (target == 'X') input[r][c] = -1;
-                else if ('1' <= target && target <= '9') input[r][c] = target-'0';
+                map[r][c] = line.charAt(c);
+                if (map[r][c] == 'S') S = new Location(1, r, c, 0);
             }
         }
-        bfs(start, max);
+        bfs(S, max);
     }
 
-    public static void bfs (Coordinate start, int max) {
+    public static void bfs(Location S, int max) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         boolean[][][] visited = new boolean[max+1][R][C];
-        visited[1][start.r][start.c] = true;
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(start);
+        Queue<Location> queue = new ArrayDeque<>();
+        queue.add(S);
+        visited[1][S.r][S.c] = true;
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            if (cur.target == input[cur.r][cur.c]) {
-                if (cur.target == max) {
-                    System.out.println(cur.distance);
+            Location cur = queue.poll();
+            if (map[cur.r][cur.c]-'0' == cur.goal) {
+                if (cur.goal == max) {
+                    System.out.println(cur.times);
                     return;
                 } else {
-                    if (!visited[cur.target+1][cur.r][cur.c]) {
-                        visited[cur.target+1][cur.r][cur.c] = true;
-                        queue.add(new Coordinate(cur.r, cur.c, cur.target+1, cur.distance));
-                    }
+                    visited[cur.goal+1][cur.r][cur.c] = true;
+                    queue.add(new Location(cur.goal+1, cur.r, cur.c, cur.times));
                     continue;
                 }
             }
             for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
                 if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
-                if (visited[cur.target][nr][nc]) continue;
-                if (input[nr][nc] == -1) continue;
-                queue.add(new Coordinate(nr, nc,  cur.target, cur.distance+1));
-                visited[cur.target][nr][nc] = true;
+                if (visited[cur.goal][nr][nc]) continue;
+                if (map[nr][nc] == 'X') continue;
+                visited[cur.goal][nr][nc] = true;
+                queue.add(new Location(cur.goal, nr, nc, cur.times+1));
             }
         }
     }
@@ -71,14 +65,14 @@ public class Q5558 {
 }
 
 /*
-class Coordinate {
-    int r, c, target, distance;
+class Location {
+    int goal, r, c, times;
 
-    Coordinate (int r, int c, int target, int distance) {
+    Location(int goal, int r, int c, int times) {
+        this.goal = goal;
         this.r = r;
         this.c = c;
-        this.target = target;
-        this.distance = distance;
+        this.times = times;
     }
 }
  */
