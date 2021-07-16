bf7d3d34599ecc43b0634e7716e7115aedbbb7ed
mjda611 <mjda611@naver.com>
2021-07-16 16:31:50 +0900

    version2

--- src/graph/Q16441.java
+++ src/graph/Q16441.java
@@ -1,5 +1,7 @@
 package graph;
 
+// 아기돼지와 늑대
+
 import java.io.*;
 import java.util.*;
 
@@ -15,82 +17,88 @@ public class Q16441 {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         map = new char[R][C];
-        String line = "";
-        ArrayList<Coordinate> wolves = new ArrayList<>();
-        for (int r = 0; r < R; ++r) {
-            line = br.readLine();
-            for (int c = 0; c < C; ++c) {
-                map[r][c] = line.charAt(c);
-                if (map[r][c] == 'W') wolves.add(new Coordinate(r, c));
-            }
-        }
+        Queue<Location> wolves = new ArrayDeque<>();
         visited = new boolean[R][C];
-        for (int i = 0; i < wolves.size(); ++i) {
-            Coordinate start = wolves.get(i);
-            bfs(start);
-        }
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int r = 0; r < R; ++r) {
-            StringBuilder sb = new StringBuilder();
-            for (int c = 0; c < C; ++c) {
-                if (!visited[r][c] && map[r][c] == '.') sb.append("P");
-                else sb.append(map[r][c]);
+        String line = "";
+        for (int r=0; r<R; ++r) {
+            line = br.readLine();
+            for (int c=0; c<C; ++c) {
+                map[r][c] = line.charAt(c);
+                if (map[r][c] == 'W') {
+                    wolves.add(new Location(r, c, 0));
+                    visited[r][c] = true;
+                }
             }
-            bw.append(sb);
+        }
+        bfs(wolves);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int r=0; r<R; ++r) {
+            for (int c=0; c<C; ++c) {
+                if (map[r][c] == '.' && !visited[r][c]) bw.append("P");
+                else bw.append(map[r][c]);
+            }
             bw.newLine();
         }
         bw.flush();
     }
 
-    public static void bfs(Coordinate start) {
+    public static void bfs(Queue<Location> wolves) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(start);
-        visited[start.r][start.c] = true;
 
-        while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            for (int dir = 0; dir < 4; ++dir) {
+        while (!wolves.isEmpty()) {
+            Location cur = wolves.poll();
+            for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
-                if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
-                if (map[nr][nc] == '#') {
-                    continue;
-                } else if (map[nr][nc] == '+') {
+                if (!check(nr, nc)) continue;
+                if (visited[nr][nc]) continue;
+                if (map[nr][nc] == '#') continue;
+                else if (map[nr][nc] == '+') {
                     while (true) {
-                        nr += dr[dir]; nc += dc[dir];
-
-                        if (nr < 0 || R <= nr || nc < 0 || C <= nc) break;
-                        if (map[nr][nc] == '#' || map[nr][nc] == '.') {
-                            if (map[nr][nc] == '#') {
-                                nr -= dr[dir]; nc -= dc[dir];
-                            }
+                        nr += dr[dir];
+                        nc += dc[dir];
+                        if (!check(nr, nc)) break;
+                        if (map[nr][nc] == '.') {
                             if (!visited[nr][nc]) {
                                 visited[nr][nc] = true;
-                                queue.add(new Coordinate(nr, nc));
+                                wolves.add(new Location(nr, nc, cur.distance + 1));
+                            }
+                            break;
+                        } else if (map[nr][nc] == '#') {
+                            nr -= dr[dir];
+                            nc -= dc[dir];
+                            if (!visited[nr][nc]) {
+                                visited[nr][nc] = true;
+                                wolves.add(new Location(nr, nc, cur.distance + 1));
                             }
                             break;
                         }
                     }
+                } else {
+                    visited[nr][nc] = true;
+                    wolves.add(new Location(nr, nc, cur.distance+1));
                 }
-                if (visited[nr][nc]) continue;
-                visited[nr][nc] = true;
-                queue.add(new Coordinate(nr, nc));
             }
         }
+
+    }
+
+    public static boolean check(int r, int c) {
+        if (r<0 || R<=r || c<0 || C<=c) return false;
+        return true;
     }
      */
 }
 
 /*
-class Coordinate {
-    int r, c;
+class Location {
+    int r, c, distance;
 
-    Coordinate(int r, int c) {
+    Location(int r, int c, int distance) {
         this.r = r;
         this.c = c;
+        this.distance = distance;
     }
 }
  */
