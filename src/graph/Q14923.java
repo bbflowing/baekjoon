cc514dbae3f10acd5ccb1abe7f4d97ede04b9d45
mjda611 <mjda611@naver.com>
2021-07-16 13:40:18 +0900

    version2

--- src/graph/Q14923.java
+++ src/graph/Q14923.java
@@ -1,5 +1,7 @@
 package graph;
 
+// 미로 탈출
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,54 +12,52 @@ public class Q14923 {
     public static int R, C;
     public static int[][] maze;
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         maze = new int[R+1][C+1];
         st = new StringTokenizer(br.readLine());
-        int startR = Integer.parseInt(st.nextToken());
-        int startC = Integer.parseInt(st.nextToken());
+        Location S = new Location(0, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), 0);
         st = new StringTokenizer(br.readLine());
-        int endR = Integer.parseInt(st.nextToken());
-        int endC = Integer.parseInt(st.nextToken());
-        for (int r=1; r<=R; ++r) {
+        Location E = new Location(0, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), 0);
+        for (int r=1; r<R+1; ++r) {
             st = new StringTokenizer(br.readLine());
-            for (int c=1; c<=C; ++c) {
-                maze[r][c] = Integer.parseInt(st.nextToken());
-            }
+            for (int c=1; c<C+1; ++c) maze[r][c] = Integer.parseInt(st.nextToken());
         }
-        bfs (startR, startC, endR, endC);
+        bfs(S, E);
     }
 
-    public static void bfs (int startR, int startC, int endR, int endC) {
+    public static void bfs(Location S, Location E) {
+        Queue<Location> queue = new ArrayDeque<>();
+        boolean[][][] visited = new boolean[2][R+1][C+1];
+        queue.add(S);
+        visited[0][S.r][S.c] = true;
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        boolean[][][] visited = new boolean[2][R+1][C+1];
-        visited[0][startR][startC] = true;
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(new Coordinate(startR, startC, 0, 0));
 
         while (!queue.isEmpty()) {
-            Coordinate c = queue.poll();
-            if (c.r == endR && c.c == endC) {
-                System.out.println(c.distance);
+            Location cur = queue.poll();
+            if (cur.r == E.r && cur.c == E.c) {
+                System.out.println(cur.time);
                 return;
             }
             for (int dir=0; dir<4; ++dir) {
-                int nr = c.r + dr[dir];
-                int nc = c.c + dc[dir];
-                int nbroken = c.broken;
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
 
-                if (nr<1 || R<nr || nc<1 || C<nc) continue;
+                if (nr<1 || R+1<=nr || nc<1 || C+1<=nc) continue;
+                if (visited[cur.used][nr][nc]) continue;
                 if (maze[nr][nc] == 1) {
-                    if (c.broken == 1) continue;
-                    nbroken = 1;
+                    if (cur.used == 0) {
+                        queue.add(new Location(1, nr, nc, cur.time+1));
+                        visited[1][nr][nc] = true;
+                    }
+                } else {
+                    queue.add(new Location(cur.used, nr, nc, cur.time+1));
+                    visited[cur.used][nr][nc] = true;
                 }
-                if (visited[nbroken][nr][nc]) continue;
-                visited[nbroken][nr][nc] = true;
-                queue.add(new Coordinate(nr, nc, nbroken,c.distance+1));
             }
         }
         System.out.println(-1);
@@ -66,17 +66,14 @@ public class Q14923 {
 }
 
 /*
-class Coordinate {
-    int r;
-    int c;
-    int broken;
-    int distance;
+class Location {
+    int r, c, used, time;
 
-    Coordinate (int r, int c, int broken, int distance) {
+    Location (int used, int r, int c, int time) {
+        this.used = used;
         this.r = r;
         this.c = c;
-        this.broken = broken;
-        this.distance = distance;
+        this.time = time;
     }
 }
  */
