8cdc937d0ab8c70920aec70f3a9dd29197c98e93
mjda611 <mjda611@naver.com>
2021-07-16 11:56:25 +0900

    version2

--- src/graph/Q2206.java
+++ src/graph/Q2206.java
@@ -10,51 +10,49 @@ import java.util.*;
 public class Q2206 {
     /*
     public static int R, C;
-    public static int[][] map;
+    public static char[][] map;
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
+        map = new char[R][C];
         String line = "";
-        for (int r = 0; r < R; ++r) {
+        for (int r=0; r<R; ++r) {
             line = br.readLine();
-            for (int c = 0; c < C; ++c) map[r][c] = line.charAt(c) - '0';
+            for (int c=0; c<C; ++c) map[r][c] = line.charAt(c);
         }
         bfs();
     }
 
     public static void bfs() {
         boolean[][][] visited = new boolean[2][R][C];
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        visited[0][0][0] = true;
-        queue.add(new Coordinate(0, 0, 0, 1));
+        Queue<Location> queue = new ArrayDeque<>();
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
+        queue.add(new Location(0, 0, 0, 1));
+        visited[0][0][0] = true;
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            if (cur.r == R - 1 && cur.c == C - 1) {
+            Location cur = queue.poll();
+            if (cur.r == R-1 && cur.c == C-1) {
                 System.out.println(cur.distance);
                 return;
             }
-            for (int dir = 0; dir < 4; ++dir) {
+            for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
 
-                if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
-                if (map[nr][nc] == 1) {
-                    if (cur.broken == 0 && !visited[1][nr][nc]) {
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (!visited[cur.broken][nr][nc] && map[nr][nc] == '0') {
+                    visited[cur.broken][nr][nc] = true;
+                    queue.add(new Location(nr, nc, cur.broken, cur.distance+1));
+                }
+                if (cur.broken == 0 && map[nr][nc] == '1') {
+                    if (!visited[1][nr][nc]) {
                         visited[1][nr][nc] = true;
-                        queue.add(new Coordinate(1, nr, nc, cur.distance + 1));
-
-                    }
-                } else {
-                    if (!visited[cur.broken][nr][nc]) {
-                        visited[cur.broken][nr][nc] = true;
-                        queue.add(new Coordinate(cur.broken, nr, nc, cur.distance + 1));
+                        queue.add(new Location(nr, nc, 1, cur.distance+1));
                     }
                 }
             }
@@ -65,14 +63,15 @@ public class Q2206 {
 }
 
 /*
-class Coordinate {
-    int broken, r, c, distance;
+class Location {
+    int r, c, broken, distance;
 
-    Coordinate(int broken, int r, int c, int distance) {
-        this.broken = broken;
+    Location(int r, int c, int broken, int distance) {
         this.r = r;
         this.c = c;
+        this.broken = broken;
         this.distance = distance;
     }
 }
+
  */
