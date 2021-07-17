8d35816f2081e62d980a3a9147df2504cfb30814
mjda611 <mjda611@naver.com>
2021-07-17 12:40:23 +0900

    version2

--- src/graph/Q13459.java
+++ src/graph/Q13459.java
@@ -1,121 +1,152 @@
 package graph;
 
+//구슬 탈출
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q13459 {
     /*
-    public static char input [][];
-    public static void main (String args[]) throws IOException {
+    public static int R, C;
+    public static char[][] board;
+    public static boolean[][][][] visited;
+    public static Queue<Location> rq, bq;
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        input = new char [N][M];
-        Coordinate hole = null; Coordinate red = null; Coordinate blue = null;
-
-        for (int i=0; i<N; ++i) {
-            String line = br.readLine();
-            for (int j=0; j<M; ++j) {
-                input[i][j] = line.charAt(j);
-                if (input[i][j] == 'O') {
-                    hole = new Coordinate(i, j);
-                } else if (input[i][j] == 'R') {
-                    red = new Coordinate(i, j);
-                } else if (input[i][j] == 'B') {
-                    blue = new Coordinate(i, j);
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new char[R][C];
+        String line = "";
+        Location blue = null;
+        Location red = null;
+        rq = new ArrayDeque<>();
+        bq = new ArrayDeque<>();
+        visited = new boolean[R][C][R][C];
+        for (int r = 0; r < R; ++r) {
+            line = br.readLine();
+            for (int c = 0; c < C; ++c) {
+                board[r][c] = line.charAt(c);
+                if (board[r][c] == 'B') {
+                    blue = new Location(r, c);
+                    bq.add(new Location(r, c));
+                } else if (board[r][c] == 'R') {
+                    red = new Location(r, c);
+                    rq.add(new Location(r, c));
                 }
             }
         }
-        bfs(hole, red, blue, N, M);
+        visited[blue.r][blue.c][red.r][red.c] = true;
+        int times = 0;
+        while (true) {
+            ++times;
+            if (move() && times <= 10) {
+                times = 1;
+                break;
+            } else if (times == 10) {
+                times = 0;
+                break;
+            }
+        }
+        System.out.println(times);
+
     }
 
-    public static void bfs (Coordinate hole, Coordinate red, Coordinate blue, int N, int M) {
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
-        boolean visited [][][][] = new boolean [N][M][N][M];
-        Queue<Coordinate> redQ = new LinkedList<>();
-        Queue<Coordinate> blueQ = new LinkedList<>();
-        visited[red.x][red.y][blue.x][blue.y] = true;
-        redQ.add(new Coordinate(red.x, red.y, 0));
-        blueQ.add(new Coordinate(blue.x, blue.y, 0));
+    public static boolean move() {
+        int bs = bq.size();
+        for (int i = 0; i < bs; ++i) {
+            Location b = bq.poll();
+            Location r = rq.poll();
 
-        while (!redQ.isEmpty() || !blueQ.isEmpty()) {
-            Coordinate cr = redQ.poll();
-            Coordinate cb = blueQ.poll();
-            if (cr.times >= 10 || cb.times >= 10) {
-                System.out.println(0);
-            }
+            for (int dir = 0; dir < 4; ++dir) {
+                int nbr = b.r;
+                int nbc = b.c;
+                int nrr = r.r;
+                int nrc = r.c;
+                boolean bf = false;
+                boolean rf = false;
 
-            for (int i=0; i<4; ++i) {
-                Coordinate nb = move(cb, i);
-                if (nb.x == hole.x && nb.y == hole.y) {
-                    continue;
-                }
-                Coordinate nr = move(cr, i);
-                if (nr.x == hole.x && nr.y == hole.y) {
-                    System.out.println(1);
-                }
-
-                if (nb.x == nr.x && nb.y == nr.y) {
-                    if (cb.x < cr.x) {
-                        ++nr.x;
-                    } else if (cb.x > cr.x) {
-                        ++nb.x;
-                    } else if (cb.y < cr.y) {
-
+                while (!bf) {
+                    nbr += dr[dir];
+                    nbc += dc[dir];
+                    if (board[nbr][nbc] == 'O') break;
+                    else if (board[nbr][nbc] == '#') {
+                        Location nb = adjust(dir, nbr, nbc);
+                        nbr = nb.r;
+                        nbc = nb.c;
+                        bf = true;
+                        break;
                     }
                 }
 
+                if (board[nbr][nbc] == 'O') continue;
+
+                while (!rf) {
+                    nrr += dr[dir];
+                    nrc += dc[dir];
+                    if (board[nrr][nrc] == 'O') return true;
+                    else if (board[nrr][nrc] == '#') {
+                        Location nr = adjust(dir, nrr, nrc);
+                        nrr = nr.r;
+                        nrc = nr.c;
+                        rf = true;
+                        break;
+                    }
+                }
+
+                if (nbr == nrr && nbc == nrc) {
+                    if (dir == 0) {
+                        if (b.r < r.r) ++nrr;
+                        else ++nbr;
+                    } else if (dir == 1) {
+                        if (b.r < r.r) --nbr;
+                        else --nrr;
+                    } else if (dir == 2) {
+                        if (b.c < r.c) ++nrc;
+                        else ++nbc;
+                    } else {
+                        if (b.c < r.c) --nbc;
+                        else --nrc;
+                    }
+                }
+
+                if (bf && rf) {
+                    if (!visited[nbr][nbc][nrr][nrc]) {
+                        visited[nbr][nbc][nrr][nrc] = true;
+                        bq.add(new Location(nbr, nbc));
+                        rq.add(new Location(nrr, nrc));
+                    }
+                }
             }
         }
-
+        return false;
     }
 
-    public static Coordinate move (Coordinate location, int direction) {
-        int dx [] = {-1, 1, 0, 0};
-        int dy [] = {0, 0, -1, 1};
-
-        int nx = location.x; int ny = location.y;
-        while (true) {
-            nx += dx[direction];
-            ny += dy[direction];
-
-            if (input[nx][ny] == 'O') {
-                return new Coordinate(nx, ny);
-            }
-
-            if (input[nx][ny] == '#') {
-                nx -= dx[direction];
-                ny -= dy[direction];
-                return new Coordinate(nx, ny);
-            }
-        }
+    public static Location adjust(int dir, int r, int c) {
+        if (dir == 0) ++r;
+        else if (dir == 1) --r;
+        else if (dir == 2) ++c;
+        else --c;
+        return new Location(r, c);
     }
-
      */
 }
 
 /*
-class Coordinate {
-    int x;
-    int y;
-    int times;
+class Location {
+    int r, c;
 
-    Coordinate (int x, int y, int times) {
-        this.x = x;
-        this.y = y;
-        this.times = times;
-    }
-
-    Coordinate (int x, int y) {
-        this.x = x;
-        this.y = y;
+    Location(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
-
  */
