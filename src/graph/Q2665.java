c8e5702116f9056eee9bd758cd8677867bf733ec
mjda611 <mjda611@naver.com>
2021-07-17 14:36:36 +0900

    version2

--- src/graph/Q2665.java
+++ src/graph/Q2665.java
@@ -1,5 +1,7 @@
 package graph;
 
+// 미로 만들기
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,53 +10,50 @@ import java.util.*;
 public class Q2665 {
     /*
     public static int N;
-    public static int[][] rooms;
+    public static char[][] rooms;
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        rooms = new int[N][N];
+        rooms = new char[N][N];
         String line = "";
-        int[][] dp = new int[N][N];
-        for (int r=0; r<N; ++r) {
+        for (int r = 0; r < N; ++r) {
             line = br.readLine();
-            for (int c=0; c<N; ++c) {
-                rooms[r][c] = line.charAt(c)-'0';
-                dp[r][c] = -1;
+            for (int c = 0; c < N; ++c) {
+                rooms[r][c] = line.charAt(c);
             }
         }
-        bfs(dp);
+        bfs();
+
     }
 
-    public static void bfs(int[][] dp) {
+    public static void bfs() {
+        PriorityQueue<Location> queue = new PriorityQueue<>();
+        int[][] dp = new int[N][N];
+        for (int r = 0; r < N; ++r) {
+            for (int c = 0; c < N; ++c) dp[r][c] = Integer.MAX_VALUE;
+        }
+        queue.add(new Location(0, 0, 0));
+        dp[0][0] = 0;
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        queue.add(new Coordinate(0, 0, 0));
-        dp[0][0] = 0;
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
+            Location cur = queue.poll();
             if (cur.r == N-1 && cur.c == N-1) {
                 System.out.println(cur.changes);
                 return;
             }
-            for (int dir=0; dir<4; ++dir) {
+            for (int dir = 0; dir < 4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
+                int nchanges = cur.changes;
 
-                if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
-                if (rooms[nr][nc] == 0) {
-                    if (dp[nr][nc] == -1 || cur.changes+1 < dp[nr][nc]) {
-                        dp[nr][nc] = cur.changes+1;
-                        queue.add(new Coordinate(nr, nc, cur.changes+1));
-                    }
-                } else {
-                    if (dp[nr][nc] == -1 || cur.changes < dp[nr][nc]) {
-                        dp[nr][nc] = cur.changes;
-                        queue.add(new Coordinate(nr, nc, cur.changes));
-                    }
-                }
+                if (nr < 0 || N <= nr || nc < 0 || N <= nc) continue;
+                if (rooms[nr][nc] == '0') ++nchanges;
+                if (nchanges >= dp[nr][nc]) continue;
+                dp[nr][nc] = nchanges;
+                queue.add(new Location(nr, nc, nchanges));
             }
         }
     }
@@ -62,18 +61,22 @@ public class Q2665 {
 }
 
 /*
-class Coordinate implements Comparable<Coordinate> {
+class Location implements Comparable<Location> {
     int r, c, changes;
 
-    Coordinate (int r, int c, int changes) {
+    Location(int r, int c, int changes) {
         this.r = r;
         this.c = c;
         this.changes = changes;
     }
 
     @Override
-    public int compareTo (Coordinate c) {
-        return this.changes - c.changes;
+    public int compareTo(Location l) {
+        if (this.changes > l.changes) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
  */
