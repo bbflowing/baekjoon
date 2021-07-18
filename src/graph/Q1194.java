e99b912cda515e13a5dda49b34be0347d249ac24
mjda611 <mjda611@naver.com>
2021-07-18 12:17:55 +0900

    version2

--- src/graph/Q1194.java
+++ src/graph/Q1194.java
@@ -21,60 +21,48 @@ public class Q1194 {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         maze = new char[R][C];
-        Coordinate start = null;
+        Location start = null;
         String line = "";
-        for (int r = 0; r < R; ++r) {
+        for (int r=0; r<R; ++r) {
             line = br.readLine();
-            for (int c = 0; c < C; ++c) {
+            for (int c=0; c<C; ++c) {
                 maze[r][c] = line.charAt(c);
                 if (maze[r][c] == '0') {
+                    start = new Location(r, c, 0, 0);
                     maze[r][c] = '.';
-                    start = new Coordinate(r, c, 0, 0);
                 }
             }
         }
         System.out.println(bfs(start));
     }
 
-    public static int bfs(Coordinate start) {
+    public static int bfs(Location start) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(start);
-        boolean[][][] visited = new boolean[R][C][1 << 6];
+        boolean[][][] visited = new boolean[R][C][1<<6];
+        Queue<Location> queue = new ArrayDeque<>();
         visited[start.r][start.c][0] = true;
+        queue.add(start);
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            for (int dir = 0; dir < 4; ++dir) {
+            Location cur = queue.poll();
+            for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
-                if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
-                if (maze[nr][nc] == '#') {
-                    continue;
-                } else if (maze[nr][nc] == '1') {
-                    return cur.distance+1;
-                } else if ('a' <= maze[nr][nc] && maze[nr][nc] <= 'f') {
-                    int nkeys = cur.keys | 1<<maze[nr][nc]-'a';
-                    if (!visited[nr][nc][nkeys]) {
-                        visited[nr][nc][nkeys] = true;
-                        queue.add(new Coordinate(nr, nc, nkeys,cur.distance+1));
-                    }
+                int nkey = cur.key;
+                int ntimes = cur.times+1;
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (visited[nr][nc][cur.key]) continue;
+                if (maze[nr][nc] == '#') continue;
+                else if ('a' <= maze[nr][nc] && maze[nr][nc] <= 'f') {
+                    nkey = cur.key | 1<<maze[nr][nc]-'a';
                 } else if ('A' <= maze[nr][nc] && maze[nr][nc] <= 'F') {
-                    int test = cur.keys & 1<<maze[nr][nc]-'A';
-                    if (test > 0) {
-                        if (!visited[nr][nc][cur.keys]) {
-                            visited[nr][nc][cur.keys] = true;
-                            queue.add(new Coordinate(nr, nc, cur.keys, cur.distance+1));
-                        }
-                    }
-                } else if (maze[nr][nc] == '.') {
-                    if (!visited[nr][nc][cur.keys]) {
-                        visited[nr][nc][cur.keys] = true;
-                        queue.add(new Coordinate(nr, nc, cur.keys, cur.distance+1));
-                    }
+                    if ((cur.key & 1<<maze[nr][nc]-'A') == 0) continue;
+                } else if (maze[nr][nc] == '1') {
+                    return ntimes;
                 }
+                visited[nr][nc][nkey] = true;
+                queue.add(new Location(nr, nc, nkey, ntimes));
             }
         }
         return -1;
@@ -83,14 +71,14 @@ public class Q1194 {
 }
 
 /*
-class Coordinate {
-    int r, c, keys, distance;
+class Location {
+    int r, c, key, times;
 
-    Coordinate(int r, int c, int keys, int distance) {
+    Location(int r, int c, int key, int times) {
         this.r = r;
         this.c = c;
-        this.keys = keys;
-        this.distance = distance;
+        this.key = key;
+        this.times = times;
     }
 }
  */
