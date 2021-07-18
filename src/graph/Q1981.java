a060724789546de435b86b0451b14f21660c95ee
mjda611 <mjda611@naver.com>
2021-07-18 12:47:37 +0900

    version2

--- src/graph/Q1981.java
+++ src/graph/Q1981.java
@@ -4,89 +4,82 @@ import java.io.*;
 import java.util.*;
 
 public class Q1981 {
-    /*
     public static int N;
     public static int[][] array;
+    public static ArrayList<Integer> numbers;
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         array = new int[N][N];
+        numbers = new ArrayList<>();
         StringTokenizer st;
-        ArrayList<Integer> numbers = new ArrayList<>();
         for (int r=0; r<N; ++r) {
             st = new StringTokenizer(br.readLine());
             for (int c=0; c<N; ++c) {
                 array[r][c] = Integer.parseInt(st.nextToken());
-                if (!numbers.contains(array[r][c])) {
-                    numbers.add(array[r][c]);
-                }
+                if (!numbers.contains(array[r][c])) numbers.add(array[r][c]);
             }
         }
         Collections.sort(numbers);
-        int answer = solve(numbers);
-        StringBuilder sb = new StringBuilder();
-        sb.append(answer);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.append(sb);
-        bw.flush();
+        solve();
     }
 
-    public static int solve (ArrayList<Integer> numbers) {
+    public static void solve() {
         int minIndex = 0;
         int maxIndex = 0;
+        int min = 0;
+        int max = 0;
         int answer = Integer.MAX_VALUE;
 
         while (minIndex < numbers.size() && maxIndex < numbers.size()) {
-            int min = numbers.get(minIndex);
-            int max = numbers.get(maxIndex);
-            if (array[0][0] < min || max < array[0][0]) {
+            min = numbers.get(minIndex);
+            max = numbers.get(maxIndex);
+            if (max < array[0][0] || array[0][0] < min) {
                 ++maxIndex;
                 continue;
             }
-            if (bfs(min, max)) {
-                answer = Math.min(max-min, answer);
+            boolean result = bfs(min, max);
+            if (result) {
+                answer = Math.min(answer, max-min);
                 ++minIndex;
-            } else ++maxIndex;
+            } else {
+                ++maxIndex;
+            }
         }
-        return answer;
+        System.out.println(answer);
     }
 
     public static boolean bfs (int min, int max) {
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
+        Queue<Location> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[N][N];
+        queue.add(new Location(0, 0));
         visited[0][0] = true;
-        Queue<Coordinate> queue = new ArrayDeque<>();
-        queue.add(new Coordinate(0, 0));
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
+            Location cur = queue.poll();
             if (cur.r == N-1 && cur.c == N-1) return true;
             for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
                 if (visited[nr][nc]) continue;
-                if (min <= array[nr][nc] && array[nr][nc] <= max) {
-                    visited[nr][nc] = true;
-                    queue.add(new Coordinate(nr, nc));
-                }
+                if (array[nr][nc] < min || max < array[nr][nc]) continue;
+                visited[nr][nc] = true;
+                queue.add(new Location(nr, nc));
             }
         }
         return false;
     }
-     */
 }
 
-/*
-class Coordinate {
+class Location {
     int r, c;
 
-    Coordinate (int r, int c) {
+    Location(int r, int c) {
         this.r = r;
         this.c = c;
     }
 }
- */
