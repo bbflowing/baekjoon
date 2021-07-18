f738acc1acf95feca9711ffa3cb71f76c6b7ff79
mjda611 <mjda611@naver.com>
2021-07-18 17:01:27 +0900

    version2

--- src/dijkstra/Q4485.java
+++ src/dijkstra/Q4485.java
@@ -1,7 +1,8 @@
 package dijkstra;
 
+// 녹색 옷 입은 애가 젤다지?
+
 import java.io.*;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -9,49 +10,53 @@ public class Q4485 {
     /*
     public static int N;
     public static int[][] cave;
+    public static int[][] dp;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int problem = 0;
+        StringTokenizer st;
+        N = -1;
+        int cases = 0;
         while (true) {
             N = Integer.parseInt(br.readLine());
             if (N == 0) break;
-            ++problem;
+            ++cases;
             cave = new int[N][N];
-            int[][] visited = new int[N][N];
-            for (int r=0; r<N; ++r) {
-                Arrays.fill(visited[r], -1);
+            dp = new int[N][N];
+            for (int r = 0; r < N; ++r) {
                 st = new StringTokenizer(br.readLine());
-                for (int c=0; c<N; ++c) cave[r][c] = Integer.parseInt(st.nextToken());
+                for (int c = 0; c < N; ++c) {
+                    cave[r][c] = Integer.parseInt(st.nextToken());
+                    dp[r][c] = -1;
+                }
             }
             StringBuilder sb = new StringBuilder();
-            sb.append("Problem "+problem+": "+dijkstra(visited));
+            sb.append("Problem "+cases+": ");
+            sb.append(bfs());
             bw.append(sb);
             bw.newLine();
         }
         bw.flush();
     }
 
-    public static int dijkstra(int[][] visited) {
+    public static int bfs() {
+        PriorityQueue<Location> queue = new PriorityQueue<>();
+        queue.add(new Location(0, 0, cave[0][0]));
+        dp[0][0] = cave[0][0];
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
-        PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        queue.add(new Coordinate(0, 0, cave[0][0]));
-        visited[0][0] = cave[0][0];
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
+            Location cur = queue.poll();
             if (cur.r == N-1 && cur.c == N-1) return cur.cost;
             for (int dir=0; dir<4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-
                 if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
-                if (visited[nr][nc] == -1 || cur.cost + cave[nr][nc] < visited[nr][nc]) {
-                    visited[nr][nc] = cur.cost + cave[nr][nc];
-                    queue.add(new Coordinate(nr, nc, visited[nr][nc]));
+                if (dp[nr][nc] == -1 || (dp[nr][nc] > cur.cost + cave[nr][nc])) {
+                    dp[nr][nc] = cur.cost + cave[nr][nc];
+                    queue.add(new Location(nr, nc, dp[nr][nc]));
                 }
             }
         }
@@ -61,18 +66,21 @@ public class Q4485 {
 }
 
 /*
-class Coordinate implements Comparable<Coordinate> {
+class Location implements Comparable<Location>{
     int r, c, cost;
 
-    Coordinate (int r, int c, int cost) {
+    Location(int r, int c, int cost) {
         this.r = r;
         this.c = c;
         this.cost = cost;
     }
 
     @Override
-    public int compareTo(Coordinate c) {
-        return this.cost - c.cost;
+    public int compareTo (Location l) {
+        if (this.cost > l.cost) return 1;
+        else return -1;
     }
 }
  */
+
+
