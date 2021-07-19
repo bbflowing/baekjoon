52f46cf3bd4f81b9d2250ce1d016591260c2ff56
mjda611 <mjda611@naver.com>
2021-07-19 14:49:25 +0900

    version2

--- src/graph/Q1987.java
+++ src/graph/Q1987.java
@@ -1,5 +1,7 @@
 package graph;
 
+// 알파벳
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,49 +11,40 @@ import java.util.StringTokenizer;
 public class Q1987 {
     /*
     public static int R, C, answer;
-    public static int[][] dp;
     public static char[][] board;
     public static boolean[] visited;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
 
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        dp = new int[R][C];
         board = new char[R][C];
+        visited = new boolean[26];
         String line = "";
         for (int r=0; r<R; ++r) {
             line = br.readLine();
-            for (int c=0; c<C; ++c) {
-                board[r][c] = line.charAt(c);
-                dp[r][c] = -1;
-            }
+            for (int c=0; c<C; ++c) board[r][c] = line.charAt(c);
         }
-        visited = new boolean[26];
-        answer = 0;
-        solve(0, 0, 1);
+        answer = 1;
+        visited[board[0][0]-'A'] = true;
+        dfs(0, 0, 1);
         System.out.println(answer);
     }
 
-    public static void solve (int r, int c, int count) {
-        if (count == 26) {
-            System.out.println(26);
-            System.exit(0);
-        }
-        answer = Math.max(answer, count);
+    public static void dfs (int r, int c, int distance) {
         for (int dir=0; dir<4; ++dir) {
             int nr = r + dr[dir];
             int nc = c + dc[dir];
-
             if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
             if (visited[board[nr][nc]-'A']) continue;
             visited[board[nr][nc]-'A'] = true;
-            solve(nr, nc, count+1);
+            dfs(nr, nc, distance+1);
             visited[board[nr][nc]-'A'] = false;
         }
+        answer = Math.max(distance, answer);
     }
      */
 }
