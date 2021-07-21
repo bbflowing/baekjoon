34d89744521f7f01d0cb220a1b79ca4cd05943ce
mjda611 <mjda611@naver.com>
2021-07-21 15:29:57 +0900

    version2

--- src/graph/Q13460.java
+++ src/graph/Q13460.java
@@ -13,118 +13,103 @@ public class Q13460 {
     /*
     public static int R, C;
     public static char[][] board;
+    public static Queue<Location> rq, bq;
     public static boolean[][][][] visited;
-    public static Queue<Location> bq, rq;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         board = new char[R][C];
-        String line = "";
-        Location blue = null; Location red = null;
-        bq = new ArrayDeque<>();
         rq = new ArrayDeque<>();
+        bq = new ArrayDeque<>();
         visited = new boolean[R][C][R][C];
+        String line = "";
+        Location red = null; Location blue = null;
         for (int r=0; r<R; ++r) {
             line = br.readLine();
             for (int c=0; c<C; ++c) {
                 board[r][c] = line.charAt(c);
-                if (board[r][c] == 'B') {
-                    bq.add(new Location(r, c));
-                    blue = new Location(r, c);
-                } else if (board[r][c] == 'R') {
-                    rq.add(new Location(r, c));
+                if (board[r][c] == 'R') {
                     red = new Location(r, c);
+                    rq.add(red);
+                } else if (board[r][c] == 'B') {
+                    blue = new Location(r, c);
+                    bq.add(blue);
                 }
             }
         }
-        visited[blue.r][blue.c][red.r][red.c] = true;
-        int times = 0;
+        visited[red.r][red.c][blue.r][blue.c] = true;
+        int time = 0;
         while (true) {
-            ++times;
-            if (move() && times <= 10) {
-                System.out.println(times);
-                break;
-            } else if (times == 10 || rq.size() == 0) {
-                System.out.println(-1);
+            ++time;
+            boolean result = bfs();
+            if (result || time == 10) {
+                if (result) break;
+                else time = -1;
                 break;
             }
         }
+        System.out.println(time);
     }
 
-    public static boolean move() {
+    public static boolean bfs() {
         int size = rq.size();
         for (int i=0; i<size; ++i) {
-            Location b = bq.poll();
-            Location r = rq.poll();
+            Location red = rq.poll();
+            Location blue = bq.poll();
 
             for (int dir=0; dir<4; ++dir) {
-                int nbr = b.r;
-                int nbc = b.c;
-                int nrr = r.r;
-                int nrc = r.c;
-
+                int nbr = blue.r;
+                int nbc = blue.c;
                 while (true) {
                     nbr += dr[dir];
                     nbc += dc[dir];
-                    if (board[nbr][nbc] == 'O') break;
-                    else if (board[nbr][nbc] == '#') {
-                        Location nb = adjust(dir, nbr, nbc);
-                        nbr = nb.r;
-                        nbc = nb.c;
+                    if (board[nbr][nbc] == '#') {
+                        nbr -= dr[dir];
+                        nbc -= dc[dir];
                         break;
-                    }
+                    } else if (board[nbr][nbc] == 'O') break;
                 }
                 if (board[nbr][nbc] == 'O') continue;
 
+                int nrr = red.r;
+                int nrc = red.c;
                 while (true) {
                     nrr += dr[dir];
                     nrc += dc[dir];
-                    if (board[nrr][nrc] == 'O') return true;
-                    else if (board[nrr][nrc] == '#') {
-                        Location nr = adjust(dir, nrr, nrc);
-                        nrr = nr.r;
-                        nrc = nr.c;
+                    if (board[nrr][nrc] == '#') {
+                        nrr -= dr[dir];
+                        nrc -= dc[dir];
                         break;
                     }
+                    if (board[nrr][nrc] == 'O') return true;
                 }
-
                 if (nbr == nrr && nbc == nrc) {
                     if (dir == 0) {
-                        if (b.r < r.r) ++nrr;
+                        if (blue.r < red.r) ++nrr;
                         else ++nbr;
                     } else if (dir == 1) {
-                        if (b.r < r.r) --nbr;
+                        if (blue.r < red.r) -- nbr;
                         else --nrr;
                     } else if (dir == 2) {
-                        if (b.c < r.c) ++nrc;
+                        if (blue.c < red.c) ++nrc;
                         else ++nbc;
                     } else {
-                        if (b.c < r.c) --nbc;
+                        if (blue.c < red.c) --nbc;
                         else --nrc;
                     }
                 }
-
-                if (!visited[nbr][nbc][nrr][nrc]) {
-                    visited[nbr][nbc][nrr][nrc] = true;
-                    bq.add(new Location(nbr, nbc));
-                    rq.add(new Location(nrr, nrc));
-                }
+                if (visited[nrr][nrc][nbr][nbc]) continue;
+                visited[nrr][nrc][nbr][nbc] = true;
+                rq.add(new Location(nrr, nrc));
+                bq.add(new Location(nbr, nbc));
             }
         }
         return false;
-    }
-
-    public static Location adjust(int dir, int r, int c) {
-        if (dir == 0) ++r;
-        else if (dir == 1) --r;
-        else if (dir == 2) ++c;
-        else --c;
-        return new Location(r, c);
     }
      */
 }
@@ -135,7 +120,7 @@ class Location {
 
     Location (int r, int c) {
         this.r = r;
-        this.c = c;
+        this.c =c;
     }
 }
  */
