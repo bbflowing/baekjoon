7e3137a3a11f051f71c7025c00123d5c8bbb31fc
mjda611 <mjda611@naver.com>
2021-07-21 13:27:37 +0900

    version2

--- src/graph/Q14948.java
+++ src/graph/Q14948.java
@@ -16,55 +16,60 @@ public class Q14948 {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         army = new int[R][C];
-        for (int r=0; r<R; ++r) {
+        for (int r = 0; r < R; ++r) {
             st = new StringTokenizer(br.readLine());
-            for (int c=0; c<C; ++c) {
-                army[r][c] = Integer.parseInt(st.nextToken());
-            }
+            for (int c = 0; c < C; ++c) army[r][c] = Integer.parseInt(st.nextToken());
         }
         bfs();
     }
 
     public static void bfs() {
-        int[][][][] visited = new int[2][2][R][C];
-        for (int i=0; i<2; ++i) {
-            for (int j=0; j<2; ++j) {
-                for (int k=0; k<R; ++k) Arrays.fill(visited[i][j][k], -1);
-            }
-        }
-        PriorityQueue<Coordinate> queue = new PriorityQueue<>();
-        queue.add(new Coordinate(0, 0, 0, army[0][0]));
-        visited[0][0][0][0] = army[0][0];
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
+        int[][][][] dp = new int[2][2][R][C];
+        for (int i=0; i<2; ++i) {
+            for (int j=0; j<2; ++j) {
+                for (int r=0; r<R; ++r) {
+                    for (int c=0; c<C; ++c) dp[i][j][r][c] = -1;
+                }
+            }
+        }
+        PriorityQueue<Location> queue = new PriorityQueue<>();
+        queue.add(new Location(0, 0, 0, army[0][0]));
 
         while (!queue.isEmpty()) {
-            Coordinate cur = queue.poll();
-            if (cur.r == R-1 && cur.c == C-1) {
+            Location cur = queue.poll();
+            if (cur.r == R - 1 && cur.c == C - 1) {
                 System.out.println(cur.max);
                 return;
             }
-            for (int dir=0; dir<4; ++dir) {
+            for (int dir = 0; dir < 4; ++dir) {
                 int nr = cur.r + dr[dir];
                 int nc = cur.c + dc[dir];
-                int nmax = cur.max;
-
-                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
-                nmax = Math.max(cur.max, army[nr][nc]);
-                if (visited[0][cur.skipped][nr][nc] != -1 && visited[0][cur.skipped][nr][nc] <= nmax) continue;
-                visited[0][cur.skipped][nr][nc] = nmax;
-                queue.add(new Coordinate(cur.skipped, nr, nc, nmax));
+                if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
+                int next = 0;
                 if (cur.skipped == 0) {
-                    int nnr = nr + dr[dir];
-                    int nnc = nc + dc[dir];
-                    if (nnr<0 || R<=nnr || nnc<0 || C<=nnc) continue;
-                    nmax = Math.max(army[nnr][nnc], cur.max);
-                    if (visited[1][0][nr][nc] != -1 && visited[1][0][nr][nc] <= nmax) continue;
-                    if (visited[0][1][nnr][nnc] != -1 && visited[0][1][nnr][nnc] <= nmax) continue;
-                    visited[1][0][nr][nc] = nmax;
-                    visited[0][1][nnr][nnc] = nmax;
-                    queue.add(new Coordinate(1, nnr, nnc, nmax));
+                    next = Math.max(cur.max, army[nr][nc]);
+                    if (dp[0][0][nr][nc] == -1 || next < dp[0][0][nr][nc]) {
+                        dp[0][0][nr][nc] = next;
+                        queue.add(new Location(0, nr, nc, next));
+                    }
+                    nr += dr[dir];
+                    nc += dc[dir];
+                    if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
+                    next = Math.max(cur.max, army[nr][nc]);
+                    if (dp[1][1][nr][nc] == -1 || next < dp[1][1][nr][nc]) {
+                        dp[1][1][nr][nc] = next;
+                        queue.add(new Location(1, nr, nc, next));
+                    }
+                } else {
+                    next = Math.max(cur.max, army[nr][nc]);
+                    if (dp[1][0][nr][nc] == -1 || next < dp[1][0][nr][nc]) {
+                        dp[1][0][nr][nc] = next;
+                        queue.add(new Location(1, nr, nc, next));
+                    }
                 }
+
             }
         }
     }
@@ -72,10 +77,10 @@ public class Q14948 {
 }
 
 /*
-class Coordinate implements Comparable<Coordinate> {
+class Location implements Comparable<Location> {
     int skipped, r, c, max;
 
-    Coordinate (int skipped, int r, int c, int max) {
+    Location(int skipped, int r, int c, int max) {
         this.skipped = skipped;
         this.r = r;
         this.c = c;
@@ -83,8 +88,10 @@ class Coordinate implements Comparable<Coordinate> {
     }
 
     @Override
-    public int compareTo (Coordinate c) {
-        return this.max - c.max;
+    public int compareTo(Location l) {
+        if (this.max > l.max) return 1;
+        else if (this.max == l.max) return 0;
+        else return -1;
     }
 }
  */
