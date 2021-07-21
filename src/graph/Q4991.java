aa99fcdb08e12ac962b21e63b5b946e996aaf742
mjda611 <mjda611@naver.com>
2021-07-21 14:44:12 +0900

    version2

--- src/graph/Q4991.java
+++ src/graph/Q4991.java
@@ -5,10 +5,12 @@ import java.util.*;
 
 public class Q4991 {
     /*
-    public static int R, C, answer;
+    public static int C, R, answer;
     public static char[][] room;
-    public static ArrayList<Location> dust;
-    public static int[][] distances;
+    public static int[][] distance;
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -21,52 +23,48 @@ public class Q4991 {
             C = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
             room = new char[R][C];
-            dust = new ArrayList<>();
-            int count = 0;
-            Location S = null;
+            Location start = null;
+            ArrayList<Location> dust = new ArrayList<>();
             for (int r=0; r<R; ++r) {
                 line = br.readLine();
                 for (int c=0; c<C; ++c) {
                     room[r][c] = line.charAt(c);
-                    if (room[r][c] == 'o') S = new Location(r, c);
-                    else if (room[r][c] == '*') {
-                        dust.add(new Location(r, c));
-                        ++count;
-                    }
+                    if (room[r][c] == 'o') start = new Location(r, c, 0);
+                    else if (room[r][c] == '*') dust.add(new Location(r, c, 0));
                 }
             }
-            distances = new int[count+1][count+1];
-            dust.add(0, S);
-            for (int i=0; i<count; ++i) {
+            dust.add(0, start);
+            distance = new int[dust.size()][dust.size()];
+            for (int i=0; i<dust.size()-1; ++i) {
                 Location begin = dust.get(i);
-                for (int j=i+1; j<count+1; ++j) {
-                    Location end = dust.get(j);
-                    int result = bfs(begin, end);
-                    distances[i][j] = result;
-                    distances[j][i] = distances[i][j];
+                for (int j=i+1; j<dust.size(); ++j) {
+                    Location end= dust.get(j);
+                    distance[i][j] = getDistance(begin, end);
+                    distance[j][i] = distance[i][j];
                 }
             }
             dust.remove(0);
-            int[] result = new int[count];
-            boolean[] visited = new boolean[count];
             answer = Integer.MAX_VALUE;
-            permutation(result, visited, count, 0);
-            if (answer == Integer.MAX_VALUE) answer = -1;
-            bw.append(String.valueOf(answer));
+            int[] result = new int[dust.size()];
+            boolean[] visited = new boolean[dust.size()];
+            permutation(result, visited, 0, dust.size());
+            if (answer == Integer.MAX_VALUE) bw.append(String.valueOf(-1));
+            else bw.append(String.valueOf(answer));
             bw.newLine();
         }
         bw.flush();
     }
 
-    public static void permutation(int[] result, boolean[] visited, int N, int index) {
+    public static void permutation(int[] result, boolean[] visited, int index, int N) {
         if (index == N) {
             int[] route = new int[N+1];
             route[0] = 0;
             for (int i=1; i<N+1; ++i) route[i] = result[i-1];
             int temp = 0;
             for (int i=0; i<N; ++i) {
-                if (distances[route[i]][route[i+1]] == -1) return;
-                temp += distances[route[i]][route[i+1]];
+                int next = distance[route[i]][route[i+1]];
+                if (next == -1) return;
+                temp += next;
                 if (answer < temp) return;
             }
             answer = temp;
@@ -75,38 +73,31 @@ public class Q4991 {
 
         for (int i=0; i<N; ++i) {
             if (!visited[i]) {
-                result[index] = i+1;
                 visited[i] = true;
-                permutation(result, visited, N, index+1);
+                result[index] = i+1;
+                permutation(result, visited, index+1, N);
                 visited[i] = false;
             }
         }
     }
 
-    public static int bfs(Location begin, Location end) {
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        boolean[][] visited = new boolean[R][C];
+    public static int getDistance (Location begin, Location end) {
         Queue<Location> queue = new ArrayDeque<>();
-        visited[begin.r][begin.c] = true;
+        boolean[][] visited = new boolean[R][C];
         queue.add(begin);
-        int distance = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            ++distance;
-            for (int i=0; i<size; ++i) {
-                Location cur = queue.poll();
-                for (int dir=0; dir<4; ++dir) {
-                    int nr = cur.r + dr[dir];
-                    int nc = cur.c + dc[dir];
+        visited[begin.r][begin.c] = true;
 
-                    if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
-                    if (nr == end.r && nc == end.c) return distance;
-                    if (visited[nr][nc]) continue;
-                    if (room[nr][nc] == 'x') continue;
-                    visited[nr][nc] = true;
-                    queue.add(new Location(nr, nc));
-                }
+        while (!queue.isEmpty()) {
+            Location cur = queue.poll();
+            if (cur.r == end.r && cur.c == end.c) return cur.distance;
+            for (int dir=0; dir<4; ++dir) {
+                int nr = cur.r + dr[dir];
+                int nc = cur.c + dc[dir];
+                if (nr<0 || R<=nr || nc<0 || C<=nc) continue;
+                if (room[nr][nc] == 'x') continue;
+                if (visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+                queue.add(new Location(nr, nc, cur.distance+1));
             }
         }
         return -1;
@@ -116,11 +107,12 @@ public class Q4991 {
 
 /*
 class Location {
-    int r, c;
+    int r, c, distance;
 
-    Location (int r, int c) {
+    Location (int r, int c, int distance) {
         this.r = r;
         this.c = c;
+        this.distance = distance;
     }
 }
  */
