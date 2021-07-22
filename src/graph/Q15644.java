2e2b1fb4efcbc5d207c3461c33f0178bacbb3e30
mjda611 <mjda611@naver.com>
2021-07-22 13:59:57 +0900

    version2

--- src/graph/Q15644.java
+++ src/graph/Q15644.java
@@ -11,67 +11,65 @@ import java.util.StringTokenizer;
 public class Q15644 {
     /*
     public static int R, C;
-    public static boolean result;
     public static char[][] board;
-    public static boolean[][][][] visited;
     public static Queue<Location> queue;
+    public static boolean[][][][] visited;
     public static int[] dr = {-1, 1, 0, 0};
     public static int[] dc = {0, 0, -1, 1};
-    public static char[] direction = {'U', 'D', 'L', 'R'};
+    public static String[] direction = {"U", "D", "L", "R"};
+    public static boolean result;
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         board = new char[R][C];
+        visited = new boolean[R][C][R][C];
+        queue = new ArrayDeque<>();
         String line = "";
-        int redR = 0; int redC = 0;
-        int blueR = 0; int blueC = 0;
+        int redr = 0; int redc = 0;
+        int bluer = 0; int bluec = 0;
         for (int r=0; r<R; ++r) {
             line = br.readLine();
             for (int c=0; c<C; ++c) {
                 board[r][c] = line.charAt(c);
-                if (board[r][c] == 'B') {
-                    blueR = r; blueC = c;
-                } else if (board[r][c] == 'R') {
-                    redR = r; redC = c;
+                if (board[r][c] == 'R') {
+                    redr = r; redc = c;
+                } else if (board[r][c] == 'B') {
+                    bluer = r; bluec =c;
                 }
             }
         }
-        visited = new boolean[R][C][R][C];
-        queue = new ArrayDeque<>();
-        queue.add(new Location(redR, redC, blueR, blueC, ""));
-        visited[redR][redC][blueR][blueC] = true;
+        queue.add(new Location(redr, redc, bluer, bluec, ""));
+        visited[redr][redc][bluer][bluec] = true;
         int times = 0;
         result = false;
         while (true) {
             ++times;
-            String route = move();
-            if (result && times <= 10) {
-                bw.append(String.valueOf(times));
-                bw.newLine();
-                bw.append(route);
-                break;
-            } else if (times == 10) {
-                bw.append(String.valueOf(-1));
+            String route = bfs();
+            if (result || times == 10) {
+                if (result) {
+                    System.out.println(times);
+                    System.out.println(route);
+                } else {
+                    System.out.println(-1);
+                }
                 break;
             }
         }
-        bw.flush();
     }
 
-    public static String move() {
+    public static String bfs() {
         int size = queue.size();
         for (int i=0; i<size; ++i) {
             Location cur = queue.poll();
+
             for (int dir=0; dir<4; ++dir) {
-                int nbr = cur.br;
-                int nbc = cur.bc;
                 int nrr = cur.rr;
                 int nrc = cur.rc;
-                String nroute = cur.route;
+                int nbr = cur.br;
+                int nbc = cur.bc;
 
                 while (true) {
                     nbr += dr[dir];
@@ -79,52 +77,48 @@ public class Q15644 {
 
                     if (board[nbr][nbc] == 'O') break;
                     else if (board[nbr][nbc] == '#') {
-                        if (dir == 0) ++nbr;
-                        else if (dir == 1) --nbr;
-                        else if (dir == 2) ++nbc;
-                        else --nbc;
+                        nbr -= dr[dir];
+                        nbc -= dc[dir];
                         break;
                     }
                 }
                 if (board[nbr][nbc] == 'O') continue;
 
-                while (true) {
+                String nroute = cur.route + direction[dir];
+                while(true) {
                     nrr += dr[dir];
                     nrc += dc[dir];
 
                     if (board[nrr][nrc] == 'O') {
                         result = true;
-                        return nroute+direction[dir];
-                    } else if (board[nrr][nrc] == '#') {
-                        if (dir == 0) ++nrr;
-                        else if (dir == 1) --nrr;
-                        else if (dir == 2) ++nrc;
-                        else --nrc;
+                        return nroute;
+                    }
+                    else if (board[nrr][nrc] == '#') {
+                        nrr -= dr[dir];
+                        nrc -= dc[dir];
                         break;
                     }
                 }
 
                 if (nbr == nrr && nbc == nrc) {
                     if (dir == 0) {
-                        if (cur.br < cur.rr) ++nrr;
-                        else ++nbr;
+                        if (cur.rr < cur.br) ++nbr;
+                        else ++nrr;
                     } else if (dir == 1) {
-                        if (cur.br < cur.rr) --nbr;
-                        else --nrr;
+                        if (cur.rr < cur.br) --nrr;
+                        else --nbr;
                     } else if (dir == 2) {
-                        if (cur.bc < cur.rc) ++nrc;
-                        else ++nbc;
+                        if (cur.rc < cur.bc) ++nbc;
+                        else ++nrc;
                     } else {
-                        if (cur.bc < cur.rc) --nbc;
-                        else --nrc;
+                        if (cur.rc < cur.bc) --nrc;
+                        else --nbc;
                     }
                 }
 
-                if (!visited[nrr][nrc][nbr][nbc]) {
-                    visited[nrr][nrc][nbr][nbc] = true;
-                    nroute += direction[dir];
-                    queue.add(new Location(nrr, nrc, nbr, nbc, nroute));
-                }
+                if (visited[nrr][nrc][nbr][nbc]) continue;
+                visited[nrr][nrc][nbr][nbc] = true;
+                queue.add(new Location(nrr, nrc, nbr, nbc, nroute));
             }
         }
         return "";
@@ -137,7 +131,7 @@ class Location {
     int rr, rc, br, bc;
     String route;
 
-    Location (int rr, int rc, int br, int bc, String route) {
+    Location(int rr, int rc, int br, int bc, String route) {
         this.rr = rr;
         this.rc = rc;
         this.br = br;
