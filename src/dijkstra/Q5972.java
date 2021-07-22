4da39e6360a8bec31d1ed520c1164962c1acdd9f
mjda611 <mjda611@naver.com>
2021-07-22 17:07:06 +0900

    version2

---
+++ src/dijkstra/Q5972.java
@@ -0,0 +1,74 @@
+package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Q5972 {
+    /*
+    public static int N;
+    public static ArrayList<ArrayList<Barn>> list;
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        for (int i=0; i<N+1; ++i) list.add(new ArrayList<>());
+        for (int i=0; i<M; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list.get(start).add(new Barn(end, cost));
+            list.get(end).add(new Barn(start, cost));
+        }
+        dijkstra();
+    }
+
+    public static void dijkstra() {
+        PriorityQueue<Barn> queue = new PriorityQueue<>();
+        queue.add(new Barn(1, 0));
+        int[] visited = new int[N+1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
+        visited[1] = 0;
+
+        while (!queue.isEmpty()) {
+            Barn cur = queue.poll();
+            if (cur.number == N) {
+                System.out.println(cur.cost);
+                return;
+            }
+            ArrayList<Barn> target = list.get(cur.number);
+            for (int i=0; i<target.size(); ++i) {
+                Barn next = target.get(i);
+                if (cur.cost + next.cost < visited[next.number]) {
+                    visited[next.number] = cur.cost + next.cost;
+                    queue.add(new Barn(next.number, visited[next.number]));
+                }
+            }
+        }
+    }
+     */
+}
+
+/*
+class Barn implements Comparable<Barn> {
+    int number, cost;
+
+    Barn(int number, int cost) {
+        this.number = number;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo (Barn b) {
+        if (this.cost > b.cost) return 1;
+        else return -1;
+    }
+}
+ */
--- src/graph/Q17142.java
+++ src/graph/Q17142.java
@@ -12,6 +12,9 @@ public class Q17142 {
     public static int N, M, blanks, answer;
     public static int[][] laboratory;
     public static ArrayList<Blank> virus;
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
+
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,73 +23,72 @@ public class Q17142 {
         laboratory = new int[N][N];
         virus = new ArrayList<>();
         blanks = 0;
+
         for (int r=0; r<N; ++r) {
             st = new StringTokenizer(br.readLine());
             for (int c=0; c<N; ++c) {
                 laboratory[r][c] = Integer.parseInt(st.nextToken());
-                if (laboratory[r][c] == 0) ++blanks;
-                else if (laboratory[r][c] == 2) virus.add(new Blank(r, c));
+                if (laboratory[r][c] == 2) virus.add(new Blank(r, c));
+                else if (laboratory[r][c] == 0) ++blanks;
             }
         }
-        if (blanks == 0) {
-            answer = 0;
-        } else {
+        answer = Integer.MAX_VALUE;
+        if (blanks == 0) answer = 0;
+        else {
             Blank[] result = new Blank[M];
-            answer = Integer.MAX_VALUE;
-            combination(result, 0, 0);
+            combination(result, virus.size(), 0, 0);
             if (answer == Integer.MAX_VALUE) answer = -1;
         }
         System.out.println(answer);
     }
 
-    public static int bfs(Blank[] result) {
-        Queue<Blank> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][N];
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        for (int i = 0; i < M; ++i) {
-            queue.add(result[i]);
-            visited[result[i].r][result[i].c] = true;
-        }
-        int time = 0;
-        int remaining = blanks;
-        int before = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            ++time;
-            before = remaining;
-            for (int i = 0; i < size; ++i) {
-                Blank cur = queue.poll();
-                for (int dir = 0; dir < 4; ++dir) {
-                    int nr = cur.r + dr[dir];
-                    int nc = cur.c + dc[dir];
-                    if (nr < 0 || N <= nr || nc < 0 || N <= nc) continue;
-                    if (visited[nr][nc]) continue;
-                    if (laboratory[nr][nc] == 1) continue;
-                    else {
-                        queue.add(new Blank(nr, nc));
-                        visited[nr][nc] = true;
-                        if (laboratory[nr][nc] == 0) --remaining;
-                    }
-                }
-            }
-            if (remaining == 0) return time;
-        }
-        return -1;
-    }
-
-    public static void combination(Blank[] result, int index, int start) {
+    public static void combination(Blank[] result, int N, int start, int index) {
         if (index == M) {
             int temp = bfs(result);
             if (temp != -1) answer = Math.min(answer, temp);
             return;
         }
 
-        if (start == virus.size()) return;
+        if (start == N) return;
 
         result[index] = virus.get(start);
-        combination(result, index+1, start+1);
-        combination(result, index, start+1);
+        combination(result, N, start+1, index+1);
+        combination(result, N, start+1, index);
+    }
+
+    public static int bfs(Blank[] result) {
+        Queue<Blank> queue = new ArrayDeque<>();
+        int count = blanks;
+        boolean[][] visited = new boolean[N][N];
+        for (int i=0; i<M; ++i) {
+            Blank next = result[i];
+            queue.add(next);
+            visited[next.r][next.c] = true;
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ++time;
+            for (int i=0; i<size; ++i) {
+                Blank cur = queue.poll();
+                for (int dir=0; dir<4; ++dir) {
+                    int nr = cur.r + dr[dir];
+                    int nc = cur.c + dc[dir];
+
+                    if (nr<0 || N<=nr || nc<0 || N<=nc) continue;
+                    if (visited[nr][nc]) continue;
+                    if (laboratory[nr][nc] != 1) {
+                        queue.add(new Blank(nr, nc));
+                        visited[nr][nc] = true;
+                        if (laboratory[nr][nc] == 0) {
+                            --count;
+                            if (count == 0) return time;
+                        }
+                    } else continue;
+                }
+            }
+        }
+        return -1;
     }
      */
 }
@@ -94,10 +96,9 @@ public class Q17142 {
 /*
 class Blank {
     int r, c;
-
     Blank (int r, int c) {
         this.r = r;
-        this.c = c;
+        this.c =c;
     }
 }
  */
